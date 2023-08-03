6977506ff8b2bd86d7ba876fe33696d3ab6f7059
Nicolas Oudard <nicolas@oudard.org>
2023-08-03 18:27:11 +0200

    remove useless declaration from admin

--- qfdmo/admin.py
+++ qfdmo/admin.py
@@ -5,12 +5,10 @@ from .models import Category, SubCategory
 
 class SubCategoryAdmin(admin.ModelAdmin):
     list_display = ("name", "category", "code")
-    verbose_name = "Sous-catégorie"
-    verbose_name_plural = "Sous-catégories"
     search_fields = [
         "category__name",
-        "name",
         "code",
+        "name",
     ]
 
 
