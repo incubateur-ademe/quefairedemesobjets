de594586a1d63b8e37df85faabe6ecfddbf1cb0c
Nicolas Oudard <nicolas@oudard.org>
2023-08-11 15:24:32 +0200

    Add nginx and dual domain setup

--- qfdmd/templatetags/qfdmd_tags.py
+++ qfdmd/templatetags/qfdmd_tags.py
@@ -1,4 +1,5 @@
 from typing import cast
+import logging
 
 from django import template
 from django.conf import settings
@@ -7,6 +8,7 @@ from django.forms import FileField
 from django.utils.safestring import mark_safe
 
 register = template.Library()
+logger = logging.getLogger(__name__)
 
 
 @register.inclusion_tag("components/patchwork/patchwork.html")
@@ -26,16 +28,24 @@ def render_file_content(file_field: FileField) -> str:
     """Renders the content of a Filefield as a safe HTML string
     and caches the result."""
 
-    def get_file_content() -> str:
-        with file_field.open() as f:
-            return mark_safe(f.read().decode("utf-8"))  # noqa: S308
+    try:
 
-    return cast(
-        str,
-        cache.get_or_set(
-            f"filefield-{file_field.name}-{file_field.size}", get_file_content
-        ),
-    )
+        def get_file_content() -> str:
+            with file_field.open() as f:
+                return mark_safe(f.read().decode("utf-8"))  # noqa: S308
+
+        return cast(
+            str,
+            cache.get_or_set(
+                f"filefield-{file_field.name}-{file_field.size}", get_file_content
+            ),
+        )
+    except FileNotFoundError as exception:
+        # We silent the error here to prevent crashing a page for a missing svg
+        logger.warning("An error was quietly ignored")
+        logger.error(exception)
+
+        return ""
 
 
 @register.inclusion_tag("head/favicon.html")
