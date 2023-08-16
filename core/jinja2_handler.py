836799e5679616ca1dbb790061966313a48c34e2
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2023-08-16 14:00:05 +0200

    display without headers and footers when iframe=1 is set passed is query (#28)
    * display without headers and footers when iframe=1 is set passed as query parameter
    
    * test is_iframe
    
    * run integration test in github action

--- core/jinja2_handler.py
+++ core/jinja2_handler.py
@@ -1,7 +1,12 @@
+from django.http import HttpRequest
 from django.templatetags.static import static
 from django.urls import reverse
 
 from jinja2 import Environment
+
+
+def is_iframe(request: HttpRequest) -> bool:
+    return "iframe" in request.GET
 
 
 def environment(**options):
@@ -10,6 +15,7 @@ def environment(**options):
         {
             "static": static,
             "reverse": reverse,
+            "is_iframe": is_iframe,
         }
     )
     return env
