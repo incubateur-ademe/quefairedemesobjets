fa45a3ea4a6ac39f3b58599f4bbe362ae928a8c5
Nicolas Oudard <nicolas@oudard.org>
2023-08-28 18:16:20 +0200

    add node tests on CI

--- static/to_compile/tests/actor.test.js
+++ static/to_compile/tests/actor.test.js
@@ -2,7 +2,7 @@ const { Actor } = require("../src/types")
 // FIXME : make the ES6 syntax worls with ts-jest and parcel
 //import { Actor } from "../src/types"
 
-describe("sum module", () => {
+describe("Actor class", () => {
     test("Actor title", () => {
         let actor = new Actor({ nom: "My name !" })
         expect(actor.popupTitle()).toBe("<p><strong>My name !</strong></b><br>")
