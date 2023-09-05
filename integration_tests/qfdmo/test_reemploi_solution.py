916f4ac7f704f704f1d296aa7e7e64b32189511a
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2023-09-05 18:01:25 +0200

    Add telephone to card in map (#70)
    * Add telephone to card in map
    
    * Add telephone in popup and test

---
+++ integration_tests/qfdmo/test_reemploi_solution.py
@@ -0,0 +1,29 @@
+import pytest
+from django.core.management import call_command
+
+
+@pytest.fixture(scope="session")
+def django_db_setup(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        call_command(
+            "loaddata", "categories", "actions", "acteur_services", "acteur_types"
+        )
+
+
+class TestReemploiSolutionView:
+    @pytest.mark.django_db
+    def test_without_parameters(self, client):
+        url = ""
+
+        response = client.get(url)
+
+        assert response.status_code == 200
+        assert response.context_data["location"] == "{}"
+        assert response.context_data["acteurs"].count() == 0
+        assert response.context_data["form"].initial == {
+            "sous_categorie_objet": None,
+            "adresse": None,
+            "direction": "jai",
+            "latitude": None,
+            "longitude": None,
+        }
