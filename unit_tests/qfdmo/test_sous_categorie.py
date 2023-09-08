467610537345dd4c10328a3151bba816ae540884
Nicolas Oudard <nicolas@oudard.org>
2023-09-08 13:14:51 +0200

    fix populate issue because of renaming ss-cat

--- unit_tests/qfdmo/test_sous_categorie.py
+++ unit_tests/qfdmo/test_sous_categorie.py
@@ -1,11 +1,30 @@
 import pytest
 
-from qfdmo.models import CategorieObjet, NomAsNaturalKeyModel, SousCategorieObjet
+from qfdmo.models import CategorieObjet, SousCategorieObjet
 
 
-class TestSousCategorieObjetNomAsNaturalKeyHeritage:
-    def test_natural(self):
-        assert NomAsNaturalKeyModel in SousCategorieObjet.mro()
+class TestActionStr:
+    def test_str_blank(self):
+        assert SousCategorieObjet(nom="").__str__() == ""
+
+    def test_str_specialchar(self):
+        assert SousCategorieObjet(nom="Åctïôn").__str__() == "Åctïôn"
+
+
+class TestActionNaturalKey:
+    def test_natural_key(self):
+        assert SousCategorieObjet(nom="Natural key", code="C_00").natural_key() == (
+            "C_00",
+        )
+
+    @pytest.mark.django_db()
+    def test_get_natural_key(self):
+        action = SousCategorieObjet(nom="Natural key", code="C_00")
+        action.save()
+        assert (
+            SousCategorieObjet.objects.get_by_natural_key("C_00").__str__()
+            == "Natural key"
+        )
 
 
 class TestSousCategorieObjetSanitizedName:
