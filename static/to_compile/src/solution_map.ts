86a55c446532ba740dd6b905223ec375bdfe15a5
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2023-10-05 15:16:25 +0200

    display marker with the color of the main action (#117)
    * display marker with the color of the main action
    
    * fix tests

--- static/to_compile/src/solution_map.ts
+++ static/to_compile/src/solution_map.ts
@@ -1,11 +1,38 @@
 import L from "leaflet"
+import "leaflet-extra-markers/dist/js/leaflet.extra-markers.min.js"
+import { defaultMarker, homeIconMarker } from "./icon_marker"
 import { Actor, Location } from "./types"
 
 const DEFAULT_LOCATION: Array<Number> = [46.227638, 2.213749]
 const DEFAULT_ZOOM: Number = 5
 const DEFAULT_MAX_ZOOM: Number = 19
+const COLOR_MAPPING: object = {
+    "blue-france": "#0055FF",
+    "green-tilleul-verveine": "#B7A73F",
+    "green-bourgeon": "#68A532",
+    "green-emeraude": "#00A95F",
+    "green-menthe": "#009081",
+    "green-archipel": "#009099",
+    "blue-ecume": "#465F9D",
+    "blue-cumulus": "#417DC4",
+    "purple-glycine": "#A558A0",
+    "pink-macaron": "#E18B76",
+    "pink-tuile": "#CE614A",
+    "yellow-tournesol": "#C8AA39",
+    "yellow-moutarde": "#C3992A",
+    "orange-terre-battue": "#E4794A",
+    "brown-cafe-creme": "#D1B781",
+    "brown-caramel": "#C08C65",
+    "brown-opera": "#BD987A",
+    "beige-gris-galet": "#AEA397",
+}
 
-import { homeIconMarker, redMarker } from "./icon_marker"
+function get_color_code(colorName: string): string {
+    if (colorName in COLOR_MAPPING) {
+        return COLOR_MAPPING[colorName]
+    }
+    return "#000"
+}
 
 export class SolutionMap {
     #map: L.Map
@@ -19,7 +46,7 @@ export class SolutionMap {
         this.#map.setView(DEFAULT_LOCATION, DEFAULT_ZOOM)
         L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
             maxZoom: DEFAULT_MAX_ZOOM,
-            attribution: "© OpenStreetMap",
+            attribution: "© OSM",
         }).addTo(this.#map)
         L.control.scale({ imperial: false }).addTo(this.#map)
         if (
@@ -39,10 +66,24 @@ export class SolutionMap {
 
         actors.forEach(function (actor: Actor) {
             if (actor.location) {
+                var customMarker = undefined
+                if (actor.actions.length > 0) {
+                    console.log(actor.actions[0])
+                    customMarker = L.ExtraMarkers.icon({
+                        icon: actor.actions[0].icon,
+                        markerColor: get_color_code(actor.actions[0].couleur),
+                        shape: "square",
+                        prefix: "qfdmo-icon",
+                        svg: true,
+                    })
+                } else {
+                    customMarker = defaultMarker
+                }
                 L.marker(
                     [actor.location.coordinates[1], actor.location.coordinates[0]],
                     {
-                        icon: redMarker,
+                        icon: customMarker,
+                        riseOnHover: true,
                     },
                 )
                     .addTo(this.#map)
