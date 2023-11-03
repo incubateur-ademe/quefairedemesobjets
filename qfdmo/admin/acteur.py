41ec6e500928bbc32c9a27aab0c3cf46845de7b6
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2023-11-03 19:29:28 +0100

    Deactivate whitenoise (#189)
    fix admin issue about redirect to acteur

--- qfdmo/admin/acteur.py
+++ qfdmo/admin/acteur.py
@@ -80,6 +80,9 @@ class FinalPropositionServiceInline(
     def has_add_permission(self, request: HttpRequest, obj=None) -> bool:
         return False
 
+    def has_change_permission(self, request, obj=None):
+        return False
+
     def has_delete_permission(self, request: HttpRequest, obj=None) -> bool:
         return False
 
