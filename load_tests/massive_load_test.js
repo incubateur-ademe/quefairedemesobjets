be402e526a0076c1fea5bff2f4473b3b24cfcbef
Nicolas Oudard <nicolas@oudard.org>
2023-11-06 23:14:17 +0100

    keep opened connexion

---
+++ load_tests/massive_load_test.js
@@ -0,0 +1,33 @@
+import http from "k6/http"
+
+import { check, sleep } from "k6"
+
+export const options = {
+    // vus: 10,
+    // duration: "30s",
+    stages: [
+        { duration: "15m", target: 500 },
+        { duration: "10m", target: 500 },
+        { duration: "5m", target: 0 },
+    ],
+}
+
+export default function () {
+    // https://longuevieauxobjets.ademe.fr/?r=76&direction=jecherche&action_list=&sous_categorie_objet=V%C3%A9lo&adresse=10+Rue+de+Gess%C3%A9+93200+Saint-Denis&longitude=2.349562&latitude=48.940018&digital=&preter=on&mettreenlocation=on&reparer=on&donner=on&echanger=on&revendre=on&emprunter=on&louer=on&echanger=on&acheter=on#solutions
+    // https://longuevieauxobjets.ademe.fr/?r=76&direction=jecherche&longitude=2.349562&latitude=48.940018
+    let directionOptions = ["jai", "jecherche"]
+    let directionIndex = Math.floor(Math.random() * directionOptions.length)
+    let direction = directionOptions[directionIndex]
+    let longitude = 6 * Math.random()
+    let latitude = 44 + 5 * Math.random()
+    let myUrlWithParams = `https://longuevieauxobjets.ademe.fr/?r=${Math.floor(
+        Math.random() * 100,
+    )}&direction=${direction}&longitude=${longitude}&latitude=${latitude}&adresse=fake+adresse`
+
+    const res = http.get(myUrlWithParams, {
+        tags: { name: "GetSolutions" },
+    })
+
+    check(res, { "status was 200": (r) => r.status == 200 })
+    sleep(10 * Math.random())
+}
