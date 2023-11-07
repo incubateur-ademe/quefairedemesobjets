e3f74e96b57682e0c9dd3f6bd83dee5a606553f2
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2023-11-07 22:38:30 +0100

    Update display tally links (#207)
    * force push to prod
    
    * update display tally links

--- integration_tests/core/test_display_as_iframe.py
+++ integration_tests/core/test_display_as_iframe.py
@@ -11,6 +11,11 @@ class TestDisplayAsIframe:
         assert response.status_code == 200
         assert 'class="fr-header' not in str(response.content)
         assert 'class="fr-footer' not in str(response.content)
+        assert (
+            "Retrouver les bonnes adresses sur <a "
+            'href="https://longuevieauxobjets.ademe.fr/" target="_blank">'
+            "longuevieauxobjets.ademe.fr</a>" in str(response.content)
+        )
 
     @pytest.mark.django_db
     def test_display_as_no_iframe(self, client):
@@ -21,3 +26,8 @@ class TestDisplayAsIframe:
         assert response.status_code == 200
         assert 'class="fr-header' in str(response.content)
         assert 'class="fr-footer' in str(response.content)
+        assert (
+            "Retrouver les bonnes adresses sur <a "
+            'href="https://longuevieauxobjets.ademe.fr/" target="_blank">'
+            "longuevieauxobjets.ademe.fr</a>" not in str(response.content)
+        )
