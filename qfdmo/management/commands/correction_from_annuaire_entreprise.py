ededccc368c1ab99d6123d2b45b6160dddba6826
Nicolas Oudard <nicolas@oudard.org>
2023-11-16 16:12:21 +0100

    update corrections by siret

--- qfdmo/management/commands/correction_from_annuaire_entreprise.py
+++ qfdmo/management/commands/correction_from_annuaire_entreprise.py
@@ -11,27 +11,54 @@ from qfdmo.models import (
     CorrectionActeurStatus,
     FinalActeur,
 )
-from qfdmo.models.acteur import Acteur
 
-SOURCE = "Recherche entreprise"
+SOURCE = "RechercheSiret"
 
 
-def call_api_recherche_entreprise(entreprise_id: str) -> dict | None:
-    insee_data = None
+def call_api_recherche_entreprise(entreprise_id: str) -> dict:
+    analyse = {
+        "siret_exist": True,
+        "error": None,
+        "etablissement_actif": True,
+        "entreprise_active": True,
+        "nb_etablissements": None,
+        "raw_result": None,
+    }
     if len(entreprise_id) == 14:
         response = requests.get(
             "https://api.recherche-entreprises.fabrique.social.gouv.fr/api/v1/"
             f"etablissement/{entreprise_id}"
         )
-        if response.status_code == 404:
-            # Should be removed
-            return insee_data
-        if response.status_code != 200:
-            print(f"error {response.status_code} : {entreprise_id}")
-        return response.json()
 
-    print(f"not the right length : {entreprise_id}")
-    return insee_data
+        if response.status_code == 404:
+            analyse["exists"] = False
+        elif response.status_code != 200:
+            analyse["error"] = {
+                "code": response.status_code,
+                "message": f"{entreprise_id} - erreur API : {response.text}",
+            }
+        else:
+            etablissement = response.json()
+
+            analyse["etablissement_actif"] = (
+                "etatAdministratifEtablissement" in etablissement
+                and etablissement["etatAdministratifEtablissement"] == "A"
+            )
+            analyse["entreprise_active"] = (
+                "etatAdministratifUniteLegale" in etablissement
+                and etablissement["etatAdministratifUniteLegale"] == "A"
+            )
+            analyse["nb_etablissements"] = (
+                etablissement["etablissements"]
+                if "etablissements" in etablissement
+                else None
+            )
+            analyse["raw_result"] = response.json()
+        return analyse
+    else:
+        raise Exception(
+            f"should be called with a siret (14 digits), not : {entreprise_id}"
+        )
 
 
 class Command(BaseCommand):
@@ -85,19 +112,32 @@ class Command(BaseCommand):
 
         for final_acteur in final_acteurs:
             print(final_acteur, " : ", final_acteur.siret)
-            insee_data = call_api_recherche_entreprise(final_acteur.siret)
-
-            if insee_data is not None:
-                if "activitePrincipaleEtablissement" in insee_data:
-                    acteur = Acteur.objects.get(
-                        identifiant_unique=final_acteur.identifiant_unique
-                    )
-                    acteur.naf_principal = insee_data["activitePrincipaleEtablissement"]
-                    acteur.save()
-
+            siret_analyse = call_api_recherche_entreprise(final_acteur.siret)
+            if not siret_analyse["siret_exist"]:
                 CorrectionActeur.objects.create(
                     source=SOURCE,
-                    resultat_brute_source=insee_data,
+                    resultat_brute_source={"code": 404, "message": "Siret non trouvé"},
+                    identifiant_unique=final_acteur.identifiant_unique,
+                    final_acteur_id=final_acteur.identifiant_unique,
+                    correction_statut=CorrectionActeurStatus.ACTIF,
+                )
+                continue
+            if siret_analyse["error"] is not None:
+                CorrectionActeur.objects.create(
+                    source=SOURCE,
+                    resultat_brute_source=siret_analyse["error"],
+                    identifiant_unique=final_acteur.identifiant_unique,
+                    final_acteur_id=final_acteur.identifiant_unique,
+                    correction_statut=CorrectionActeurStatus.ACTIF,
+                )
+                continue
+            if (
+                siret_analyse["etablissement_actif"]
+                and siret_analyse["entreprise_active"]
+            ):
+                CorrectionActeur.objects.create(
+                    source=SOURCE,
+                    resultat_brute_source=siret_analyse,
                     identifiant_unique=final_acteur.identifiant_unique,
                     final_acteur_id=final_acteur.identifiant_unique,
                     correction_statut=CorrectionActeurStatus.PAS_DE_MODIF,
@@ -105,7 +145,7 @@ class Command(BaseCommand):
             else:
                 CorrectionActeur.objects.create(
                     source=SOURCE,
-                    resultat_brute_source="{}",
+                    resultat_brute_source=siret_analyse,
                     identifiant_unique=final_acteur.identifiant_unique,
                     final_acteur_id=final_acteur.identifiant_unique,
                     correction_statut=CorrectionActeurStatus.ACTIF,
