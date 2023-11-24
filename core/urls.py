9835b88f105badfe76534506da15793096538cf1
Nicolas Oudard <nicolas@oudard.org>
2023-11-24 16:50:49 +0100

    remove account path which are useless and finish as 500 error

--- core/urls.py
+++ core/urls.py
@@ -16,13 +16,10 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
-    path("accounts/logout/", auth_views.logout_then_login, name="logout"),
     path("explorer/", include("explorer.urls")),
     path("", include(("qfdmo.urls", "qfdmo"), namespace="qfdmo")),
 ]
