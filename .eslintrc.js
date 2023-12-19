9c3db3f6e49881a17c46e738f0691133f2dbeb6d
Nicolas Oudard <nicolas@oudard.org>
2023-12-19 14:47:57 +0100

    use prettier with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,26 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    "extends": "standard-with-typescript",
-    "overrides": [
+    extends: ["standard-with-typescript", "prettier"],
+    plugins: ["prettier"],
+    overrides: [
         {
-            "env": {
-                "node": true
+            env: {
+                node: true,
             },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script",
+            },
+        },
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
     },
-    "rules": {
-    }
+    rules: {
+        "prettier/prettier": "error",
+    },
 }
