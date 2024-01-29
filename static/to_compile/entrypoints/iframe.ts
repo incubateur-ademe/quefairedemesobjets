342648c07ffbe553f6644de1f027817d910c0c56
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2024-01-29 15:32:43 +0100

    manage calling resizable iframe using a single js tag (#374)
    * mange calling resizable iframe using a single js tag
    
    * trying refacto
    
    * refacto and finish iframe version using JS tag
    
    * adding iframe-resizer missing package
    
    * typos and remove console.log
    
    * dynamically get script host
    
    * Adding playwright tests for iframe purpose (#388)
    
    * add playwright tests
    
    * check width and height of iframe
    
    * update tests

--- static/to_compile/entrypoints/iframe.ts
+++ static/to_compile/entrypoints/iframe.ts
@@ -1,11 +1,47 @@
-function postHeightSize(): void {
-    const target: Window | undefined = parent.postMessage ? parent : undefined
+import { iframeResize } from "iframe-resizer"
 
-    if (target && document.body.scrollHeight) {
-        target.postMessage(document.body.scrollHeight, "*")
+const setupIframe = () => {
+    // Add iFrame just after the script tag
+    const scriptTag = document.currentScript as HTMLScriptElement
+    const BASE_URL = new URL(scriptTag.getAttribute("src")!).origin
+
+    const urlParams = new URLSearchParams()
+    urlParams.append("iframe", "1")
+    let maxWidth = 800
+    for (const param in scriptTag.dataset) {
+        if (param === "max_width") {
+            maxWidth = parseInt(scriptTag.dataset[param])
+            continue
+        }
+        urlParams.append(param, scriptTag.dataset[param])
     }
+
+    const iframe = document.createElement("iframe")
+    const iframeAttributes = {
+        src: `${BASE_URL}?${urlParams.toString()}`,
+        id: "lvao_iframe",
+        frameborder: "0",
+        scrolling: "no",
+        allow: "geolocation",
+        style: "width: 100%",
+        allowfullscreen: true,
+        webkitallowfullscreen: true,
+        mozallowfullscreen: true,
+    }
+    for (var key in iframeAttributes) {
+        iframe.setAttribute(key, iframeAttributes[key])
+    }
+    scriptTag.insertAdjacentElement("afterend", iframe)
+
+    iframeResize(
+        {
+            heightCalculationMethod: "bodyScroll",
+            maxWidth: maxWidth,
+            checkOrigin: [BASE_URL],
+        },
+        iframe,
+    )
 }
 
-window.addEventListener("load", function (): void {
-    postHeightSize()
-})
+//addIframeResizer()
+setupIframe()
