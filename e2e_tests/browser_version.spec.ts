338ecf71e2ef3188b162ced67f8ab68176dbf557
Nicolas Oudard <nicolas@oudard.org>
2024-04-15 09:48:45 +0200

    Update browserlist and test it is not displayed by default

---
+++ e2e_tests/browser_version.spec.ts
@@ -0,0 +1,19 @@
+import { expect, test } from "@playwright/test"
+
+test("Browser version alert is not displayed", async ({ page }) => {
+    await page.goto(`http://localhost:8000/`, { waitUntil: "networkidle" })
+
+    const titlePage = await page.title()
+    expect(titlePage).toBe("Longue vie aux objets")
+
+    // Check that the browser version alert is not displayed
+    const alert = await page.$("#obsolete_browser_message")
+    expect(await alert?.getAttribute("class")).toContain("qfdmo-hidden")
+
+    // check libelé "L'application nécessite Javascript pour être exécutée correctement" is inside a noscript tag
+    const noscript = await page.$("noscript")
+    const noscriptContent = await noscript?.textContent()
+    expect(noscriptContent).toContain(
+        "L'application nécessite Javascript pour être exécutée correctement",
+    )
+})
