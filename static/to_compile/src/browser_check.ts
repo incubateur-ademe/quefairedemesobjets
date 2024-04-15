0f7773f96ce8f95db007eaaa15c3c567a9d4637b
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2024-04-15 09:57:12 +0200

    Update browserlist and test it is not displayed by default (#523)

--- static/to_compile/src/browser_check.ts
+++ static/to_compile/src/browser_check.ts
@@ -4,15 +4,11 @@ function isBrowserSupported(): boolean {
     const browser = Bowser.getParser(window.navigator.userAgent)
     const isValidBrowser = browser.satisfies({
         // declare browsers and versions you want to support
-        // 2017
-        chrome: ">60",
-        // 2017
-        firefox: ">55",
-        // 2017
-        safari: ">10",
-        // 2019
-        opera: ">50",
-        edge: ">15",
+        chrome: ">85",
+        firefox: ">79",
+        safari: ">=14",
+        opera: ">70",
+        edge: ">84",
         ie: "none",
     })
 
