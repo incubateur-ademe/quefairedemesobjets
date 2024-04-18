e8ddde577ec9fbfab6cfe558cc8fcba124e4baa5
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2024-04-18 16:48:41 +0200

    add tests about displaying headers of of adresse detail (#536)

--- integration_tests/qfdmo/test_adresse_detail.py
+++ integration_tests/qfdmo/test_adresse_detail.py
@@ -9,10 +9,29 @@ class TestDisplayLabel:
     def test_display_ess_label(self, client):
 
         adresse = DisplayedActeurFactory()
-        label = LabelQualiteFactory(
+        label_ess = LabelQualiteFactory(
             code="ess",
             libelle="Enseigne de l'économie sociale et solidaire",
             type_enseigne=True,
+        )
+        adresse.labels.add(label_ess)
+
+        url = f"/adresse/{adresse.identifiant_unique}"
+
+        response = client.get(url)
+        assert response.status_code == 200
+
+        soup = BeautifulSoup(response.content, "html.parser")
+        label_tag = soup.find(attrs={"data-testid": "adresse_detail_header_tag"})
+        assert label_tag is not None
+        assert "Enseigne de l'économie sociale et solidaire" in label_tag.text
+
+    def test_display_one_label(self, client):
+
+        adresse = DisplayedActeurFactory()
+        label = LabelQualiteFactory(
+            code="label",
+            libelle="Mon label",
         )
         adresse.labels.add(label)
 
@@ -22,6 +41,105 @@ class TestDisplayLabel:
         assert response.status_code == 200
 
         soup = BeautifulSoup(response.content, "html.parser")
-        label_tag = soup.find("p", {"class": "fr-tag"})
+        label_tag = soup.find(attrs={"data-testid": "adresse_detail_header_tag"})
         assert label_tag is not None
-        assert "Enseigne de l'économie sociale et solidaire" in label_tag.text
+        assert "Mon label" in label_tag.text
+
+    def test_display_two_label(self, client):
+
+        adresse = DisplayedActeurFactory()
+        label1 = LabelQualiteFactory(
+            code="label1",
+            libelle="Mon label 1",
+        )
+        label2 = LabelQualiteFactory(
+            code="label2",
+            libelle="Mon label 2",
+        )
+        adresse.labels.add(label1, label2)
+
+        url = f"/adresse/{adresse.identifiant_unique}"
+
+        response = client.get(url)
+        assert response.status_code == 200
+
+        soup = BeautifulSoup(response.content, "html.parser")
+        label_tag = soup.find(attrs={"data-testid": "adresse_detail_header_tag"})
+        assert label_tag is not None
+        assert "Cet établissement dispose de plusieurs labels" in label_tag.text
+
+    def test_display_bonus_label(self, client):
+
+        adresse = DisplayedActeurFactory()
+        label = LabelQualiteFactory(
+            code="label",
+            libelle="Mon label",
+            bonus=True,
+        )
+        adresse.labels.add(label)
+
+        url = f"/adresse/{adresse.identifiant_unique}"
+
+        response = client.get(url)
+        assert response.status_code == 200
+
+        soup = BeautifulSoup(response.content, "html.parser")
+        label_tag = soup.find(attrs={"data-testid": "adresse_detail_header_tag"})
+        assert label_tag is not None
+        assert "Éligible au bonus réparation" in label_tag.text
+
+    def test_display_bonus_first_label(self, client):
+        adresse = DisplayedActeurFactory()
+        label_bonus = LabelQualiteFactory(
+            code="label",
+            libelle="Mon label",
+            bonus=True,
+        )
+        label1 = LabelQualiteFactory(
+            code="label1",
+            libelle="Mon label 1",
+        )
+        label2 = LabelQualiteFactory(
+            code="label2",
+            libelle="Mon label 2",
+        )
+        label_ess = LabelQualiteFactory(
+            code="ess",
+            libelle="Enseigne de l'économie sociale et solidaire",
+            type_enseigne=True,
+        )
+        adresse.labels.add(label_bonus, label1, label2, label_ess)
+
+        url = f"/adresse/{adresse.identifiant_unique}"
+
+        response = client.get(url)
+        assert response.status_code == 200
+
+        soup = BeautifulSoup(response.content, "html.parser")
+        label_tag = soup.find(attrs={"data-testid": "adresse_detail_header_tag"})
+        assert label_tag is not None
+        assert "Éligible au bonus réparation" in label_tag.text
+
+    def test_display_label_before_ess(self, client):
+
+        adresse = DisplayedActeurFactory()
+        label = LabelQualiteFactory(
+            code="label",
+            libelle="Mon label",
+        )
+        label_ess = LabelQualiteFactory(
+            code="ess",
+            libelle="Enseigne de l'économie sociale et solidaire",
+            type_enseigne=True,
+        )
+        adresse.labels.add(label, label_ess)
+
+        url = f"/adresse/{adresse.identifiant_unique}"
+
+        response = client.get(url)
+        assert response.status_code == 200
+
+        soup = BeautifulSoup(response.content, "html.parser")
+        label_tag = soup.find(attrs={"data-testid": "adresse_detail_header_tag"})
+        assert label_tag is not None
+        assert "Mon label" in label_tag.text
