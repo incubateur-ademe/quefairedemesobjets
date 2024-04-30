47f3a0cfd257545cd585b2e1157b3b086fb591a1
Nicolas Oudard <nicolas@oudard.org>
2024-04-30 15:47:24 +0200

    display list ordered in django admin

--- unit_tests/qfdmo/test_acteur_service.py
+++ unit_tests/qfdmo/test_acteur_service.py
@@ -1,11 +1,18 @@
 import pytest
 
 from qfdmo.models import ActeurService, CodeAsNaturalKeyModel
+from unit_tests.qfdmo.acteur_factory import ActeurServiceFactory
 
 
 class TestEntiteServiceNomAsNaturalKeyHeritage:
     def test_natural(self):
         assert CodeAsNaturalKeyModel in ActeurService.mro()
+
+    def test_str(self):
+        acteur_service = ActeurServiceFactory.build(
+            code="My Code", libelle="My Libelle"
+        )
+        assert str(acteur_service) == "My Libelle (My Code)"
 
     @pytest.mark.django_db
     def test_serialize(self):
