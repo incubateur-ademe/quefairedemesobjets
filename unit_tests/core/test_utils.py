c29e539c61b809411344dac9b70b0445b553ed32
Nicolas Oudard <nicolas@oudard.org>
2024-05-06 15:56:08 +0200

    test core/utils.py>get_direction

---
+++ unit_tests/core/test_utils.py
@@ -0,0 +1,43 @@
+from unittest.mock import patch
+
+import pytest
+from django.test import RequestFactory, override_settings
+
+from core import utils
+
+
+@pytest.fixture
+def request_factory():
+    return RequestFactory()
+
+
+@patch(
+    "core.utils.CachedDirectionAction.get_directions"
+)  # replace with the actual module path
+class TestGetDirection:
+    def test_get_direction_for_carte(self, mock_get_directions, request_factory):
+        mock_get_directions.return_value = [{"code": "north"}, {"code": "south"}]
+        # Test when "carte" is in GET parameters
+        request = request_factory.get("/?carte")
+        assert utils.get_direction(request) is None
+
+    @override_settings(DEFAULT_ACTION_DIRECTION="north")
+    def test_get_direction_default_direction(
+        self, mock_get_directions, request_factory
+    ):
+        mock_get_directions.return_value = [{"code": "north"}, {"code": "south"}]
+
+        request = request_factory.get("/")
+        assert utils.get_direction(request) == "north"
+
+        request = request_factory.get("/?direction=fake")
+        assert utils.get_direction(request) == "north"
+
+    def test_get_direction_direction(self, mock_get_directions, request_factory):
+        mock_get_directions.return_value = [{"code": "north"}, {"code": "south"}]
+
+        request = request_factory.get("/?direction=north")
+        assert utils.get_direction(request) == "north"
+
+        request = request_factory.get("/?direction=south")
+        assert utils.get_direction(request) == "south"
