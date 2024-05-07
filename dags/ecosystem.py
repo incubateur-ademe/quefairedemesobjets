c49797f8069f03edd30aa79bef9900cd62f4cec3
Nicolas Oudard <nicolas@oudard.org>
2024-05-07 14:45:25 +0200

    Proposition de réorganisation des dags

--- dags/ecosystem.py
+++ dags/ecosystem.py
@@ -3,11 +3,10 @@ from importlib import import_module
 from pathlib import Path
 
 from airflow import DAG
-from airflow.operators.python import PythonOperator
 
 env = Path(__file__).parent.name
 utils = import_module(f"{env}.utils.utils")
-dag_eo_utils = import_module(f"{env}.utils.dag_eo_utils")
+eo_operators = import_module(f"{env}.utils.eo_operators")
 
 default_args = {
     "owner": "airflow",
@@ -18,34 +17,18 @@ default_args = {
     "retries": 1,
 }
 
-dag = DAG(
+with DAG(
     utils.get_dag_name(__file__, "ecosystem"),
     default_args=default_args,
     description=(
         "A pipeline to fetch, process, and load to validate data into postgresql"
         " for Ecosystem dataset"
     ),
-    schedule=None,
-)
-
-
-fetch_data_task = PythonOperator(
-    task_id="fetch_data_from_api",
-    python_callable=dag_eo_utils.fetch_data_from_api,
-    op_kwargs={"dataset": "donnees-eo-ecosystem"},
-    dag=dag,
-)
-
-load_data_task = PythonOperator(
-    task_id="load_data_from_postgresql",
-    python_callable=dag_eo_utils.load_data_from_postgresql,
-    dag=dag,
-)
-
-create_actors_task = PythonOperator(
-    task_id="create_actors",
-    python_callable=dag_eo_utils.create_actors,
-    op_kwargs={
+    params={
+        "endpoint": (
+            "https://data.pointsapport.ademe.fr/data-fair/api/v1/datasets/"
+            "donnees-eo-ecosystem/lines?size=10000"
+        ),
         "column_mapping": {
             "id_point_apport_ou_reparation": "identifiant_externe",
             "type_de_point_de_collecte": "acteur_type_id",
@@ -65,46 +48,23 @@ create_actors_task = PythonOperator(
             "longitudewgs84": "location",
             "latitudewgs84": "location",
         },
-        "column_to_drop": ["siret"],
+        "column_to_drop": [
+            "siret",
+        ],
     },
-    dag=dag,
-)
-
-create_proposition_services_task = PythonOperator(
-    task_id="create_proposition_services",
-    python_callable=dag_eo_utils.create_proposition_services,
-    dag=dag,
-)
-
-create_proposition_services_sous_categories_task = PythonOperator(
-    task_id="create_proposition_services_sous_categories",
-    python_callable=dag_eo_utils.create_proposition_services_sous_categories,
-    dag=dag,
-)
-
-write_data_task = PythonOperator(
-    task_id="write_data_to_validate_into_dagruns",
-    python_callable=dag_eo_utils.write_to_dagruns,
-    dag=dag,
-)
-
-serialize_to_json_task = PythonOperator(
-    task_id="serialize_actors_to_records",
-    python_callable=dag_eo_utils.serialize_to_json,
-    dag=dag,
-)
-
-create_labels_task = PythonOperator(
-    task_id="create_labels",
-    python_callable=dag_eo_utils.create_labels,
-    dag=dag,
-)
-
-(
-    [fetch_data_task, load_data_task]
-    >> create_actors_task
-    >> [create_proposition_services_task, create_labels_task]
-    >> create_proposition_services_sous_categories_task
-    >> serialize_to_json_task
-    >> write_data_task
-)
+    schedule=None,
+) as dag:
+    (
+        [
+            eo_operators.fetch_data_task(dag),
+            eo_operators.load_data_task(dag),
+        ]
+        >> eo_operators.create_actors_task(dag)
+        >> [
+            eo_operators.create_proposition_services_task(dag),
+            eo_operators.create_labels_task(dag),
+        ]
+        >> eo_operators.create_proposition_services_sous_categories_task(dag)
+        >> eo_operators.serialize_to_json_task(dag)
+        >> eo_operators.write_data_task(dag)
+    )  # type: ignore
---
+++ dags/utils/eo_operators.py
@@ -0,0 +1,73 @@
+from importlib import import_module
+from pathlib import Path
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+env = Path(__file__).parent.parent.name
+
+dag_eo_utils = import_module(f"{env}.utils.dag_eo_utils")
+
+
+def fetch_data_task(dag: DAG) -> PythonOperator:
+    return PythonOperator(
+        task_id="fetch_data_from_api",
+        python_callable=dag_eo_utils.fetch_data_from_api,
+        dag=dag,
+    )
+
+
+def load_data_task(dag: DAG) -> PythonOperator:
+    return PythonOperator(
+        task_id="load_data_from_postgresql",
+        python_callable=dag_eo_utils.load_data_from_postgresql,
+        dag=dag,
+    )
+
+
+def create_actors_task(dag: DAG) -> PythonOperator:
+    return PythonOperator(
+        task_id="create_actors",
+        python_callable=dag_eo_utils.create_actors,
+        dag=dag,
+    )
+
+
+def create_proposition_services_task(dag: DAG) -> PythonOperator:
+    return PythonOperator(
+        task_id="create_proposition_services",
+        python_callable=dag_eo_utils.create_proposition_services,
+        dag=dag,
+    )
+
+
+def create_proposition_services_sous_categories_task(dag: DAG) -> PythonOperator:
+    return PythonOperator(
+        task_id="create_proposition_services_sous_categories",
+        python_callable=dag_eo_utils.create_proposition_services_sous_categories,
+        dag=dag,
+    )
+
+
+def write_data_task(dag: DAG) -> PythonOperator:
+    return PythonOperator(
+        task_id="write_data_to_validate_into_dagruns",
+        python_callable=dag_eo_utils.write_to_dagruns,
+        dag=dag,
+    )
+
+
+def serialize_to_json_task(dag: DAG) -> PythonOperator:
+    return PythonOperator(
+        task_id="serialize_actors_to_records",
+        python_callable=dag_eo_utils.serialize_to_json,
+        dag=dag,
+    )
+
+
+def create_labels_task(dag: DAG) -> PythonOperator:
+    return PythonOperator(
+        task_id="create_labels",
+        python_callable=dag_eo_utils.create_labels,
+        dag=dag,
+    )
