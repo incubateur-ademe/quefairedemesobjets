b651f3b6e9c30c8715f95df1f5b6338b6618bdca
Hamza A <azelmat.hamza@gmail.com>
2024-06-19 09:02:30 +0300

    refactor the siret control dag

--- dags/annuaire_entreprise_checks.py
+++ dags/annuaire_entreprise_checks.py
@@ -8,13 +8,14 @@ from airflow import DAG
 from airflow.models.param import Param
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from fuzzywuzzy import fuzz
 
 env = Path(__file__).parent.name
 utils = import_module(f"{env}.utils.utils")
 dag_eo_utils = import_module(f"{env}.utils.dag_eo_utils")
 api_utils = import_module(f"{env}.utils.api_utils")
 mapping_utils = import_module(f"{env}.utils.mapping_utils")
+siret_control_utils = import_module(f"{env}.utils.siret_control_utils")
+
 pd.set_option("display.max_columns", None)
 
 default_args = {
@@ -43,27 +44,6 @@ def fetch_and_parse_data(**context):
     pg_hook = PostgresHook(postgres_conn_id=utils.get_db_conn_id(__file__))
     engine = pg_hook.get_sqlalchemy_engine()
     df_acteur = pd.read_sql("qfdmo_displayedacteur", engine)
-
-    df_acteur = df_acteur[
-        (df_acteur["siret"] != "None")
-        & (df_acteur["siret"].notna())
-        & (df_acteur["siret"] != "")
-        & (df_acteur["siret"] != " ")
-        & (df_acteur["siret"] != "ZZZ")
-        & (df_acteur["statut"] == "ACTIF")
-    ]
-
-    df_acteur = df_acteur[
-        (df_acteur["adresse"] != "")
-        & (df_acteur["adresse"].notnull())
-        & (df_acteur["ville"].notnull())
-        & (df_acteur["code_postal"].notnull())
-        & (df_acteur["adresse"] != "-")
-        & (df_acteur["adresse"].str.len() > 5)
-        & (df_acteur["code_postal"].str.len() == 5)
-        & (df_acteur["statut"] == "ACTIF")
-    ]
-
     df_acteur["full_adresse"] = (
         df_acteur["adresse"]
         .fillna("")
@@ -71,11 +51,43 @@ def fetch_and_parse_data(**context):
         .str.cat(df_acteur["ville"].fillna(""), sep=" ")
     )
 
-    df_acteur["full_adresse"] = df_acteur["full_adresse"].str.strip()
+    df_acteur = df_acteur[df_acteur["statut"] == "ACTIF"]
     if limit > 1:
         df_acteur = df_acteur.head(limit)
 
-    return df_acteur
+    good_siret_condition = (
+        df_acteur["siret"].notna()
+        & (df_acteur["siret"] != "")
+        & (df_acteur["siret"] != "None")
+        & (df_acteur["siret"] != " ")
+        & (df_acteur["siret"] != "ZZZ")
+    )
+    good_address_condition = (
+        df_acteur["adresse"].notna()
+        & (df_acteur["ville"].notnull())
+        & (df_acteur["adresse"] != "")
+        & (df_acteur["adresse"] != "-")
+        & (df_acteur["adresse"].str.len() > 5)
+        & (df_acteur["code_postal"].str.len() == 5)
+    )
+
+    df1 = df_acteur[good_siret_condition & good_address_condition]
+
+    df2 = df_acteur[~good_siret_condition & good_address_condition]
+    df3 = df_acteur[good_siret_condition & ~good_address_condition]
+
+    df4 = df_acteur[
+        ~df_acteur.index.isin(df1.index)
+        & ~df_acteur.index.isin(df2.index)
+        & ~df_acteur.index.isin(df3.index)
+    ]
+
+    return {
+        "ok_siret_ok_adresse": df1,
+        "nok_siret_ok_adresse": df2,
+        "ok_siret_nok_adresse": df3,
+        "nok_siret_nok_adresse": df4,
+    }
 
 
 def check_actor_with_adresse(**kwargs):
@@ -102,227 +114,6 @@ def check_actor_with_siret(**kwargs):
     return df_acteur[
         ["identifiant_unique", "siret", "nom", "statut", "ae_result", "full_adresse"]
     ]
-
-
-def combine_ae_result_dicts(row):
-    ae_result_siret = (
-        row["ae_result_siret"] if isinstance(row["ae_result_siret"], list) else []
-    )
-    ae_result_adresse = (
-        row["ae_result_adresse"] if isinstance(row["ae_result_adresse"], list) else []
-    )
-
-    siret_tuples = [tuple(sorted(d.items())) for d in ae_result_siret]
-    adresse_tuples = [tuple(sorted(d.items())) for d in ae_result_adresse]
-
-    unique_dicts = set(siret_tuples + adresse_tuples)
-
-    return [dict(t) for t in unique_dicts]
-
-
-def update_statut(row):
-    for result in row["ae_result"]:
-        if (
-            row["siret"] == result.get("siret_candidat")
-            and result["search_by_siret_candidat"]
-        ):
-            if result["etat_admin_candidat"] == "A":
-                return pd.Series(
-                    [
-                        "ACTIF",
-                        result["categorie_naf_candidat"],
-                        result["adresse_candidat"],
-                    ]
-                )
-            else:
-                return pd.Series(
-                    [
-                        "SUPPRIME",
-                        result["categorie_naf_candidat"],
-                        result["adresse_candidat"],
-                    ]
-                )
-    return pd.Series(["SUPPRIME", None, row["full_adresse"]])
-
-
-def set_cohort_id(row):
-    current_nom = row["nom"]
-    current_adresse_ae = row["ae_adresse"]
-    current_adresse_lvao = row["full_adresse"]
-    current_naf = row["categorie_naf"]
-    current_siret = row["siret"]
-    current_siren = current_siret[:9]
-    current_siret_size = len(current_siret.strip())
-    priorities = {
-        "ownership_transferred_matching_category_lvao_address": 11,
-        "ownership_transferred_matching_category_ae_address": 10,
-        "ownership_transferred_different_category": 9,
-        "ownership_transferred_different_names": 8,
-        "relocation_same_siren_matching_name_and_naf": 7,
-        "relocation_same_siren_matching_name_only": 6,
-        "relocation_same_siren_not_matching_name": 5,
-        "relocation": 4,
-        "closed_0_open_candidates_address_result_included": 3,
-        "closed_0_open_candidates": 2,
-        "closed": 1,
-    }
-
-    best_outcome = "closed"
-    highest_priority_level = 1
-    best_candidate_index = -1
-    total_nb_etablissements_ouverts = sum(
-        candidate.get("nombre_etablissements_ouverts", 0)
-        for candidate in row["ae_result"]
-    )
-
-    for index, candidate in enumerate(row["ae_result"]):
-        nom_match_strength = fuzz.ratio(candidate["nom_candidat"], current_nom)
-        adresse_lvao_match_ratio = fuzz.ratio(
-            candidate["adresse_candidat"], current_adresse_lvao
-        )
-        adresse_ae_match_ratio = fuzz.ratio(
-            candidate["adresse_candidat"], current_adresse_ae
-        )
-        candidate_siren = candidate["siret_candidat"][:9]
-        if (
-            adresse_lvao_match_ratio > 80
-            and candidate["categorie_naf_candidat"] == current_naf
-            and candidate["etat_admin_candidat"] == "A"
-            and nom_match_strength > 80
-        ):
-            best_outcome = "ownership_transferred_matching_category_lvao_address"
-            best_candidate_index = index
-            break
-        elif (
-            adresse_ae_match_ratio > 80
-            and candidate["categorie_naf_candidat"] == current_naf
-            and candidate["etat_admin_candidat"] == "A"
-            and nom_match_strength > 80
-            and priorities["ownership_transferred_matching_category_ae_address"]
-            > highest_priority_level
-        ):
-            best_outcome = "ownership_transferred_matching_category_ae_address"
-            best_candidate_index = index
-            break
-        elif (
-            (adresse_ae_match_ratio > 80 or adresse_lvao_match_ratio > 80)
-            and candidate["categorie_naf_candidat"] != current_naf
-            and candidate["etat_admin_candidat"] == "A"
-            and nom_match_strength > 80
-            and priorities["ownership_transferred_different_category"]
-            > highest_priority_level
-        ):
-            best_outcome = "ownership_transferred_different_category"
-            highest_priority_level = priorities[best_outcome]
-            best_candidate_index = index
-        elif (
-            (adresse_ae_match_ratio > 80 or adresse_lvao_match_ratio > 80)
-            and candidate["categorie_naf_candidat"] == current_naf
-            and candidate["etat_admin_candidat"] == "A"
-            and priorities["ownership_transferred_different_names"]
-            > highest_priority_level
-        ):
-            best_outcome = "ownership_transferred_different_names"
-            highest_priority_level = priorities[best_outcome]
-            best_candidate_index = index
-
-        elif (
-            candidate["nombre_etablissements_ouverts"] == 1
-            and current_siren == candidate_siren
-            and candidate["etat_admin_candidat"] == "A"
-            and nom_match_strength > 80
-            and candidate["categorie_naf_candidat"] == current_naf
-            and priorities["relocation_same_siren_matching_name_and_naf"]
-            > highest_priority_level
-        ):
-            best_outcome = "relocation_same_siren_matching_name_and_naf"
-            highest_priority_level = priorities[best_outcome]
-            best_candidate_index = index
-
-        elif (
-            candidate["nombre_etablissements_ouverts"] == 1
-            and current_siren == candidate_siren
-            and candidate["etat_admin_candidat"] == "A"
-            and nom_match_strength > 80
-            and priorities["relocation_same_siren_matching_name_only"]
-            > highest_priority_level
-        ):
-            best_outcome = "relocation_same_siren_matching_name_only"
-            highest_priority_level = priorities[best_outcome]
-            best_candidate_index = index
-
-        elif (
-            candidate["nombre_etablissements_ouverts"] == 1
-            and current_siren == candidate_siren
-            and candidate["etat_admin_candidat"] == "A"
-            and priorities["relocation_same_siren_not_matching_name"]
-            > highest_priority_level
-        ):
-            best_outcome = "relocation_same_siren_not_matching_name"
-            highest_priority_level = priorities[best_outcome]
-            best_candidate_index = index
-        elif (
-            candidate["adresse_candidat"] != current_adresse_ae
-            and candidate["adresse_candidat"] != current_adresse_lvao
-            and candidate["categorie_naf_candidat"] == current_naf
-            and candidate["etat_admin_candidat"] == "A"
-            and nom_match_strength > 80
-            and priorities["relocation"] > highest_priority_level
-        ):
-            best_outcome = "relocation"
-            highest_priority_level = priorities[best_outcome]
-            best_candidate_index = index
-
-        elif (
-            total_nb_etablissements_ouverts == 0
-            and current_siret_size == 14
-            and priorities["closed_0_open_candidates_address_result_included"]
-            > highest_priority_level
-        ):
-            best_outcome = "closed_0_open_candidates_address_result_included"
-            highest_priority_level = priorities[best_outcome]
-            best_candidate_index = index
-        elif (
-            candidate["nombre_etablissements_ouverts"] == 0
-            and candidate["search_by_siret_candidat"]
-            and current_siret_size == 14
-            and priorities["closed_0_open_candidates"] > highest_priority_level
-        ):
-            best_outcome = "closed_0_open_candidates"
-            highest_priority_level = priorities[best_outcome]
-            best_candidate_index = index
-
-    if best_candidate_index != -1:
-        row["ae_result"][best_candidate_index]["used_for_decision"] = True
-
-    return best_outcome
-
-
-def combine_actors(**kwargs):
-    df_acteur_with_siret = kwargs["ti"].xcom_pull(task_ids="check_with_siret")
-    df_acteur_with_adresse = kwargs["ti"].xcom_pull(task_ids="check_with_adresse")
-
-    df = pd.merge(
-        df_acteur_with_siret,
-        df_acteur_with_adresse,
-        on=["identifiant_unique", "nom", "statut", "siret", "full_adresse"],
-        how="inner",
-        suffixes=("_siret", "_adresse"),
-    )
-    cohort_dfs = {}
-
-    df["ae_result"] = df.apply(combine_ae_result_dicts, axis=1)
-    df[["statut", "categorie_naf", "ae_adresse"]] = df.apply(update_statut, axis=1)
-    df = df[df["statut"] == "SUPPRIME"]
-    if len(df) > 0:
-        df["cohort_id"] = df.apply(set_cohort_id, axis=1)
-    else:
-        return cohort_dfs
-
-    for cohort_id in df["cohort_id"].unique():
-        cohort_dfs[cohort_id] = df[df["cohort_id"] == cohort_id]
-
-    return cohort_dfs
 
 
 def enrich_row(row):
@@ -409,7 +200,7 @@ serialize_to_json_task = PythonOperator(
 
 combine_candidates = PythonOperator(
     task_id="combine_actors",
-    python_callable=combine_actors,
+    python_callable=siret_control_utils.combine_actors,
     dag=dag,
 )
 
