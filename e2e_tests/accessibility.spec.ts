bdc75d3eacecad28926b6044895d1c81dc4943fd
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-06-27 21:48:26 +0200

    wip

--- e2e_tests/accessibility.spec.ts
+++ e2e_tests/accessibility.spec.ts
@@ -1,15 +1,15 @@
 import { AxeBuilder } from "@axe-core/playwright"
-
 import { expect, test } from "@playwright/test"
 
 test("iFrame is WCAG compliant", async ({ page }) => {
   await page.goto(`http://localhost:8000/test_iframe`, { waitUntil: "networkidle" })
 
   const accessibilityScanResults = await new AxeBuilder({ page })
-    .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
+    .include("iframe") // restriction du scan à l'iframe uniquement
+    .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])  // TODO : trouver quelle règle se rapproche le plus du RGAA
     .analyze()
+
   expect(accessibilityScanResults.violations).toEqual([])
 
   await browser.close();
-
 })
