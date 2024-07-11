aec96cc9de00a8a2538a66a0426c749e0b1c1079
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-07-11 17:25:51 +0200

    remove useless change in tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -147,25 +147,24 @@ module.exports = {
             "yellow-moutarde": "#C3992A",
             "yellow-tournesol": "#cab300",
         },
+        spacing: {
+            // https://www.systeme-de-design.gouv.fr/elements-d-interface/fondamentaux-techniques/espacements
+            0: 0,
+            "1v": "0.25rem",
+            "1w": "0.5rem",
+            "3v": "0.75rem",
+            "2w": "1rem",
+            "3w": "1.5rem",
+            "4w": "2rem",
+            "5w": "2.5rem",
+            "6w": "3rem",
+            "7w": "3.5rem",
+            "8w": "4rem",
+            "9w": "4.5rem",
+            "12w": "6rem",
+            "15w": "7.5rem",
+        },
         extend: {
-            spacing: {
-                // https://www.systeme-de-design.gouv.fr/elements-d-interface/fondamentaux-techniques/espacements
-                0: 0,
-                "1v": "0.25rem",
-                "1w": "0.5rem",
-                "3v": "0.75rem",
-                "2w": "1rem",
-                "3w": "1.5rem",
-                "4w": "2rem",
-                "5w": "2.5rem",
-                "6w": "3rem",
-                "7w": "3.5rem",
-                "8w": "4rem",
-                "9w": "4.5rem",
-                "12w": "6rem",
-                "15w": "7.5rem",
-            },
-
             maxWidth: {
                 readable: "80ch",
                 "120w": "60rem",
