f0acc9c1e6e25a590a5162d597814def612fb0b7
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-07-15 10:41:13 +0200

    add root domain

--- static/to_compile/src/iframe.ts
+++ static/to_compile/src/iframe.ts
@@ -1,6 +1,6 @@
 window.addEventListener("message", (event) => {
   const domain = new URL(event.origin).hostname
-  if (event.data === "ademe" && (domain === 'localhost' || domain.endsWith("ademe.fr"))) {
+  if (event.data === "ademe" && (domain === 'localhost' || domain.endsWith(".ademe.fr"))) {
     document.querySelector("#logo")?.remove()
   }
 });
