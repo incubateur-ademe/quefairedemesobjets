49a7576ab3ce81b35ca15f99232d5efadc4ffa05
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-07-15 10:50:59 +0200

    add script to hide logo when origin is ademe.fr (#702)
    * add script to hide logo when origin is ademe.fr
    
    * add script file
    
    * add root domain

---
+++ static/to_compile/src/iframe.ts
@@ -0,0 +1,6 @@
+window.addEventListener("message", (event) => {
+  const domain = new URL(event.origin).hostname
+  if (event.data === "ademe" && (domain === 'localhost' || domain.endsWith(".ademe.fr"))) {
+    document.querySelector("#logo")?.remove()
+  }
+});
