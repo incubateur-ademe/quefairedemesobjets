ec9a758793589c698682420fd7dd87df357dc92d
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-07-18 13:15:18 +0200

    add qfdmo api

---
+++ qfdmo/api.py
@@ -0,0 +1,27 @@
+from typing import List
+from django.shortcuts import get_object_or_404
+from ninja import ModelSchema, Router
+from ninja.pagination import paginate
+
+from qfdmo.models import ActeurStatus, DisplayedActeur
+
+router = Router()
+
+
+class ActeurSchema(ModelSchema):
+    class Meta:
+        model = DisplayedActeur
+        fields = ["nom", "nom_commercial"]
+
+
+@router.get("/acteurs", response=List[ActeurSchema])
+@paginate
+def acteurs(request):
+    return DisplayedActeur.objects.filter(
+        statut=ActeurStatus.ACTIF,
+    )
+
+
+@router.get("/acteur", response=ActeurSchema)
+def acteur(request, identifiant_unique: str):
+    return get_object_or_404(DisplayedActeur, pk=id, statut=ActeurStatus.ACTIF)
