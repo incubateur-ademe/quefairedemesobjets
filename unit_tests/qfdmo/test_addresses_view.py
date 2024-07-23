6e6f4ba4a2b3926337173659c5cf825720435496
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-07-23 10:20:03 +0200

    Fix regression in adresse / carte filters (#720)

--- unit_tests/qfdmo/test_addresses_view.py
+++ unit_tests/qfdmo/test_addresses_view.py
@@ -1,17 +1,7 @@
-from django.contrib.gis.geos import Point
-from unittest.mock import MagicMock
 import pytest
 from django.http import HttpRequest
 
-from qfdmo.models.acteur import ActeurStatus
 from qfdmo.views.adresses import AddressesView
-from unit_tests.core.test_utils import query_dict_from
-from unit_tests.qfdmo.acteur_factory import (
-    DisplayedActeurFactory,
-    DisplayedPropositionServiceFactory,
-    LabelQualiteFactory,
-)
-from unit_tests.qfdmo.action_factory import ActionFactory
 
 
 class TestAdessesViewGetActionList:
@@ -70,75 +60,3 @@ class TestAdessesViewGetActionList:
         assert [
             action["libelle"] for action in adresses_view.get_action_list()
         ] == action_list
-
-
-@pytest.fixture
-def adresses_view():
-    reparacteur = LabelQualiteFactory(code="reparacteur")
-    action = ActionFactory()
-
-    displayed_acteur = DisplayedActeurFactory(
-        exclusivite_de_reprisereparation=True,
-        location=Point(1, 1),
-        statut=ActeurStatus.ACTIF,
-    )
-    display_proposition_service = DisplayedPropositionServiceFactory(action=action)
-    displayed_acteur.labels.add(reparacteur)
-    displayed_acteur.proposition_services.add(display_proposition_service)
-
-    adresses_view = AddressesView()
-    adresses_view._get_reparer_action_id = MagicMock(return_value=action.id)
-    adresses_view._get_selected_action_ids = MagicMock(return_value=[action.id])
-    return adresses_view
-
-
-@pytest.mark.django_db
-class TestExclusiviteReparation:
-    def test_pas_action_reparer_exclut_acteurs_avec_exclusivite(self, adresses_view):
-        request = HttpRequest()
-        request.GET = query_dict_from(
-            {
-                "action_list": ["preter"],
-                "latitude": [1],
-                "longitude": [1],
-                "pas_exclusivite_reparation": ["true"],
-            }
-        )
-        adresses_view.request = request
-        context = adresses_view.get_context_data()
-
-        assert context["acteurs"].count() == 0
-
-    def test_action_reparer_exclut_par_defaut_acteurs_avec_exclusivite(
-        self, adresses_view
-    ):
-        request = HttpRequest()
-        request.GET = query_dict_from(
-            {
-                "action_list": ["preter|reparer"],
-                "latitude": [1],
-                "longitude": [1],
-                "pas_exclusivite_reparation": ["true"],
-            }
-        )
-        adresses_view.request = request
-        context = adresses_view.get_context_data()
-
-        assert context["acteurs"].count() == 0
-
-    def test_action_reparer_et_exclusivite_inclut_acteurs_avec_exclusivite(
-        self, adresses_view
-    ):
-        request = HttpRequest()
-        request.GET = query_dict_from(
-            {
-                "action_list": ["preter|reparer"],
-                "latitude": [1],
-                "longitude": [1],
-                "pas_exclusivite_reparation": ["false"],
-            }
-        )
-        adresses_view.request = request
-        context = adresses_view.get_context_data()
-
-        assert context["acteurs"].count() == 1
