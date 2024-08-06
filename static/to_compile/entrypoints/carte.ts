85de3cc16022244c87a8a4131cafeaee2fc680d0
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2024-08-06 15:52:14 +0200

    Extract functions to don't call the iframe builder twice (#754)
    extract functions to don't call the iframe builder twice

--- static/to_compile/entrypoints/carte.ts
+++ static/to_compile/entrypoints/carte.ts
@@ -1,11 +1,16 @@
-import { buildAndInsertIframeFrom, getIframeAttributesAndExtra } from "./iframe"
-
+import {
+    buildAndInsertIframeFrom,
+    getIframeAttributesAndExtra,
+} from "./iframe_functions"
 
 const setupIframe = () => {
-  // Add iFrame just after the script tag
-  const scriptTag = document.currentScript as HTMLScriptElement
-  const [iframeAttributes, iframeExtraAttributes] = getIframeAttributesAndExtra(["carte", "1"], scriptTag)
-  buildAndInsertIframeFrom(iframeAttributes, iframeExtraAttributes, scriptTag)
+    // Add iFrame just after the script tag
+    const scriptTag = document.currentScript as HTMLScriptElement
+    const [iframeAttributes, iframeExtraAttributes] = getIframeAttributesAndExtra(
+        ["carte", "1"],
+        scriptTag,
+    )
+    buildAndInsertIframeFrom(iframeAttributes, iframeExtraAttributes, scriptTag)
 }
 
 //addIframeResizer()
