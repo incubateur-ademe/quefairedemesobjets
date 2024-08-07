5a3af795622503a7483474ec4cddfd971b7eb375
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-08-07 17:15:46 +0200

    Better handle 404 on qfdmd products (#758)

--- qfdmd/api.py
+++ qfdmd/api.py
@@ -2,7 +2,6 @@ from django.shortcuts import get_object_or_404
 from ninja import ModelSchema, Router
 
 from qfdmo.models.categorie_objet import SousCategorieObjet
-from .models import Produit
 
 router = Router()
 
@@ -17,23 +16,11 @@ class SousCategorieSchema(ModelSchema):
 
 @router.get("/produit", response=SousCategorieSchema)
 def sous_categorie_from_product(request, id: str):
-    try:
-        # L'API Data Ademe retourne un identifiant de la forme 180_0, 180_1 etc
-        # pour les synonymes du produit 180.
-        # On retourne donc la même sous_catégorie pour tous les synonymes, celle-ci
-        # correspondant à la partie avant l'underscore
-        id = id.split("_")[0]
-        return (
-            get_object_or_404(
-                Produit,
-                id=id,
-            )
-            .sous_categories.filter(afficher_carte=True)
-            .first()
-        )
-
-    except (
-        AttributeError,
-        Produit.DoesNotExist,
-    ):
-        return None
+    # L'API Data Ademe retourne un identifiant de la forme 180_0, 180_1 etc
+    # pour les synonymes du produit 180.
+    # On retourne donc la même sous_catégorie pour tous les synonymes, celle-ci
+    # correspondant à la partie avant l'underscore
+    id = id.split("_")[0]
+    return get_object_or_404(
+        SousCategorieObjet, qfdmd_produits__id__in=[id], afficher_carte=True
+    )
