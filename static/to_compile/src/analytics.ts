3fa8a607d9c56a3ad6b4ec4efcee11f0bd2a5b0d
Nicolas Oudard <nicolas@oudard.org>
2024-08-12 17:45:41 +0200

    deactivate posthog cookie and localhost

--- static/to_compile/src/analytics.ts
+++ static/to_compile/src/analytics.ts
@@ -3,6 +3,7 @@ import posthog from "posthog-js"
 if (process.env.NODE_ENV !== "development") {
     posthog.init("phc_SGbYOrenShCMKJOQYyl62se9ZqCHntjTlzgKNhrKnzm", {
         api_host: "https://eu.posthog.com",
+        persistence: "memory",
     })
 }
 
