8e377f4758c8356ce23a27411549c784afb114fb
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-08-14 16:01:52 +0200

    oopsy

--- static/to_compile/src/analytics_controller.ts
+++ static/to_compile/src/analytics_controller.ts
@@ -3,7 +3,7 @@ import { InteractionType as PosthogUIInteractionType, PosthogEventType } from ".
 import posthog from "posthog-js"
 
 export default class extends Controller<HTMLElement> {
-    // Sert principalement à type les appels à la   this.capturede Posthog.
+    // Sert principalement à type les appels à la méthode capture de Posthog.
     // Ça évite d'appeler un événement indéfini, ce qui peut rapidement polluer
     // les données stockées côtés PostHog.
     capture(posthogEvent: PosthogEventType, details: object) {
