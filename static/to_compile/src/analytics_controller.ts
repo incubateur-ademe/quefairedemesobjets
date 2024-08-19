e8ab5fbb9974442cec1868355b9e151182d3bef1
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-08-19 15:50:43 +0200

    Track product interactions in posthog (#765)

---
+++ static/to_compile/src/analytics_controller.ts
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+import { InteractionType as PosthogUIInteractionType, PosthogEventType } from "./types"
+import posthog from "posthog-js"
+
+export default class extends Controller<HTMLElement> {
+    // Sert principalement à type les appels à la méthode capture de Posthog.
+    // Ça évite d'appeler un événement indéfini, ce qui peut rapidement polluer
+    // les données stockées côtés PostHog.
+    capture(posthogEvent: PosthogEventType, details: object) {
+        posthog.capture(posthogEvent, details)
+    }
+
+    captureUIInteraction(UIInteractionType: PosthogUIInteractionType) {
+        this.capture("ui_interaction", {
+            ui_interaction_type: UIInteractionType,
+        })
+    }
+
+    captureInteractionWithSolutionDetails() {
+        this.captureUIInteraction("solution_details")
+    }
+
+    captureInteractionWithMap() {
+        this.captureUIInteraction("map")
+    }
+}
