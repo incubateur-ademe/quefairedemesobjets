3a67efb9135de8fe78aef76257ffd87f61cd14f7
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2024-08-22 17:40:07 +0200

    Suppression de noopener sur les liens avec target = _blank (#811)
    Suppression de noopener sur les liens avec target = _blank et alignement des versions de navigateur supporté

--- static/to_compile/src/browser_check.ts
+++ static/to_compile/src/browser_check.ts
@@ -4,11 +4,11 @@ function isBrowserSupported(): boolean {
     const browser = Bowser.getParser(window.navigator.userAgent)
     let isValidBrowser = browser.satisfies({
         // declare browsers and versions you want to support
-        chrome: ">85",
-        firefox: ">79",
+        chrome: ">=88",
+        firefox: ">=79",
         safari: ">=14",
-        opera: ">70",
-        edge: ">84",
+        opera: ">=74",
+        edge: ">=88",
         ie: "none",
     })
     if (isValidBrowser === undefined) {
