3db19a98f4c464252e90e13fe3d07904567edb85
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-09-12 17:06:43 +0200

    Add test for other pages

--- e2e_tests/advanced_filters.spec.ts
+++ e2e_tests/advanced_filters.spec.ts
@@ -1,12 +1,30 @@
 import { expect, test } from "@playwright/test"
 
-test("Filtres avancés s'ouvrent et se ferment", async ({ page }) => {
-    await page.goto(`http://localhost:8000/`, {
+async function testAdvancedFilters(page) {
+  await page.locator("#djHideToolBarButton").click()
+  await page.locator("button[data-testid=advanced-filters]").click()
+  await expect(page.locator("[data-testid=advanced-filters-modal] .fr-modal__content h4")).toBeInViewport()
+  await page.locator("[data-testid=advanced-filters-modal] .fr-modal__header button").click()
+  await expect(page.locator("[data-testid=advanced-filters-modal] .fr-modal__content h4")).toBeHidden()
+}
+
+test("Filtres avancés s'ouvrent et se ferment en mode iframe", async ({ page }) => {
+    await page.goto(`http://localhost:8000/?iframe`, {
         waitUntil: "networkidle",
     })
-    await page.locator("#djHideToolBarButton").click()
-    await page.locator("button[data-testid=advanced-filters]").click()
-    await expect(page.locator("[data-testid=advanced-filters-modal] .fr-modal__content h4")).toBeInViewport()
-    await page.locator("[data-testid=advanced-filters-modal] .fr-modal__header button").click()
-    await expect(page.locator("[data-testid=advanced-filters-modal] .fr-modal__content h4")).toBeHidden()
+    await testAdvancedFilters(page)
+})
+
+test("Filtres avancés s'ouvrent et se ferment en mode carte", async ({ page }) => {
+    await page.goto(`http://localhost:8000/?carte`, {
+        waitUntil: "networkidle",
+    })
+    await testAdvancedFilters(page)
+})
+
+test("Filtres avancés s'ouvrent et se ferment en mode formulaire", async ({ page }) => {
+    await page.goto(`http://localhost:8000/?carte`, {
+        waitUntil: "networkidle",
+    })
+    await testAdvancedFilters(page)
 })
