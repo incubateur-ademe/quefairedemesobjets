74c989a073c52e5cfb6fdd4089b5c5ad9ac8926d
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-09-16 12:37:38 +0200

    oopsy...

--- playwright.config.ts
+++ playwright.config.ts
@@ -69,7 +69,7 @@ export default defineConfig({
         /* Test against branded browsers. */
         {
             name: "Microsoft Edge",
-            grepInvert: /Desktop/,
+            grepInvert: /Mobile/,
             use: { ...devices["Desktop Edge"], channel: "msedge" },
         },
     ],
