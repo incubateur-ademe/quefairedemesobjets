7dbdb2665b0294c1d5368830d402a0295870c842
Nicolas Oudard <nicolas@oudard.org>
2024-09-24 23:32:46 +0200

    sync

--- sync_dags.sh
+++ sync_dags.sh
@@ -4,6 +4,6 @@ echo "Syncing dags from s3 to ./dags"
 
 AWS_SECRET_ACCESS_KEY=$CELLAR_ADDON_KEY_SECRET AWS_ACCESS_KEY_ID=$CELLAR_ADDON_KEY_ID\
  aws --endpoint-url https://$CELLAR_ADDON_HOST s3 sync --delete --exclude .env.template --exclude .env\
-  --exclude download_dags.py s3://prod-qfdmo-airflow-dags $AIRFLOW__CORE__DAGS_FOLDER
+  --exclude download_dags.py s3://$CELLAR_ADDON_BUCKET $AIRFLOW__CORE__DAGS_FOLDER
 
 exit 0
