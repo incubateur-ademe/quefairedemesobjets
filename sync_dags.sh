c811be9f9c19bedb6d8ffd459c37ea3a0df72c39
Nicolas Oudard <nicolas@oudard.org>
2024-09-24 21:54:33 +0200

    update sync dags

--- sync_dags.sh
+++ sync_dags.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
-# synchroniser les dags du s3 vers le airflow
 
-
-echo $AWS_ACCESS_KEY_ID
 echo "Syncing dags from s3 to ./dags"
-AWS_SECRET_ACCESS_KEY=$CELLAR_ADDON_KEY_SECRET AWS_ACCESS_KEY_ID=$CELLAR_ADDON_KEY_ID aws --debug --endpoint-url https://$CELLAR_ADDON_HOST s3 sync --delete --exclude .env --exclude download_dags.py s3://prod-qfdmo-airflow-dags ./dags
 
+AWS_SECRET_ACCESS_KEY=$CELLAR_ADDON_KEY_SECRET AWS_ACCESS_KEY_ID=$CELLAR_ADDON_KEY_ID\
+ aws --endpoint-url https://$CELLAR_ADDON_HOST s3 sync --delete --exclude .env.template --exclude .env\
+  --exclude download_dags.py s3://prod-qfdmo-airflow-dags $AIRFLOW__CORE__DAGS_FOLDER
 
-
+exit 0
