74d4255d69c7ebb5ea70a071f29495ab77841054
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-10-16 12:17:04 +0200

    Update core/logging.py

--- core/logging.py
+++ core/logging.py
@@ -4,7 +4,7 @@ from logging import Filter
 
 def log_pretty(dictionnary, sort_dicts=False):
     pretty_printer = pprint.PrettyPrinter(indent=2, sort_dicts=sort_dicts)
-    """Beautifully log dict as json in the console
+    """Beautifully log dict with line breaks and indentation in the console
 
     This can be used in a logger.info(log_pretty(some_dict))
 
