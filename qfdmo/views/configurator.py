c97baae34507ed492a610a8b3b7c0406220cd5dd
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-10-16 11:01:14 +0200

    Update qfdmo/views/configurator.py

--- qfdmo/views/configurator.py
+++ qfdmo/views/configurator.py
@@ -6,7 +6,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.edit import FormView
-from typing_extensions import Any
+from typing import Any
 
 from qfdmo.forms import AdvancedConfiguratorForm, ConfiguratorForm
 
