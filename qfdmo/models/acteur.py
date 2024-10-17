e57ed6f838c8a71c4d0b78c1a351cba062c47522
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-10-17 09:34:32 +0200

    Update acteur.py

--- qfdmo/models/acteur.py
+++ qfdmo/models/acteur.py
@@ -15,12 +15,7 @@ from unidecode import unidecode
 
 from qfdmo.models.action import Action, CachedDirectionAction
 from qfdmo.models.categorie_objet import SousCategorieObjet
-from qfdmo.models.utils import (
-    CodeAsNaturalKeyModel,
-    NomAsNaturalKeyManager,
-    NomAsNaturalKeyModel,
-)
-
+from qfdmo.models.utils import CodeAsNaturalKeyModel
 
 class ActeurService(CodeAsNaturalKeyModel):
     class Meta:
