ef255a65ca17de8253e5c1be90c5495a3d498af0
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-10-17 13:13:10 +0200

    Remove useless code

--- static/to_compile/src/icon_marker.ts
+++ static/to_compile/src/icon_marker.ts
@@ -1,12 +1,6 @@
 import L from "leaflet"
 import "leaflet-extra-markers/dist/js/leaflet.extra-markers.min.js"
 
-export const homeIconMarker = L.ExtraMarkers.icon({
-    icon: "fr-icon-map-pin-2-fill",
-    markerColor: "#E3E3FD",
-    prefix: "qfdmo-icon",
-})
-
 export const defaultMarker = L.ExtraMarkers.icon({
     icon: "fr-icon-checkbox-circle-line",
     markerColor: "#000",
