a1a124ba451210939a3408666a8c3ee62bf7f6c2
Nicolas Oudard <nicolas@oudard.org>
2024-10-17 17:23:15 +0200

    Revert "add new pinpoints (#941)"
    This reverts commit a41084e2b810eb73ac515966b9e59238ed13bac4.

--- static/to_compile/src/types.ts
+++ static/to_compile/src/types.ts
@@ -7,18 +7,24 @@ export interface ActorLocation {
     coordinates: number[]
 }
 
-export interface DisplayedActeur {
-  identifiant_unique: string
-  icon: string
-  couleur: string
-  location: ActorLocation
-  bonus: boolean
+export class Actor {
+    identifiant_unique: string
+    location: ActorLocation
+    icon: string
+    couleur: string
+
+    constructor(actor_fields: object) {
+        this.identifiant_unique = actor_fields["identifiant_unique"]
+        this.location = actor_fields["location"]
+        this.icon = actor_fields["icon"]
+        this.couleur = actor_fields["couleur"]
+    }
 }
 
 export class SSCatObject {
     label: string
     sub_label: string
-    identifier: number
+    identifier: int
 }
 
 export type InteractionType = "map" | "solution_details"
