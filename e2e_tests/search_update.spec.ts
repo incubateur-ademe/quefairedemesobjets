dbf18f99b0de59901d46519594cd4ec32526c834
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-10-17 14:11:07 +0200

    make exception for formulaire

--- e2e_tests/search_update.spec.ts
+++ e2e_tests/search_update.spec.ts
@@ -18,7 +18,7 @@ test("Recherche et modification d'une recherche", async ({ page }) => {
     await page.locator("input#id_adresse").fill("10 rue de la paix")
     await expect (page.locator("#id_adresseautocomplete-list.autocomplete-items div:nth-of-type(2)")).toBeInViewport()
     await page.locator("#id_adresseautocomplete-list.autocomplete-items div:nth-of-type(2)").click()
-    // await page.locator("button[data-testid=rechercher-adresses-submit]").click()
+    await page.locator("button[data-testid=rechercher-adresses-submit]").click()
 
     await expect (page.locator("[data-search-solution-form-target=headerAddressPanel]")).toBeInViewport()
     await page.locator("button[data-testid=modifier-recherche]").click()
