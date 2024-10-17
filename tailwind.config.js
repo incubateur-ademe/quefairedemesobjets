f59a8b367fd4f20337212a9d9a35e4639674ce0d
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2024-10-17 17:55:10 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     "templates/**/*html",
     "static/to_compile/**/*{j,t}s",
     "static/to_compile/**/*svg",
-    "./**/forms.py
+    "./**/forms.py",
   ],
   prefix: "qfdmo-",
   corePlugins: {
