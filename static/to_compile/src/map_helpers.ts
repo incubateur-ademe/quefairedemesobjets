70ba50112d01b88316eca1d629a93b5d446c90aa
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-10-21 15:35:08 +0200

    Home pinpoint update (#946)

---
+++ static/to_compile/src/map_helpers.ts
@@ -0,0 +1,15 @@
+/**
+This file holds constants and functions that are used both
+by Stimulus controllers and Leaflet instance.
+
+It was created / use mainly to prevent errors when running
+tests.
+*/
+
+export const ACTIVE_PINPOINT_CLASSNAME = "active-pinpoint"
+
+export function clearActivePinpoints() {
+  document.querySelectorAll(`.${ACTIVE_PINPOINT_CLASSNAME}`).forEach((element) => {
+    element.classList.remove(ACTIVE_PINPOINT_CLASSNAME)
+  })
+}
