b1519caae6b9313b8accf1fe27016491b4a04046
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-11-04 10:52:30 +0100

    Fix e2e tests / a11y

--- e2e_tests/accessibility.spec.ts
+++ e2e_tests/accessibility.spec.ts
@@ -1,8 +1,19 @@
 import { AxeBuilder } from "@axe-core/playwright"
 import { expect, test } from "@playwright/test"
 
-test("iFrame is WCAG compliant", async ({ page }) => {
+test("formulaire iFrame is WCAG compliant", async ({ page }) => {
   await page.goto(`http://localhost:8000/test_iframe`, { waitUntil: "networkidle" })
+
+  const accessibilityScanResults = await new AxeBuilder({ page })
+    .include("iframe") // restriction du scan à l'iframe uniquement
+    .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])  // TODO : trouver quelle règle se rapproche le plus du RGAA
+    .analyze()
+
+  expect(accessibilityScanResults.violations).toEqual([])
+})
+
+test("carte iFrame is WCAG compliant", async ({ page }) => {
+  await page.goto(`http://localhost:8000/test_iframe?carte`, { waitUntil: "networkidle" })
 
   const accessibilityScanResults = await new AxeBuilder({ page })
     .include("iframe") // restriction du scan à l'iframe uniquement
