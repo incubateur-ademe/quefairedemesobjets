da6ed5b220036f8ce24e7ffcb2b750963003f245
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-11-07 20:25:39 +0100

    Remove useless context processor

--- core/context_processors.py
+++ core/context_processors.py
@@ -6,6 +6,4 @@ def environment(request):
         "ENVIRONMENT": settings.ENVIRONMENT,
         "DEBUG": settings.DEBUG,
         "is_embedded": "carte" in request.GET or "iframe" in request.GET,
-        "is_carte": "carte" in request.GET,  # TODO: Voir pour gérer ca autrement,
-        # je m'en sers pour déclencher la couleur dans les tags de la barre latérale
     }
