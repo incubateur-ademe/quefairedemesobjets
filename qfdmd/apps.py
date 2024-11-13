842f18b66b4a3cee11ff3b3925a50cbc57517a0f
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-11-13 15:32:33 +0100

    Add verbose name for django apps

--- qfdmd/apps.py
+++ qfdmd/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class QfdmoConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
+    verbose_name = "Que faire de mes déchets"
     name = "qfdmd"
--- qfdmo/apps.py
+++ qfdmo/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class QfdmoConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
+    verbose_name = "Que faire de mes objets"
     name = "qfdmo"
