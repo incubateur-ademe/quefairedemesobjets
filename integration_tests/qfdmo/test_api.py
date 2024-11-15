8bf15fcabb54b60259c39a8b26eafda5660bc7b0
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-11-15 11:37:09 +0100

    Update tests

--- integration_tests/qfdmo/test_api.py
+++ integration_tests/qfdmo/test_api.py
@@ -1,4 +1,5 @@
 import pytest
+from django.contrib.gis.geos import Point
 
 from qfdmo.models.acteur import ActeurStatus
 from unit_tests.qfdmo.acteur_factory import DisplayedActeurFactory
@@ -25,21 +26,24 @@ def test_get_groupe_actions(client):
 @pytest.mark.django_db
 def test_get_acteurs(client):
     """Test the /acteurs endpoint with filters"""
+    DisplayedActeurFactory(
+        pk="UN-ACTEUR", location=Point(2.3, 48.86), statut=ActeurStatus.ACTIF
+    )
     params = {
         "latitude": 48.86,
         "longitude": 2.3,
         "rayon": 5,
-        "types": [1, 2],
-        "services": [3],
-        "actions": [4],
     }
     response = client.get(f"{BASE_URL}/acteurs", params=params)
     assert response.status_code == 200
     data = response.json()
     assert isinstance(data["items"], list)
-    if items := data["items"] and data["count"] > 0:
-        assert "nom" in items[0]
-        assert "adresse" in items[0]
+    assert data["count"] > 0
+
+    returned_acteur = data["items"][0]
+    assert "nom" in returned_acteur
+    assert "adresse" in returned_acteur
+    assert returned_acteur["identifiant_unique"] == "UN-ACTEUR"
 
 
 @pytest.mark.django_db
@@ -49,9 +53,10 @@ def test_get_acteurs_types(client):
     assert response.status_code == 200
     data = response.json()
     assert isinstance(data, list)
-    if data:
-        assert "id" in data[0]
-        assert "libelle" in data[0]
+    assert len(data) > 0
+    assert "code" in data[0]
+    assert "id" in data[0]
+    assert "libelle" in data[0]
 
 
 @pytest.mark.django_db
@@ -61,9 +66,9 @@ def test_get_acteurs_services(client):
     assert response.status_code == 200
     data = response.json()
     assert isinstance(data, list)
-    if data:
-        assert "id" in data[0]
-        assert "libelle" in data[0]
+    assert "code" in data[0]
+    assert "id" in data[0]
+    assert "libelle" in data[0]
 
 
 @pytest.mark.django_db
@@ -77,6 +82,8 @@ def test_get_acteur_by_identifiant(client):
     assert response.status_code == 200
     data = response.json()
     assert "nom" in data
+    assert "nom_commercial" in data
+    assert "siret" in data
     assert "adresse" in data
     assert data.get("identifiant_unique") == identifiant_unique
 
