5e8349fd74eab5394b49bd7dbf4aa965a33fd20f
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-11-19 21:44:29 +0100

    Do not enable sites framework for now

--- core/settings.py
+++ core/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.sitemaps",
-    "django.contrib.sites",
     "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
     "django.contrib.gis",
