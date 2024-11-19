0148db44de36f00098e9102afa20d69e7f29797f
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-11-19 14:54:52 +0100

    Add export in django admin for synonymes

--- qfdmd/admin.py
+++ qfdmd/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from qfdmd.models import Lien, Produit, Synonyme
 from qfdmo.admin.acteur import NotEditableInlineMixin
+
+
+class SynonymeResource(resources.ModelResource):
+    class Meta:
+        model = Synonyme
 
 
 class SynonymeInline(admin.StackedInline):
@@ -32,7 +39,8 @@ class LienAdmin(NotEditableInlineMixin, admin.ModelAdmin):
     inlines = [ProduitInline]
 
 
-class SynonymeAdmin(NotEditableInlineMixin, admin.ModelAdmin):
+class SynonymeAdmin(NotEditableInlineMixin, ImportExportModelAdmin, admin.ModelAdmin):
+    resource_classes = [SynonymeResource]
     list_display = ("nom", "produit")
 
 
