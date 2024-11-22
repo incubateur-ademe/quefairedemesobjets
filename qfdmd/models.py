8633462ca1765b5580442657158485000ec036c2
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-11-22 14:31:39 +0100

    Assistant v2 - première itération (#1022)

--- qfdmd/models.py
+++ qfdmd/models.py
@@ -1,4 +1,7 @@
 from django.contrib.gis.db import models
+from django.urls.base import reverse
+from django.utils.functional import cached_property
+from django_extensions.db.fields import AutoSlugField
 
 
 class Produit(models.Model):
@@ -16,23 +19,49 @@ class Produit(models.Model):
     )
     synonymes_existants = models.TextField(blank=True, help_text="Synonymes existants")
     code = models.CharField(blank=True, help_text="Code")
-
     bdd = models.CharField(blank=True, help_text="Bdd")
-
     comment_les_eviter = models.TextField(blank=True, help_text="Comment les éviter ?")
     qu_est_ce_que_j_en_fais = models.TextField(
         blank=True, help_text="Qu'est-ce que j'en fais ?"
     )
-
     que_va_t_il_devenir = models.TextField(
         blank=True, help_text="Que va-t-il devenir ?"
     )
     nom_eco_organisme = models.TextField(blank=True, help_text="Nom de l’éco-organisme")
     filieres_rep = models.TextField(blank=True, help_text="Filière(s) REP concernée(s)")
     slug = models.CharField(blank=True, help_text="Slug - ne pas modifier")
+    picto = models.FileField(upload_to="pictos", blank=True, null=True)
 
     def __str__(self):
         return f"{self.id} - {self.nom}"
+
+    @cached_property
+    def sous_categorie_with_carte_display(self):
+        return self.sous_categories.filter(afficher_carte=True).first()
+
+    @cached_property
+    def content_display(self) -> list[dict[str, str]]:
+        return [
+            item
+            for item in [
+                {
+                    "id": "Comment mieux consommer",
+                    "title": "Comment mieux consommer",
+                    "content": self.comment_les_eviter,
+                },
+                {
+                    "id": "Que va-t-il devenir ?",
+                    "title": "Que va-t-il devenir ?",
+                    "content": self.que_va_t_il_devenir,
+                },
+                {
+                    "id": "En savoir plus",
+                    "title": "En savoir plus",
+                    "content": self.que_va_t_il_devenir,
+                },
+            ]
+            if item["content"]
+        ]
 
 
 class Lien(models.Model):
@@ -48,10 +77,25 @@ class Lien(models.Model):
 
 
 class Synonyme(models.Model):
+    slug = AutoSlugField(populate_from=["nom"])
     nom = models.CharField(blank=True, unique=True, help_text="Nom du produit")
     produit = models.ForeignKey(
         Produit, related_name="synonymes", on_delete=models.CASCADE
     )
 
-    def __str__(self):
+    @property
+    def url(self) -> str:
+        return self.get_absolute_url()
+
+    def get_absolute_url(self) -> str:
+        return reverse("qfdmd:synonyme-detail", args=[self.slug])
+
+    def __str__(self) -> str:
         return self.nom
+
+
+class Suggestion(models.Model):
+    produit = models.OneToOneField(Synonyme, primary_key=True, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return str(self.produit)
---
+++ qfdmd/views.py
@@ -0,0 +1,59 @@
+import logging
+from typing import Any
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+
+from qfdmd.forms import SearchForm
+from qfdmd.models import Suggestion, Synonyme
+
+logger = logging.getLogger(__name__)
+
+
+def generate_iframe_script() -> str:
+    return '<script id="datagir_dechets" '
+    'src="https://quefairedemesdechets.ademe.fr/iframe.js" '
+    'data-search="?theme=default"></script>'
+
+
+SEARCH_VIEW_TEMPLATE_NAME = "components/search/view.html"
+
+
+def search_view(request) -> HttpResponse:
+    form = SearchForm(request.GET)
+    context = {}
+    template_name = SEARCH_VIEW_TEMPLATE_NAME
+
+    if form.is_valid():
+        form.search()
+        context.update(search_form=form)
+
+    return render(request, template_name, context=context)
+
+
+class BaseView:
+    """Base view that provides templates used on all pages.
+    TODO: this could be moved to a context processor"""
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context.update(
+            search_form=SearchForm(),
+            search_view_template_name=SEARCH_VIEW_TEMPLATE_NAME,
+        )
+        return context
+
+
+class HomeView(BaseView, ListView):
+    template_name = "qfdmd/home.html"
+    model = Suggestion
+
+
+class SynonymeDetailView(BaseView, DetailView):
+    model = Synonyme
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context.update(iframe_script=generate_iframe_script())
+        return context
