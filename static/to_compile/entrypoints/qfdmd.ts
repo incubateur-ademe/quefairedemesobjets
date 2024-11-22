212a5d8873f1f39890023462e5217ed5a901dd9d
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-11-22 13:58:57 +0100

    Add comments

--- static/to_compile/entrypoints/qfdmd.ts
+++ static/to_compile/entrypoints/qfdmd.ts
@@ -6,6 +6,7 @@ import SearchController from "../js/controllers/assistant/search"
 window.stimulus = Application.start()
 stimulus.register("search", SearchController)
 
+// TODO: remove once the map will be loaded using a turbo frame
 window.addEventListener("load", () => {
   const iframe = document.querySelector("#ou-l-apporter iframe")
   iframe.contentWindow.postMessage("ademe", "*");
