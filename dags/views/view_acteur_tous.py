03d006a4fb83b89f62f8cb1af7b7ebd17946d2ed
maxcorbeau <contact@maxcorbeau.com>
2024-11-26 08:22:32 +0100

    Versionner et utiliser les compilé/formatté de la vue

--- dags/views/view_acteur_tous.py
+++ dags/views/view_acteur_tous.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from logging import getLogger
+from pathlib import Path
 
 from airflow import DAG
 from airflow.decorators import task
@@ -20,8 +21,17 @@ SCHEDULE_INTERVAL = "0 2 * * *"
 VIEW_NAME = "qfdmo_vue_acteur_tous"
 
 
-def SQL_VIEW_CREATE_QUERY_get() -> str:
-    """Construit la requête SQL pour la vue matérialisée acteur_all"""
+def sql_view_generate_code() -> str:
+    """Génère le code SQL de la vue matérialisé
+    en faisant python dags/views/view_acteur_tous.py
+
+    Pour tester et finaliser la vue:
+    - placer le code dans le fichier dédié ./sql/view_acteur_tous.sql
+    - la formatter et pouvoir la relire plus facilement
+    - tester avec un client SQL
+
+    Au final on à le code SQL définitif dans notre repo pour le versionning
+    """
     # Champs communs à toutes les tables, on prend au mieux avec COALESCE
     fields_on_all = [
         # IDs
@@ -59,6 +69,15 @@ def SQL_VIEW_CREATE_QUERY_get() -> str:
     )
     # dernier champ sélectionné: parent_id qui n'existe que dans revisionacteur
     selected += ",\nra.parent_id AS parent_id,\n"
+    selected += """-- Si l'identifiant est dans parent_ids, alors c'est un parent
+            CASE
+                WHEN COALESCE(
+                    da.identifiant_unique,
+                    ra.identifiant_unique,
+                    a.identifiant_unique)
+                    IN (SELECT id FROM parent_ids) THEN TRUE
+                ELSE FALSE
+            END AS est_parent,\n"""
     selected += """da.identifiant_unique IS NOT NULL AS est_dans_displayedacteur,
                     ra.identifiant_unique IS NOT NULL AS est_dans_revisionacteur,
                     a.identifiant_unique IS NOT NULL AS est_dans_acteur"""
@@ -67,7 +86,28 @@ def SQL_VIEW_CREATE_QUERY_get() -> str:
     -- Il n'existe pas aujourdhui de CREATE OR REPLACE MATERIALIZED VIEW
     -- d'où l'autre requête DROP IF EXISTS avant de créer
     CREATE MATERIALIZED VIEW {VIEW_NAME} AS (
-        WITH acteur_all AS (
+        WITH
+        parent_ids AS (
+            SELECT DISTINCT
+                parent_id AS id
+            FROM
+                qfdmo_revisionacteur
+        ),
+        parent_ids_to_enfants AS (
+            SELECT
+                parent_id,
+                ARRAY_AGG (identifiant_unique) AS enfants_liste,
+                CARDINALITY(ARRAY_AGG (identifiant_unique)) AS enfants_nombre
+            FROM
+                qfdmo_revisionacteur AS ra
+            WHERE
+                parent_id IS NOT NULL
+            GROUP BY
+                1
+            ORDER BY
+                3 DESC
+        ),
+        acteur_all AS (
             SELECT
                 {selected}
                     FROM qfdmo_displayedacteur AS da
@@ -75,9 +115,6 @@ def SQL_VIEW_CREATE_QUERY_get() -> str:
                         ON da.identifiant_unique = ra.identifiant_unique
                     FULL OUTER JOIN qfdmo_acteur AS a
                         ON da.identifiant_unique = a.identifiant_unique
-        ), parent_ids AS (
-                SELECT DISTINCT parent_id AS id
-                FROM qfdmo_revisionacteur
         )
         SELECT
             -- ne pas faire un lazy * car ceci sélectionne des champs génériques
@@ -85,11 +122,21 @@ def SQL_VIEW_CREATE_QUERY_get() -> str:
             acteur_all.*,
             ST_Y(acteur_all.location) AS location_lat,
             ST_X(acteur_all.location) AS location_long,
-            -- Si l'identifiant est dans la liste des parent_ids, alors c'est un parent
+            -- Infos enfants pour les parents
             CASE
-                WHEN identifiant_unique IN (SELECT id FROM parent_ids) THEN TRUE
-                ELSE FALSE
-            END AS est_parent,
+                WHEN est_parent THEN (SELECT
+                    enfants_nombre
+                    FROM parent_ids_to_enfants
+                    WHERE parent_id = acteur_all.identifiant_unique)
+                ELSE NULL
+            END AS enfants_nombre,
+            CASE
+                WHEN est_parent THEN (SELECT
+                    enfants_liste
+                    FROM parent_ids_to_enfants
+                    WHERE parent_id = acteur_all.identifiant_unique)
+                ELSE NULL
+            END AS enfants_liste,
             -- Les codes pour être plus pratique ques les ids
             s.code AS source_code,
             atype.code AS acteur_type_code
@@ -100,8 +147,9 @@ def SQL_VIEW_CREATE_QUERY_get() -> str:
     return query
 
 
+DIR_CURRENT = Path(__file__).parent
 SQL_VIEW_DROP_QUERY = f"DROP MATERIALIZED VIEW IF EXISTS {VIEW_NAME};"
-SQL_VIEW_CREATE_QUERY = SQL_VIEW_CREATE_QUERY_get()
+SQL_VIEW_CREATE_QUERY = Path(DIR_CURRENT / "sql" / "view_acteur_tous.sql").read_text()
 
 DESCRIPTION = f"""Vue matérialisée SQL {VIEW_NAME} pour afficher tous les acteurs
 dans la base de données à partir des tables: qdfdmo_displayedacteur,
@@ -124,7 +172,7 @@ with DAG(
 ):
 
     @task
-    def view_acteur_all_drop_if_exists_and_create_task():
+    def view_acteur_tous_drop_if_exists_and_create_task():
         """Tâche qui supprime (si existe) et crée la vue matérialisée view_acteur_all
         La tâche gère les 2 opérations car:
             - logiquement elles sont liées (pas l'un sans l'autre)
@@ -143,9 +191,12 @@ with DAG(
                 cursor.execute(SQL_VIEW_CREATE_QUERY)
                 conn.commit()
 
-    view_acteur_all_drop_if_exists_and_create_task()
+    view_acteur_tous_drop_if_exists_and_create_task()
 
-# Pour débugger facilement via CLI
+# Pour générer le code SQL de la vue
+# python dags/views/view_acteur_tous.py
+# voir commentaire de la fonction sql_view_generate_code
+# pour les étapes suivantes
 if __name__ == "__main__":
-    print("SQL_VIEW_DROP_QUERY:\n", SQL_VIEW_DROP_QUERY)
-    print("SQL_VIEW_CREATE_QUERY:\n", SQL_VIEW_CREATE_QUERY)
+    print("Requête SQL générée:")
+    print(sql_view_generate_code())
