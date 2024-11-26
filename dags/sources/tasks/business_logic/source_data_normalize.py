53381585bf03dc6c2d3f0aa42bbe08fdd027c429
Nicolas Oudard <nicolas@oudard.org>
2024-11-26 09:20:23 +0100

    remove duplicated function

--- dags/sources/tasks/business_logic/source_data_normalize.py
+++ dags/sources/tasks/business_logic/source_data_normalize.py
@@ -1,9 +1,6 @@
 import logging
 
 import pandas as pd
-from sources.tasks.business_logic.read_mapping_from_postgres import (
-    read_mapping_from_postgres,
-)
 from sources.tasks.transform.transform_column import (
     cast_eo_boolean_or_string_to_boolean,
     mapping_try_or_fallback_column_value,
@@ -15,56 +12,6 @@ from utils import shared_constants as constants
 from utils.base_utils import extract_details, get_address
 
 logger = logging.getLogger(__name__)
-
-
-def source_data_normalize_wrapper(**kwargs) -> pd.DataFrame:
-    df = kwargs["ti"].xcom_pull(task_ids="source_data_download")
-
-    params = kwargs["params"]
-    source_code = params.get("source_code")
-    column_mapping = params.get("column_mapping", {})
-    columns_to_add_by_default = params.get("columns_to_add_by_default", {})
-    label_bonus_reparation = params.get("label_bonus_reparation")
-    validate_address_with_ban = params.get("validate_address_with_ban", False)
-    ignore_duplicates = params.get("ignore_duplicates", False)
-    combine_columns_categories = params.get("combine_columns_categories", [])
-    merge_duplicated_acteurs = params.get("merge_duplicated_acteurs", False)
-    product_mapping = params.get("product_mapping", {})
-    dechet_mapping = params.get("dechet_mapping", {})
-    acteurtype_id_by_code = read_mapping_from_postgres(table_name="qfdmo_acteurtype")
-    source_id_by_code = read_mapping_from_postgres(table_name="qfdmo_source")
-
-    log.preview("df avant normalisation", df)
-    log.preview("source_code", source_code)
-    log.preview("column_mapping", column_mapping)
-    log.preview("columns_to_add_by_default", columns_to_add_by_default)
-    log.preview("label_bonus_reparation", label_bonus_reparation)
-    log.preview("validate_address_with_ban", validate_address_with_ban)
-    log.preview("ignore_duplicates", ignore_duplicates)
-    log.preview("combine_columns_categories", combine_columns_categories)
-    log.preview("merge_duplicated_acteurs", merge_duplicated_acteurs)
-    log.preview("product_mapping", product_mapping)
-    log.preview("dechet_mapping", dechet_mapping)
-    log.preview("acteurtype_id_by_code", acteurtype_id_by_code)
-    log.preview("source_id_by_code", source_id_by_code)
-
-    params = kwargs["params"]
-
-    return source_data_normalize(
-        df_acteur_from_source=df,
-        source_code=source_code,
-        column_mapping=column_mapping,
-        columns_to_add_by_default=columns_to_add_by_default,
-        label_bonus_reparation=label_bonus_reparation,
-        validate_address_with_ban=validate_address_with_ban,
-        ignore_duplicates=ignore_duplicates,
-        combine_columns_categories=combine_columns_categories,
-        merge_duplicated_acteurs=merge_duplicated_acteurs,
-        product_mapping=product_mapping,
-        dechet_mapping=dechet_mapping,
-        acteurtype_id_by_code=acteurtype_id_by_code,
-        source_id_by_code=source_id_by_code,
-    )
 
 
 def source_data_normalize(
