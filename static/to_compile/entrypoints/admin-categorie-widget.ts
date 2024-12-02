f555c07ea956b7dddb141e2385d510ac9190f802
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-12-02 14:28:00 +0100

    Remove stimulus debug

--- static/to_compile/entrypoints/admin-categorie-widget.ts
+++ static/to_compile/entrypoints/admin-categorie-widget.ts
@@ -2,5 +2,4 @@ import { Application } from "@hotwired/stimulus"
 import AdminCategorieWidgetController from "../js/controllers/admin/categorie_widget_controller"
 
 window.stimulus = Application.start()
-stimulus.debug = true
 stimulus.register("admin-categorie-widget", AdminCategorieWidgetController)
