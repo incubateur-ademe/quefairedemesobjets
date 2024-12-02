dca3e7aa3249c2ee17ae12026fcf8dda25a29b90
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-12-02 14:26:48 +0100

    Cleanup function

--- static/to_compile/js/controllers/admin/categorie_widget_controller.ts
+++ static/to_compile/js/controllers/admin/categorie_widget_controller.ts
@@ -5,24 +5,19 @@ export default class extends Controller<HTMLFormElement> {
   declare readonly optionTargets: Array<HTMLOptionElement>
 
   syncSousCategorie(event) {
-    console.log(event)
     const sousCategories: HTMLSelectElement = document.querySelector(
       // TODO : un peu fragile....
       "#id_proposition_services-0-sous_categories",
-    )
-    const valuesToSelect = [...event.target.options]
+    )!
+    const categoriesSelected = [...event.target.options]
       .filter((option) => option.selected)
       .map((option) => option.value)
-    for (const option of sousCategories.options) {
-      if (valuesToSelect.includes(option.dataset.categorie)) {
-        option.selected = true
+    for (const sousCategorieOption of sousCategories.options) {
+      if (categoriesSelected.includes(sousCategorieOption.dataset.categorie)) {
+        sousCategorieOption.selected = true
       } else {
-        option.selected = false
+        sousCategorieOption.selected = false
       }
     }
   }
-
-  // (event) => {
-  // console.log({ event })
-  // })
 }
