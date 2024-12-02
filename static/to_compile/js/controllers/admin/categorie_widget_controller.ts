a76d4008f7e5c0d056309e9891670545491022ce
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-12-02 15:22:53 +0100

    More robust selector

--- static/to_compile/js/controllers/admin/categorie_widget_controller.ts
+++ static/to_compile/js/controllers/admin/categorie_widget_controller.ts
@@ -5,13 +5,14 @@ export default class extends Controller<HTMLFormElement> {
   declare readonly optionTargets: Array<HTMLOptionElement>
 
   syncSousCategorie(event) {
-    const sousCategories: HTMLSelectElement = document.querySelector(
-      // TODO : un peu fragile....
-      "#id_proposition_services-0-sous_categories",
-    )!
+    const sousCategories: HTMLSelectElement = this.element
+      .closest("fieldset")
+      .querySelector(`select[multiple]:not(#${this.element.id})`)!
+
     const categoriesSelected = [...event.target.options]
       .filter((option) => option.selected)
       .map((option) => option.value)
+
     for (const sousCategorieOption of sousCategories.options) {
       if (categoriesSelected.includes(sousCategorieOption.dataset.categorie)) {
         sousCategorieOption.selected = true
