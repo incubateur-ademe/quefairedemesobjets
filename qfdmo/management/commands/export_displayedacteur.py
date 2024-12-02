b3d80c9563d0effa5c853e593e0114f4ae5596d7
Nicolas Oudard <nicolas@oudard.org>
2024-12-02 12:10:57 +0100

    enregistrement de l'export sur le repo s3

--- qfdmo/management/commands/export_displayedacteur.py
+++ qfdmo/management/commands/export_displayedacteur.py
@@ -1,12 +1,13 @@
+import tempfile
 from datetime import datetime
-from pathlib import Path
 
 import openpyxl
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
 from django.core.management.base import BaseCommand
 
 from qfdmo.admin import OpenSourceDisplayedActeurResource
 
-TARGET_FILE = "export_lavo_acteurs.xlsx"
 CHUNK = 1000
 
 
@@ -15,33 +16,41 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write(f"Exporting Ressources, starting at {datetime.now()}")
+        target_file = datetime.now().strftime(
+            "exports/export_acteur_%Y%m%d_%H%M%S.xlsx"
+        )
 
-        if Path(TARGET_FILE).exists():
-            Path(TARGET_FILE).unlink()
+        with tempfile.NamedTemporaryFile(mode="w+b", suffix=".xlsx") as tmp_file:
 
-        try:
-            workbook = openpyxl.load_workbook(TARGET_FILE)
-            sheet = workbook.active
-        except FileNotFoundError:
-            workbook = openpyxl.Workbook()
-            sheet = workbook.active
+            try:
+                workbook = openpyxl.load_workbook("tmp.xlsx")
+                sheet = workbook.active
+            except FileNotFoundError:
+                workbook = openpyxl.Workbook()
+                sheet = workbook.active
 
-        offset = 0
-        dataset = OpenSourceDisplayedActeurResource(limit=CHUNK, offset=offset).export()
-        sheet.append(dataset.headers)
-
-        while dataset.dict:
-            self.stdout.write(f"Exporting {offset} to {offset + CHUNK}")
-            dataset.headers = None
-
-            for row in dataset.dict:
-                sheet.append(row)
-
-            offset += CHUNK
+            offset = 0
             dataset = OpenSourceDisplayedActeurResource(
                 limit=CHUNK, offset=offset
             ).export()
+            sheet.append(dataset.headers)
 
-        workbook.save(TARGET_FILE)
+            while dataset.dict:
+                self.stdout.write(f"Exporting {offset} to {offset + CHUNK}")
+                dataset.headers = None
+
+                for row in dataset.dict:
+                    sheet.append(row)
+
+                offset += CHUNK
+                dataset = OpenSourceDisplayedActeurResource(
+                    limit=CHUNK, offset=offset
+                ).export()
+
+            self.stdout.write(f"Writing to {target_file}")
+
+            workbook.save(tmp_file.name)
+            tmp_file.seek(0)
+            default_storage.save(target_file, ContentFile(tmp_file.read()))
 
         self.stdout.write(f"Ended at {datetime.now()}")
