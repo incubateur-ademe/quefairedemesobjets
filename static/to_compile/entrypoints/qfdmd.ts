a99c80c1b52ef885b2d6055c9df3d4da6cf79df3
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-12-04 21:08:19 +0100

    Add posthog

--- static/to_compile/entrypoints/qfdmd.ts
+++ static/to_compile/entrypoints/qfdmd.ts
@@ -11,11 +11,5 @@ stimulus.register("search", SearchController)
 stimulus.register("blink", BlinkController)
 stimulus.register("copy", CopyController)
 
-// TODO: remove once the map will be loaded using a turbo frame
-window.addEventListener("load", () => {
-  const iframe = document.querySelector("#ou-l-apporter iframe")
-  iframe.contentWindow.postMessage("ademe", "*");
-})
-
 
 Turbo.session.drive = false;
