a48810ba7a5b1a49ecfeb5673a777d73ae81963e
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-12-04 14:45:47 +0100

    Add carte to tabs

--- qfdmd/templatetags/qfdmd_tags.py
+++ qfdmd/templatetags/qfdmd_tags.py
@@ -4,23 +4,15 @@ from django.utils.safestring import mark_safe
 register = template.Library()
 
 
-@register.inclusion_tag("components/carte/carte.html", takes_context=True)
-def carte_from(context, produit):
-    request = context["request"]
-    try:
-        return {
-            "carte_settings": produit.sous_categorie_with_carte_display.carte_settings.items(),  # noqa: E501
-            "request": request,
-        }
-    except AttributeError:
-        return {}
-
-
 @register.inclusion_tag("components/patchwork/patchwork.html")
 def patchwork():
     from qfdmd.models import Synonyme
 
-    produits = Synonyme.objects.exclude(picto="").exclude(picto=None)
+    produits = (
+        Synonyme.objects.exclude(picto="")
+        .exclude(picto=None)
+        .filter(pin_on_homepage=True)
+    )
     return {"top": produits[:24], "left": produits[24:30], "right": produits[30:36]}
 
 
