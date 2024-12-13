723a53baddf381a059fbec8df9124ab3413fdce3
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-12-13 13:34:33 +0100

    Assistant v2 - iframe (#1133)

--- static/to_compile/entrypoints/qfdmd.ts
+++ static/to_compile/entrypoints/qfdmd.ts
@@ -1,12 +1,15 @@
 import * as Turbo from "@hotwired/turbo"
+import 'iframe-resizer/js/iframeResizer.contentWindow.min.js';
 import { Application } from "@hotwired/stimulus"
 
 import SearchController from "../js/controllers/assistant/search"
 import BlinkController from "../js/controllers/assistant/blink"
+import CopyController from "../js/copy_controller"
 
 window.stimulus = Application.start()
 stimulus.register("search", SearchController)
 stimulus.register("blink", BlinkController)
+stimulus.register("copy", CopyController)
 
 // TODO: remove once the map will be loaded using a turbo frame
 window.addEventListener("load", () => {
