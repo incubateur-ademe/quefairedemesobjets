eacd9e4f20c185c086f28cdd04513d7826a07716
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2024-12-13 10:43:05 +0100

    Exporter les Acteurs selon la licence à appliquer à la source (#1135)

--- qfdmo/management/commands/export_displayedacteur.py
+++ qfdmo/management/commands/export_displayedacteur.py
@@ -8,6 +8,7 @@ from django.core.files.storage import default_storage
 from django.core.management.base import BaseCommand
 
 from qfdmo.admin import OpenSourceDisplayedActeurResource
+from qfdmo.models.acteur import DataLicense
 
 CHUNK = 1000
 
@@ -15,10 +16,43 @@ CHUNK = 1000
 class Command(BaseCommand):
     help = "Export Ressources using CSV format"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--file",
+            type=str,
+            help="File to export to",
+            default=(f"export_acteur_{datetime.now().strftime('%Y%m%d_%H%M%S')}.xlsx"),
+        )
+        parser.add_argument(
+            "--licenses",
+            nargs="+",
+            action="extend",
+            type=str,
+            help=(
+                f"Licenses to export, options : {DataLicense.values}, "
+                f"default: '{DataLicense.OPEN_LICENSE.value}'"
+            ),
+        )
+
     def handle(self, *args, **options):
-        self.stdout.write(f"Exporting Ressources, starting at {datetime.now()}")
-        target_file = datetime.now().strftime(
-            "exports/export_acteur_%Y%m%d_%H%M%S.xlsx"
+        self.stdout.write(
+            self.style.SUCCESS(f"Exporting Ressources, starting at {datetime.now()}")
+        )
+        target_file = "exports/" + options["file"]
+        licenses = options["licenses"]
+        if not licenses:
+            licenses = [DataLicense.OPEN_LICENSE.value]
+        if not all(license in DataLicense.values for license in licenses):
+            self.stdout.write(
+                self.style.ERROR(
+                    f"Invalid licenses, options : {licenses}, "
+                    f"Available values: '{DataLicense.values}'"
+                )
+            )
+            return
+
+        self.stdout.write(
+            self.style.SUCCESS(f"Exporting DisplayedActeur using licenses: {licenses}")
         )
 
         with tempfile.NamedTemporaryFile(mode="w+b", suffix=".xlsx") as tmp_file:
@@ -31,12 +65,14 @@ class Command(BaseCommand):
 
             offset = 0
             dataset = OpenSourceDisplayedActeurResource(
-                limit=CHUNK, offset=offset
+                limit=CHUNK, offset=offset, licenses=licenses
             ).export()
             sheet.append(dataset.headers)
 
             while dataset.dict:
-                self.stdout.write(f"Exporting {offset} to {offset + CHUNK}")
+                self.stdout.write(
+                    self.style.SUCCESS(f"Exporting {offset} to {offset + CHUNK}")
+                )
                 dataset.headers = None
 
                 for row in dataset.dict:
@@ -44,13 +80,12 @@ class Command(BaseCommand):
 
                 offset += CHUNK
                 dataset = OpenSourceDisplayedActeurResource(
-                    limit=CHUNK, offset=offset
+                    limit=CHUNK, offset=offset, licenses=licenses
                 ).export()
-
-            self.stdout.write(f"Writing to {target_file}")
+            self.stdout.write(self.style.SUCCESS(f"Writing to {target_file}"))
 
             workbook.save(tmp_file.name)
             tmp_file.seek(0)
             default_storage.save(target_file, ContentFile(tmp_file.read()))
 
-        self.stdout.write(f"Ended at {datetime.now()}")
+        self.stdout.write(self.style.SUCCESS(f"Ended at {datetime.now()}"))
