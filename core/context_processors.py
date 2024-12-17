b503765dfb5ad0532d1ae5ca43f9e018bd6d1d1f
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-12-17 16:22:24 +0100

    Add posthog

--- core/context_processors.py
+++ core/context_processors.py
@@ -5,10 +5,12 @@ from . import constants
 
 
 def environment(request):
+    host_without_port = request.get_host().split(":")[0]
     return {
         "ENVIRONMENT": settings.ENVIRONMENT,
         "DEBUG": settings.DEBUG,
         "is_embedded": True,
+        "is_ademe": host_without_port in settings.ALLOWED_HOSTS,
         "turbo": request.headers.get("Turbo-Frame"),
     }
 
