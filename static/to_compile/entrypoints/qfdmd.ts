9b3d5b54aa5a6800e937c3bc827e7865d1a60786
Fabien Le Frapper <contact@fabienlefrapper.me>
2024-12-18 17:59:39 +0100

    wip

--- static/to_compile/entrypoints/qfdmd.ts
+++ static/to_compile/entrypoints/qfdmd.ts
@@ -8,11 +8,9 @@ import AnalyticsController from "../js/controllers/assistant/analytics"
 import CopyController from "../js/copy_controller"
 
 window.stimulus = Application.start()
-stimulus.debug = document.body.dataset.stimulusDebug
 stimulus.register("search", SearchController)
 stimulus.register("blink", BlinkController)
 stimulus.register("copy", CopyController)
 stimulus.register("analytics", AnalyticsController)
-
 
 Turbo.session.drive = false;
