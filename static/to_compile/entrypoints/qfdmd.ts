4949d17cd940d28287010021de575d8cee12f947
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-01-06 23:56:03 +0100

    Improve Posthog tracking for anonymous users

--- static/to_compile/entrypoints/qfdmd.ts
+++ static/to_compile/entrypoints/qfdmd.ts
@@ -8,9 +8,11 @@ import AnalyticsController from "../js/controllers/assistant/analytics"
 import CopyController from "../js/copy_controller"
 
 window.stimulus = Application.start()
+stimulus.debug = document.body.dataset.stimulusDebug
 stimulus.register("search", SearchController)
 stimulus.register("blink", BlinkController)
 stimulus.register("copy", CopyController)
 stimulus.register("analytics", AnalyticsController)
+
 
 Turbo.session.drive = false;
--- static/to_compile/js/controllers/assistant/search.ts
+++ static/to_compile/js/controllers/assistant/search.ts
@@ -4,13 +4,4 @@ export default class extends Controller<HTMLFormElement> {
   submitForm(event) {
     this.element.requestSubmit()
   }
-  click(event) {
-    console.log({ event })
-    event.target.click()
-  }
-
-  reset(event) {
-    alert("coucou")
-    console.log({ event })
-  }
 }
