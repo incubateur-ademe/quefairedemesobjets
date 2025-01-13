7898ec9270afce49af0db2750b90d2e7fc7706fc
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-01-13 08:56:39 +0100

    Ajout du support de la navigation au clavier dans le champ d'autocomplete de l'assistant v2 (#1194)

--- static/to_compile/js/controllers/assistant/search.ts
+++ static/to_compile/js/controllers/assistant/search.ts
@@ -1,7 +1,42 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller<HTMLFormElement> {
-  submitForm(event) {
-    this.element.requestSubmit()
+  declare readonly formTarget: HTMLFormElement
+  static targets = ["form"]
+
+  submitForm() {
+    this.formTarget.requestSubmit()
+  }
+
+  clear() {
+    for (const inputElement of this.formTarget.querySelectorAll("input")){
+      inputElement.value =''
+      this.submitForm()
+    }
+  }
+
+  #move(direction: "up" | "down") {
+    const searchResultAlreadyFocused = document.querySelector<HTMLAnchorElement>("#search-results > a:focus")
+    let elementToFocus = document.querySelector<HTMLAnchorElement>("#search-results > a")
+
+    if (searchResultAlreadyFocused) {
+      searchResultAlreadyFocused.blur();
+
+      if (direction == "up" && searchResultAlreadyFocused.previousElementSibling) {
+        elementToFocus = searchResultAlreadyFocused.previousElementSibling as HTMLAnchorElement
+      }
+      else if (searchResultAlreadyFocused.nextElementSibling) {
+        elementToFocus = searchResultAlreadyFocused.nextElementSibling as HTMLAnchorElement
+      }
+    }
+    elementToFocus?.focus()
+  }
+
+  down() {
+    this.#move("down")
+  }
+
+  up() {
+    this.#move("up")
   }
 }
