ecaaabb4cbc2106b573e72b62528823ca560c6f6
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-01-14 18:44:33 +0100

    Ajout des pages légales à l'assistant (#1212)
    * add legal pages to assistant
    
    * add to footer
    
    * Add to env
    
    * merge migrations
    
    * squash migrations

--- qfdmd/urls.py
+++ qfdmd/urls.py
@@ -4,7 +4,7 @@ from django.templatetags.static import static
 from django.urls import path
 from django.views.generic import RedirectView
 
-from qfdmd.views import HomeView, SynonymeDetailView, search_view
+from qfdmd.views import CMSPageDetailView, HomeView, SynonymeDetailView, search_view
 
 
 def get_assistant_script(request):
@@ -13,12 +13,13 @@ def get_assistant_script(request):
 
 urlpatterns = [
     path("dechet/", HomeView.as_view(), name="home"),
-    path("dechet/recherche", search_view, name="search"),
-    path("dechet/<slug:slug>/", SynonymeDetailView.as_view(), name="synonyme-detail"),
+    path("assistant/recherche", search_view, name="search"),
+    path("<slug:slug>/", SynonymeDetailView.as_view(), name="synonyme-detail"),
     # The URL here needs to be kept as is because it was used in the previous
     # Gatsby website. If changed, a redirect need to be created to keep the
     # legacy behaviour.
     path("iframe.js", get_assistant_script, name="script"),
+    path("assistant/<slug:slug>", CMSPageDetailView.as_view(), name="cms-page"),
     path(
         "assistant-enquete",
         RedirectView.as_view(
