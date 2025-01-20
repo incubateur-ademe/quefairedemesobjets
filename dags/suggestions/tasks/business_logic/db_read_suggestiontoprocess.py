9f62facb48a93a2ee2124d5f6195fba4a5bad632
Nicolas Oudard <nicolas@oudard.org>
2025-01-20 11:25:57 +0100

    fix suggestion process

--- dags/suggestions/tasks/business_logic/db_read_suggestiontoprocess.py
+++ dags/suggestions/tasks/business_logic/db_read_suggestiontoprocess.py
@@ -1,17 +1,23 @@
-from airflow.providers.postgres.hooks.postgres import PostgresHook
+import pandas as pd
+from shared.tasks.database_logic.db_manager import PostgresConnectionManager
 from sources.config import shared_constants as constants
 
 
 def get_first_suggetsioncohorte_to_insert():
-    hook = PostgresHook(postgres_conn_id="qfdmo_django_db")
-    row = hook.get_first(
+    engine = PostgresConnectionManager().engine
+
+    # get first cohorte suggestion to process as a dict
+    suggestion_cohorte = pd.read_sql_query(
         f"""
         SELECT * FROM data_suggestioncohorte
         WHERE statut = '{constants.SUGGESTION_ATRAITER}'
         LIMIT 1
-        """
+        """,
+        engine,
     )
-    return row
+    if suggestion_cohorte.empty:
+        return None
+    return suggestion_cohorte.to_dict(orient="records")[0]
 
 
 def db_read_suggestiontoprocess(**kwargs):
