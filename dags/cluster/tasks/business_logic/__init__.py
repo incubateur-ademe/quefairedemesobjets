793439d1083b35c22e2f77573726782fce31d724
maxcorbeau <contact@maxcorbeau.com>
2025-01-27 18:20:05 +0100

    mise à jour de l'init cluster business logic

--- dags/cluster/tasks/business_logic/__init__.py
+++ dags/cluster/tasks/business_logic/__init__.py
@@ -3,6 +3,10 @@ from .cluster_acteurs_metadata import cluster_acteurs_metadata  # noqa
 from .cluster_acteurs_parent_calculations import (  # noqa
     cluster_acteurs_parent_calculations,
 )
+from .cluster_acteurs_selection_acteur_type_parents import (  # noqa
+    cluster_acteurs_selection_acteur_type_parents,
+)
+from .cluster_acteurs_selection_from_db import cluster_acteurs_selection_from_db  # noqa
 from .cluster_acteurs_suggestions import cluster_acteurs_suggestions  # noqa
 from .cluster_acteurs_suggestions_to_db import cluster_acteurs_suggestions_to_db  # noqa
 from .cluster_acteurs_suggestions_validate import (  # noqa
