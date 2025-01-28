6c6f7881aad21ff405a5590e9b3343b2598b74bb
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-01-28 20:54:18 +0100

    More modular approach for servers.conf.erb

--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+import mimetypes
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.staticfiles import finders
 from django.http import HttpResponse
@@ -6,9 +8,17 @@ from django.http import HttpResponse
 def static_file_content_from(path):
     file_path = finders.find(path)
 
+    if not file_path:
+        return HttpResponse("File not found", status=404)
+
+    content_type, _ = mimetypes.guess_type(file_path)
+    # If the MIME type cannot be guessed (e.g., unknown file extension),
+    # we default to application/octet-stream, which is a generic binary type.
+    content_type = content_type or "application/octet-stream"
+
     with open(file_path, "r") as file:
         file_content = file.read()
-        return HttpResponse(file_content, content_type="application/javascript")
+        return HttpResponse(file_content, content_type=content_type)
 
 
 class IsStaffMixin(LoginRequiredMixin):
