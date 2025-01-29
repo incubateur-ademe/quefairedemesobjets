c6125c87bcbff0850c79426150b32b6e9ddfc822
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-01-29 13:41:28 +0100

    fx tests

--- static/to_compile/tests/iframe_functions.test.ts
+++ static/to_compile/tests/iframe_functions.test.ts
@@ -39,9 +39,9 @@ describe("getIframeAttributesAndExtra function tests", () => {
           '{"southWest": {"lat": 48.916, "lng": 2.298202514648438}, "northEast": {"lat": 48.98742568330284, "lng": 2.483596801757813}}',
         direction: "jai",
       },
-      initialParams: ["carte", "1"],
+      route: "carte",
       expectedSrc:
-        "https://example.com?carte=1&address_placeholder=toto%2Ctata&bounding_box=%7B%22southWest%22%3A%7B%22lat%22%3A48.916%2C%22lng%22%3A2.298202514648438%7D%2C%22northEast%22%3A%7B%22lat%22%3A48.98742568330284%2C%22lng%22%3A2.483596801757813%7D%7D&direction=jai",
+        "https://example.com/carte?address_placeholder=toto%2Ctata&bounding_box=%7B%22southWest%22%3A%7B%22lat%22%3A48.916%2C%22lng%22%3A2.298202514648438%7D%2C%22northEast%22%3A%7B%22lat%22%3A48.98742568330284%2C%22lng%22%3A2.483596801757813%7D%7D&direction=jai",
     },
     {
       description: "with a single EPCI code",
@@ -50,9 +50,9 @@ describe("getIframeAttributesAndExtra function tests", () => {
         epci_codes: "200073146",
         limit: "71",
       },
-      initialParams: ["carte", "1"],
+      route: "carte",
       expectedSrc:
-        "https://example.com?carte=1&action_list=acheter%7Crevendre%7Cpreter%7Cemprunter%7Clouer%7Cmettreenlocation%7Cdonner%7Cechanger%7Creparer&epci_codes=200073146&limit=71",
+        "https://example.com/carte?action_list=acheter%7Crevendre%7Cpreter%7Cemprunter%7Clouer%7Cmettreenlocation%7Cdonner%7Cechanger%7Creparer&epci_codes=200073146&limit=71",
     },
     {
       description: "with multiple EPCI codes",
@@ -61,19 +61,18 @@ describe("getIframeAttributesAndExtra function tests", () => {
         epci_codes: "200073146,200040442,245804497",
         limit: "71",
       },
-      initialParams: ["carte", "1"],
+      route: "carte",
       expectedSrc:
-        "https://example.com?carte=1&action_list=acheter%7Crevendre%7Cpreter%7Cemprunter%7Clouer%7Cmettreenlocation%7Cdonner%7Cechanger%7Creparer&epci_codes=200073146&epci_codes=200040442&epci_codes=245804497&limit=71",
+        "https://example.com/carte?action_list=acheter%7Crevendre%7Cpreter%7Cemprunter%7Clouer%7Cmettreenlocation%7Cdonner%7Cechanger%7Creparer&epci_codes=200073146&epci_codes=200040442&epci_codes=245804497&limit=71",
     },
   ])(
     "should generate correct iframe attributes $description",
-    ({ dataset, initialParams, expectedSrc }) => {
+    ({ dataset, route, expectedSrc }) => {
       // These are tests...any seems acceptable but we might want to type this at some point.
       setScriptDataset(dataset as any);
 
       const [iframeAttributes, iframeExtraAttributes] = getIframeAttributesAndExtra(
-        initialParams as [string, string],
-        scriptTag
+        scriptTag, route
       );
 
       assertIframeAttributes(iframeAttributes, expectedSrc);
