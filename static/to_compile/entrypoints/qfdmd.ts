5a087c77183ebbd4e792d37d4e5dfa05a2ae1215
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-01-30 11:14:35 +0100

    clear cache and unregister sw

--- static/to_compile/entrypoints/qfdmd.ts
+++ static/to_compile/entrypoints/qfdmd.ts
@@ -14,6 +14,36 @@ stimulus.register("blink", BlinkController)
 stimulus.register("copy", CopyController)
 stimulus.register("analytics", AnalyticsController)
 
+document.addEventListener("DOMContentLoaded", () => {
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then(function(registrations) {
+      registrations.forEach(function(registration) {
+        registration.unregister().then(function(success) {
+          if (success) {
+            console.log('Service worker unregistered successfully.');
+          } else {
+            console.log('Failed to unregister service worker.');
+          }
+        });
+      });
+    }).catch(function(error) {
+      console.error('Error while fetching service workers:', error);
+    });
+  }
+  if ('caches' in window) {
+    caches.keys().then(function(cacheNames) {
+      cacheNames.forEach(function(cacheName) {
+        caches.delete(cacheName).then(function(success) {
+          if (success) {
+            console.log('Cache deleted:', cacheName);
+          }
+        });
+      });
+    });
+  }
+})
+
 
 
 Turbo.session.drive = false;
