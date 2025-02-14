c7ff76579a9551bc013f6404381e85fff063a5eb
Nicolas Oudard <nicolas@oudard.org>
2025-02-14 14:23:50 +0100

    Ajout du champ uuid à VueActeur et ajout d'indexes

--- qfdmo/admin/acteur.py
+++ qfdmo/admin/acteur.py
@@ -771,6 +771,8 @@ class OpenSourceDisplayedActeurResource(resources.ModelResource):
 
 
 class DisplayedActeurAdmin(import_export_admin.ExportMixin, BaseActeurAdmin):
+    list_display = list(BaseActeurAdmin.list_display) + ["uuid"]
+    search_fields = list(BaseActeurAdmin.search_fields) + ["uuid"]
     change_form_template = "admin/displayed_acteur/change_form.html"
     gis_widget = CustomOSMWidget
     base_fields = list(BaseActeurAdmin.fields)
@@ -807,6 +809,8 @@ class VueActeurLabelQualiteInline(NotMutableMixin, admin.StackedInline):
 
 
 class VueActeurAdmin(NotMutableMixin, BaseActeurAdmin):
+    list_display = list(BaseActeurAdmin.list_display) + ["uuid"]
+    search_fields = list(BaseActeurAdmin.search_fields) + ["uuid"]
     inlines = [
         VuePropositionServiceInline,
         VueActeurLabelQualiteInline,
