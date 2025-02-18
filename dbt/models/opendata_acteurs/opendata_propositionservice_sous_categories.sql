ad75c394112551d252cce2a042bb165963a1069e
Nicolas Oudard <nicolas@oudard.org>
2025-02-18 12:26:08 +0100

    ajout de models opendata

---
+++ dbt/models/opendata_acteurs/opendata_propositionservice_sous_categories.sql
@@ -0,0 +1,32 @@
+with
+    parent_vuepropositionservice_sous_categories
+    AS
+    (
+        SELECT
+            MIN(qfdmo_vuepropositionservice_sous_categories.id) AS id,
+            CONCAT(temp_opendata_parentpropositionservice.parent_id::text, '_', temp_opendata_parentpropositionservice.action_id::text) AS propositionservice_id,
+            qfdmo_vuepropositionservice_sous_categories.souscategorieobjet_id AS souscategorieobjet_id
+        FROM qfdmo_vuepropositionservice_sous_categories
+            INNER JOIN {{ ref('temp_opendata_parentpropositionservice') }} AS temp_opendata_parentpropositionservice
+                ON temp_opendata_parentpropositionservice.id = qfdmo_vuepropositionservice_sous_categories.vuepropositionservice_id
+        GROUP BY
+            propositionservice_id,
+            souscategorieobjet_id
+    ),
+    nochild_vuepropositionservice_sous_categories
+    AS
+    (
+        SELECT
+            qfdmo_vuepropositionservice_sous_categories.id AS id,
+            qfdmo_vuepropositionservice_sous_categories.vuepropositionservice_id AS propositionservice_id,
+            qfdmo_vuepropositionservice_sous_categories.souscategorieobjet_id AS souscategorieobjet_id
+        FROM qfdmo_vuepropositionservice_sous_categories
+            INNER JOIN {{ ref('temp_opendata_propositionservice') }} AS ps ON qfdmo_vuepropositionservice_sous_categories.vuepropositionservice_id = ps.id
+            INNER JOIN {{ ref('temp_opendata_filteredacteur') }} AS cfa ON ps.acteur_id = cfa.identifiant_unique AND cfa.parent_id is null
+    )
+
+SELECT *
+    FROM parent_vuepropositionservice_sous_categories
+UNION ALL
+SELECT *
+    FROM nochild_vuepropositionservice_sous_categories
