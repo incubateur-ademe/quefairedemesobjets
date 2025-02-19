bb9b23c3c8220914e835093b6798b3cf1478ca6f
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2025-02-19 12:31:04 +0100

    script de ratrappage par source des propositions de service (#1370)
    script de ratrappage par soure car les modification de proposition de services on été ignoré lors de l'ingestion des dernière nouvelle version des sources

---
+++ qfdmo/management/commands/enrich_revisionpropositionservice.py
@@ -0,0 +1,115 @@
+import argparse
+
+from django.core.management.base import BaseCommand
+from django.db.models import QuerySet
+
+from qfdmo.models.acteur import (
+    Acteur,
+    RevisionActeur,
+    RevisionPropositionService,
+    Source,
+)
+from qfdmo.models.action import Action
+
+
+def ps_by_action(propositionservices: QuerySet):
+    return {
+        p.action.code: [sscat for sscat in p.sous_categories.all()]
+        for p in propositionservices
+    }
+
+
+class Command(BaseCommand):
+    help = "Suppression des parents qui n'ont pas d'enfants"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--dry-run",
+            help="Run command without writing changes to the database",
+            action=argparse.BooleanOptionalAction,
+            default=False,
+        )
+        parser.add_argument(
+            "--source_code",
+            help="code de la source concernée",
+            type=str,
+            required=True,
+        )
+
+    def handle(self, *args, **options):
+        dry_run = options["dry_run"]
+        source_code = options["source_code"]
+
+        try:
+            source = Source.objects.get(code=source_code)
+        except Source.DoesNotExist:
+            self.stdout.write(self.style.ERROR(f"Source {source_code} non trouvée"))
+
+        for acteur in Acteur.objects.filter(source=source):
+            try:
+                revision_acteur = RevisionActeur.objects.get(
+                    identifiant_unique=acteur.identifiant_unique
+                )
+            except RevisionActeur.DoesNotExist:
+                continue
+
+            ps_acteur_by_action = ps_by_action(acteur.proposition_services.all())
+            rps_acteur_by_action = ps_by_action(
+                revision_acteur.proposition_services.all()
+            )
+
+            pss_to_add = []
+            pss_to_update = []
+            for action_code, ps_acteur_sscats in ps_acteur_by_action.items():
+                if action_code not in rps_acteur_by_action:
+                    pss_to_add.append(
+                        {
+                            action_code: ps_acteur_sscats,
+                        }
+                    )
+                    continue
+
+                if sscat := set(ps_acteur_sscats) - set(
+                    rps_acteur_by_action[action_code]
+                ):
+                    pss_to_update.append(
+                        {
+                            action_code: sscat,
+                        }
+                    )
+
+            action_by_code = {a.code: a for a in Action.objects.all()}
+            if pss_to_add or pss_to_update:
+                message = f"Acteur {acteur.identifiant_unique}:\n"
+                if pss_to_add:
+                    displayed_ps = [
+                        {k: [s.code for s in v] for k, v in ps.items()}
+                        for ps in pss_to_add
+                    ]
+                    message += "Ajout des proposition de services\n"
+                    message += f"{displayed_ps}"
+                if pss_to_update:
+                    displayed_ps = [
+                        {k: [s.code for s in v] for k, v in ps.items()}
+                        for ps in pss_to_update
+                    ]
+                    message += "Mise à jour des proposition de services\n"
+                    message += f"{displayed_ps}"
+                self.stdout.write(self.style.WARNING(message))
+                if not dry_run:
+                    if pss_to_add:
+                        for ps_to_add in pss_to_add:
+                            for action_code, ps_acteur_sscats in ps_to_add.items():
+                                ps = RevisionPropositionService.objects.create(
+                                    action=action_by_code[action_code],
+                                    acteur=revision_acteur,
+                                )
+                                ps.sous_categories.add(*ps_acteur_sscats)
+                    if pss_to_update:
+                        for ps_to_update in pss_to_update:
+                            for action_code, ps_acteur_sscats in ps_to_update.items():
+                                ps = RevisionPropositionService.objects.get(
+                                    action=action_by_code[action_code],
+                                    acteur=revision_acteur,
+                                )
+                                ps.sous_categories.add(*ps_acteur_sscats)
