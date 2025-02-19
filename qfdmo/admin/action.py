e793bef7f23fda76951ba205deff5765bbf5045b
Nicolas Oudard <nicolas@oudard.org>
2025-02-19 12:24:56 +0100

    Fix de répétition bien vue par Fabien

--- qfdmo/admin/action.py
+++ qfdmo/admin/action.py
@@ -1,5 +1,4 @@
 from django.contrib.gis import admin
-from django.http import HttpRequest
 
 from core.admin import CodeLibelleModelMixin, OnlyEditableMixin
 from qfdmo.models import Action, ActionDirection, GroupeAction
@@ -13,25 +12,12 @@ class ActionAdmin(OnlyEditableMixin, CodeLibelleModelMixin, admin.ModelAdmin):
 
 
 class ActionDirectionAdmin(OnlyEditableMixin, CodeLibelleModelMixin, admin.ModelAdmin):
-
     list_display = ("code", "libelle", "order")
-
-    def has_add_permission(self, request: HttpRequest) -> bool:
-        return False
-
-    def has_delete_permission(self, request: HttpRequest, obj=None) -> bool:
-        return False
 
 
 class GroupeActionAdmin(OnlyEditableMixin, CodeLibelleModelMixin, admin.ModelAdmin):
     list_display = ("code", "libelle", "order")
     search_fields = ["code", "libelle"]
-
-    def has_add_permission(self, request: HttpRequest) -> bool:
-        return False
-
-    def has_delete_permission(self, request: HttpRequest, obj=None) -> bool:
-        return False
 
 
 admin.site.register(Action, ActionAdmin)
