a1ab72047da0258b10763e8fe5128f33746b53c5
Max Corbeau <contact@maxcorbeau.com>
2025-02-20 16:46:48 +0100

    crawl urls fonctionnel en local

--- data/models/changes/__init__.py
+++ data/models/changes/__init__.py
@@ -2,11 +2,13 @@ from .acteur_change_nothing_in_base import ChangeActeurNothingBase
 from .acteur_create_as_parent import ChangeActeurCreateAsParent
 from .acteur_delete_as_parent import ChangeActeurDeleteAsParent
 from .acteur_keep_as_parent import ChangeActeurKeepAsParent
+from .acteur_update_data import ChangeActeurUpdateData
 from .acteur_update_parent_id import ChangeActeurUpdateParentId
 from .acteur_verify_in_revision import ChangeActeurVerifyRevision
 from .sample_model_do_nothing import SampleModelDoNothing
 
 CHANGE_MODELS = {
+    ChangeActeurUpdateData.name(): ChangeActeurUpdateData,
     ChangeActeurCreateAsParent.name(): ChangeActeurCreateAsParent,
     ChangeActeurDeleteAsParent.name(): ChangeActeurDeleteAsParent,
     ChangeActeurUpdateParentId.name(): ChangeActeurUpdateParentId,
