7237a313278fd07363bfe68e601378ceb2dc6659
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-02-21 12:59:37 +0100

    Use single template tag

--- core/templatetags/seo_tags.py
+++ core/templatetags/seo_tags.py
@@ -1,17 +1,28 @@
-from urllib.parse import quote_plus
+from urllib.parse import quote, quote_plus
 
 from django import template
 
 register = template.Library()
 
 
-def get_sharer_content(request, object, social_network):
+def get_sharer_content(request, object, social_network=None):
     """This "dummy" function is defined here to provide
     similar features for jinja and django templating language.
     Once jinja will be removed from the project, this can be merged in
     share_url function below"""
-    url = quote_plus(object.get_share_url(request))
+    try:
+        url = quote_plus(object.get_share_url(request))
+    except AttributeError:
+        url = quote_plus(request.build_absolute_uri())
+
     INTRO = "Découvrez le site de l'ADEME “Que faire de mes objets & déchets”"
+    BODY = ("Bonjour,\n "
+            "Vous souhaitez encourager au tri et la consommation responsable, "
+            "le site de l’ADEME “Que faire de mes objets & déchets” accompagne "
+            "les citoyens grâce à des bonnes pratiques et adresses près de chez eux,"
+            " pour éviter l'achat neuf et réduire les déchets.\n"
+            "Découvrez le ici : "
+            )
 
     template = {
         "url": url,
@@ -29,19 +40,18 @@ def get_sharer_content(request, object, social_network):
         },
         "email": {
             "title": f"partager {object} par email - nouvelle fenêtre",
-            "url": f"mailto:?subject={INTRO}&body=Bonjour,\n "
-            "Vous souhaitez encourager au tri et la consommation responsable, "
-            "le site de l’ADEME Que faire de mes objets & déchets accompagne "
-            "les citoyens grâce à des bonnes pratiques et adresses près de chez eux,"
-            " pour éviter l'achat neuf et réduire les déchets.\n"
-            f"Découvrez le ici : {url}",
+            "url": f"mailto:?subject={quote(INTRO)}&body={quote(BODY)} {url}"
         },
     }
+    if not social_network:
+        return template
+
     return template[social_network]
 
 
-@register.simple_tag
-def sharer(context, social_network):
+@register.simple_tag(takes_context=True)
+def configure_sharer(context):
+    object = context["object"].produit
     request = context["request"]
-    object = context["object"]
-    return get_sharer_content(request, object, social_network)
+    context["sharer"] = get_sharer_content(request, object)
+    return ""
