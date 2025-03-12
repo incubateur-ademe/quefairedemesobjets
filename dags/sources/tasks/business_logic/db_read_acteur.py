9190c96eb4f412751ac4e9b53bd8b2924738e985
Nicolas Oudard <nicolas@oudard.org>
2025-03-12 22:30:31 +0100

    inyégration des commentaires de Max

--- dags/sources/tasks/business_logic/db_read_acteur.py
+++ dags/sources/tasks/business_logic/db_read_acteur.py
@@ -66,10 +66,11 @@ def db_read_acteur(df_normalized: pd.DataFrame, dag_config: DAGConfig):
             for proposition_service in acteur.proposition_services.all()
         ]
         acteurs_list.append(acteur_dict)
-    if not acteurs_list:
+    if acteurs_list:
+        df_acteur = pd.DataFrame(acteurs_list)
+    else:
         # Set an empty dataframe with the expected columns to avoid errors on next tasks
-        return pd.DataFrame(columns=list(dag_config.get_expected_columns()))
-    df_acteur = pd.DataFrame(acteurs_list)
+        df_acteur = pd.DataFrame(columns=list(dag_config.get_expected_columns()))
 
     log.preview("df_acteur retourné par la tâche", df_acteur)
     return df_acteur
