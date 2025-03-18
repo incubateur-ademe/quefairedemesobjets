3f3fcc4ad02a0bc1c4a3b99c4a61689d1a00d539
Nicolas Oudard <nicolas@oudard.org>
2025-03-18 23:43:09 +0100

    compute shortuuid

--- dbt/models/marts/opendata/marts_opendata_filtered_acteur.sql
+++ dbt/models/marts/opendata/marts_opendata_filtered_acteur.sql
@@ -1,6 +1,8 @@
-SELECT tva.*
-FROM {{ ref('int_acteur') }} AS tva
-LEFT JOIN {{ ref('int_acteur') }} AS tpva
+
+WITH filtered_children_acteur AS (
+    SELECT tva.*
+    FROM {{ ref('int_acteur') }} AS tva
+    LEFT JOIN {{ ref('int_acteur') }} AS tpva
     ON tva.parent_id = tpva.identifiant_unique
 LEFT JOIN {{ ref('base_source') }} AS ts ON tva.source_id = ts.id
 WHERE tva.statut = 'ACTIF'
@@ -8,3 +10,15 @@ WHERE tva.statut = 'ACTIF'
     AND ts.licence = 'OPEN_LICENSE' -- Only open lisense
     AND tva.public_accueilli NOT IN ('AUCUN', 'PROFESSIONNELS') -- AND va.public_accueilli != 'Professionnels' ?
     AND tva.identifiant_unique NOT LIKE '%_reparation_%'
+    AND tva.nom='VELOPITO'
+),
+filtered_parent_acteur AS (
+    SELECT tva.*
+    FROM {{ ref('int_acteur') }} AS tva
+    WHERE tva.identifiant_unique IN (SELECT parent_id FROM filtered_children_acteur)
+)
+
+SELECT * FROM filtered_parent_acteur
+UNION ALL
+SELECT * FROM filtered_children_acteur
+
