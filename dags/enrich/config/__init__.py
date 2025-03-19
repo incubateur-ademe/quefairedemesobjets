03b13a69a693366dd2e40a02d3150672a24c2b9f
Max Corbeau <contact@maxcorbeau.com>
2025-03-19 13:01:25 +0100

    refacto dags/rgpd -> dags/enrich, utilisation dbt

--- dags/enrich/config/__init__.py
+++ dags/enrich/config/__init__.py
@@ -1,4 +1,5 @@
 from .columns import COLS  # noqa: F401
+from .dbt import DBT  # noqa: F401
 from .paths import DIR_SQL_READ  # noqa: F401
 from .tasks import TASKS  # noqa: F401
 from .xcoms import XCOMS, xcom_pull  # noqa: F401
