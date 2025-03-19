c099c9e489c06f1615509b0dd012a33aac5d08e1
Max Corbeau <contact@maxcorbeau.com>
2025-03-19 17:25:04 +0100

    ajout nouvelle config & test rgpd

--- dags/enrich/config/__init__.py
+++ dags/enrich/config/__init__.py
@@ -1,5 +1,6 @@
 from .columns import COLS  # noqa: F401
 from .dbt import DBT  # noqa: F401
 from .paths import DIR_SQL_READ  # noqa: F401
+from .rgpd import RGPD  # noqa: F401
 from .tasks import TASKS  # noqa: F401
 from .xcoms import XCOMS, xcom_pull  # noqa: F401
