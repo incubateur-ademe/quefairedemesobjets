95a6f3dda176460a361e99a77e1e6113a550d96f
Max Corbeau <contact@maxcorbeau.com>
2025-03-19 13:01:25 +0100

    refacto dags/rgpd -> dags/enrich, utilisation dbt

--- dags/enrich/tasks/business_logic/enrich_ae_rgpd_read.py
+++ dags/enrich/tasks/business_logic/enrich_ae_rgpd_read.py
@@ -2,29 +2,29 @@
 
 import numpy as np
 import pandas as pd
-from rgpd.config import DIR_SQL_READ
 from utils import logging_utils as log
 from utils.django import django_setup_full
 
 django_setup_full()
 
 
-def rgpd_anonymize_people_read(filter_comments_contain: str = "") -> pd.DataFrame:
+def enrich_ae_rgpd_read(
+    dbt_model_name: str, filter_comments_contain: str = ""
+) -> pd.DataFrame:
     """Reads necessary QFDMO acteurs and AE entries from DB"""
     from django.db import connection
 
-    # Get SQL query and set filter
-    sql = (DIR_SQL_READ / "rgpd_anonymize_people_read.sql").read_text()
-    sql = sql.replace("{{filter_comments_contain}}", filter_comments_contain)
-
     # Execute SQL query and get data
     with connection.cursor() as cursor:
-        cursor.execute(sql)
+        cursor.execute(f"SELECT * FROM {dbt_model_name}")
         columns = [col[0] for col in cursor.description]
         data = cursor.fetchall()
 
     # Create DataFrame and preview
     df = pd.DataFrame(data, columns=columns, dtype="object").replace({np.nan: None})
-    log.preview_df_as_markdown("Acteurs & entrées Annuaire Entreprise", df)
+    log.preview_df_as_markdown("Matches AVANT filtre commentaires", df)
+    if not df.empty and filter_comments_contain:
+        df = df[df["acteur_commentaires"].str.contains(filter_comments_contain)].copy()
+    log.preview_df_as_markdown("Matches APRES filtre commentaires", df)
 
     return df
