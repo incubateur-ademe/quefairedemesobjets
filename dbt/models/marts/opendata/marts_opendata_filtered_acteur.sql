a1fc2519e51d5aae5577ca4a5c85045b6cb9ae7e
Nicolas Oudard <nicolas@oudard.org>
2025-03-19 14:06:58 +0100

    Change gcc pou g++ à cause de sites-faciles

--- dbt/models/marts/opendata/marts_opendata_filtered_acteur.sql
+++ dbt/models/marts/opendata/marts_opendata_filtered_acteur.sql
@@ -8,9 +8,8 @@ LEFT JOIN {{ ref('base_source') }} AS ts ON tva.source_id = ts.id
 WHERE tva.statut = 'ACTIF'
     AND (tpva.statut is null or tpva.statut = 'ACTIF')
     AND ts.licence = 'OPEN_LICENSE' -- Only open lisense
-    AND tva.public_accueilli NOT IN ('AUCUN', 'PROFESSIONNELS') -- AND va.public_accueilli != 'Professionnels' ?
+    AND (tva.public_accueilli IS NULL OR tva.public_accueilli NOT IN ('AUCUN', 'PROFESSIONNELS')) -- AND va.public_accueilli != 'Professionnels' ?
     AND tva.identifiant_unique NOT LIKE '%_reparation_%'
-    AND tva.nom='VELOPITO'
 ),
 filtered_parent_acteur AS (
     SELECT tva.*
