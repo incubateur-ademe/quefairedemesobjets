93f9b004f54cc7cd480a654c55486764a76737c1
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-03-26 21:04:36 +0100

    Wip

--- qfdmo/views/carte.py
+++ qfdmo/views/carte.py
@@ -3,7 +3,7 @@ import logging
 from django.db.models import Q
 from django.views.generic import DetailView
 
-from qfdmo.models import CarteConfig, GroupeAction
+from qfdmo.models import CarteConfig
 from qfdmo.views.adresses import CarteSearchActeursView
 
 logger = logging.getLogger(__name__)
@@ -12,63 +12,53 @@ logger = logging.getLogger(__name__)
 class CustomCarteView(DetailView, CarteSearchActeursView):
     model = CarteConfig
 
-    def _get_groupe_actions(self):
+    @property
+    def groupe_actions(self):
         # TODO: cache
-        ids = (
-            self.get_object()
-            .groupe_action_config.all()
-            .values_list("groupe_action", flat=True)
-        )
-        return GroupeAction.objects.filter(id__in=ids)
+        return self.get_object().groupe_action.all().order_by("order")
 
     def _set_action_list(self, *args, **kwargs):
-        actions = self._get_groupe_actions()
-        return actions
+        return self.groupe_actions
 
     def _set_action_displayed(self, *args, **kwargs):
-        actions = self._get_groupe_actions()
-        return actions
+        return self.groupe_actions
 
     def _get_selected_action_code(self, *args, **kwargs):
-        return self._set_action_list()
+        return self.groupe_actions
+
+    def get_cached_groupe_action_with_displayed_actions(self, *args, **kwargs):
+        return [
+            [groupe_action, groupe_action.actions.all()]
+            for groupe_action in self.groupe_actions
+        ]
+
+    def _grouped_action_from(self, *args, **kwargs):
+        return [
+            "|".join(groupe_action.actions.all().values_list("code", flat=True))
+            for groupe_action in self.get_object().groupe_action.all()
+        ]
 
     def _compile_acteurs_queryset(self, *args, **kwargs):
         filters, excludes = super()._compile_acteurs_queryset(*args, **kwargs)
 
-        acteur_types_to_filter = (
-            self.get_object()
-            .groupe_action_config.all()
-            .values_list("acteur_type", flat=True)
-        )
-        if acteur_types_to_filter:
-            filters &= Q(acteur_type__in=acteur_types_to_filter)
+        # acteur_types_to_filter = (
+        #     self.get_object()
+        #     .groupe_action_config.all()
+        #     .values_list("acteur_type", flat=True)
+        # )
+        # if acteur_types_to_filter:
+        #     filters &= Q(acteur_type__in=acteur_types_to_filter)
 
-        if sous_categorie_filter := self.get_object().sous_categorie_objet:
+        if sous_categorie_filter := self.get_object().sous_categorie_objet.all():
             filters &= Q(
-                proposition_services__sous_categories=sous_categorie_filter.first().id,
+                proposition_services__sous_categories__in=sous_categorie_filter,
             )
 
+        if groupe_action_filter := self.get_object().groupe_action.all():
+            action_ids = list(
+                groupe_action_filter.values_list("actions__id", flat=True)
+            )
+            filters &= Q(
+                proposition_services__action_id__in=action_ids,
+            )
         return filters, excludes
-
-    def get_initial(self, *args, **kwargs):
-        initial = super().get_initial(*args, **kwargs)
-        # Action to display and check
-        action_displayed = self._set_action_displayed()
-        initial["action_displayed"] = "|".join([a.code for a in action_displayed])
-
-        action_list = self._set_action_list(action_displayed)
-        initial["action_list"] = "|".join([a.code for a in action_list])
-
-        if self.is_carte:
-            grouped_action_choices = self._get_grouped_action_choices(action_displayed)
-            actions_to_select = self._get_selected_action()
-            initial["grouped_action"] = self._grouped_action_from(
-                grouped_action_choices, actions_to_select
-            )
-            # TODO : refacto forms, merge with grouped_action field
-            initial["legend_grouped_action"] = initial["grouped_action"]
-
-            initial["action_list"] = "|".join(
-                [a for ga in initial["grouped_action"] for a in ga.split("|")]
-            )
-        return initial
