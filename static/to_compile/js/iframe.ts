7e7e2d5b8baaf8db60803a17c2db2e14f43dcba1
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-04-01 21:48:37 +0200

    Masquer le bouton infos dans le cas où l'intégrateur est l'ADEME

--- static/to_compile/js/iframe.ts
+++ static/to_compile/js/iframe.ts
@@ -5,7 +5,10 @@ near future (around january 2025)
 */
 window.addEventListener("DOMContentLoaded", (event) => {
   const domain = new URL(document.referrer).hostname
-  if (domain === 'localhost' || domain.endsWith(".ademe.fr") || domain === "quefairedemesobjets-preprod.osc-fr1.scalingo.io") {
-    document.querySelector("#logo")?.remove()
+  if (true || domain === 'localhost' || domain.endsWith(".ademe.fr") || domain === "quefairedemesobjets-preprod.osc-fr1.scalingo.io") {
+    for (const elementToRemove of document.querySelectorAll("[data-remove-if-internal]")) {
+      console.log({ elementToRemove })
+      elementToRemove.remove()
+    }
   }
 });
