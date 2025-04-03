8861f2df0acba46a34ec5a73d5210148df36ac51
Nicolas Oudard <nicolas@oudard.org>
2025-04-03 11:36:03 +0200

    validate and replace vueacteur table

--- dbt/models/exposure/acteurs/exhaustive/exposure_exhaustive_acteur_acteur_services.sql
+++ dbt/models/exposure/acteurs/exhaustive/exposure_exhaustive_acteur_acteur_services.sql
@@ -1,1 +1,5 @@
-select * from {{ ref('marts_exhaustive_acteur_acteur_services') }}
+select
+    id,
+    acteur_id AS vueacteur_id,
+    acteurservice_id
+ from {{ ref('marts_exhaustive_acteur_acteur_services') }}
