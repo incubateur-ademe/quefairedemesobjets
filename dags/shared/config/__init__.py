912883bd0a09824bc71f73042bd06f7cd8444626
Max Corbeau <contact@maxcorbeau.com>
2025-04-07 08:43:38 +0200

    utilisation constantes de config dans DAG

--- dags/shared/config/__init__.py
+++ dags/shared/config/__init__.py
@@ -1,3 +1,4 @@
 from .catchups import CATCHUPS  # noqa: F401
+from .models import config_to_airflow_params  # noqa: F401
 from .schedules import SCHEDULES  # noqa: F401
 from .start_dates import START_DATES  # noqa: F401
