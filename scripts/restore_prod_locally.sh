abb3530c627c2ef2fe6ea961d85a1a427c1d18d7
maxcorbeau <contact@maxcorbeau.com>
2025-04-16 11:18:36 +0200

    drop changes in restore script

--- scripts/restore_prod_locally.sh
+++ scripts/restore_prod_locally.sh
@@ -1,4 +1,3 @@
-DUMP_FILE=/home/me/Downloads/20250410002559_quefairedem_5084/20250410002559_quefairedem_5084.pgsql
 DATABASE_URL=postgres://qfdmo:qfdmo@localhost:6543/qfdmo # pragma: allowlist secret
 
 for table in $(psql "${DATABASE_URL}" -t -c "SELECT \"tablename\" FROM pg_tables WHERE schemaname='public'"); do
