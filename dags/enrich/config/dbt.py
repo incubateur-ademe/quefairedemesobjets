d418ca3c1cd3d5de1b0ca5412cdb5fb3d477726b
maxcorbeau <contact@maxcorbeau.com>
2025-04-16 10:30:57 +0200

    suppression code RGPD

--- dags/enrich/config/dbt.py
+++ dags/enrich/config/dbt.py
@@ -5,7 +5,6 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class DBT:
-    MARTS_ENRICH_AE_RGPD: str = "marts_enrich_ae_rgpd"
     MARTS_ENRICH_AE_CLOSED_CANDIDATES: str = "marts_enrich_acteurs_closed_candidates"
     MARTS_ENRICH_AE_CLOSED_REPLACED_SAME_SIREN: str = (
         "marts_enrich_acteurs_closed_suggest_replaced_same_siren"
