1cdc304f011d7c5820e1acddc91522d1b3bddd52
maxcorbeau <contact@maxcorbeau.com>
2025-04-16 10:35:40 +0200

    cont. suppression RGPD & move data_reconstruct

--- dags/cluster/tasks/business_logic/misc/data_serialize_reconstruct.py
+++ dags/cluster/tasks/business_logic/misc/data_serialize_reconstruct.py
@@ -4,7 +4,6 @@ to lose Python/Django objects"""
 
 from datetime import datetime
 
-from django.contrib.gis.geos import Point
 from django.db import models
 
 
@@ -46,56 +45,6 @@ def data_serialize(model: type[models.Model], data: dict) -> dict:
             result["latitude"] = data["location"].y
         elif isinstance(value, datetime):
             result[key] = value.isoformat()
-        else:
-            result[key] = value
-
-    return result
-
-
-def data_reconstruct(model: type[models.Model], data_src: dict) -> dict:
-    """
-    Reconstruct data ready to use in Django model.
-
-    Args:
-    - model_class: The Django model class.
-    - data: The dictionary containing the data to reconstruct.
-
-    Returns:
-    - An instance of the model with the data populated.
-    """
-    result = {}
-    data = data_src.copy()
-
-    if "longitude" in data and "latitude" in data:
-        result["location"] = Point(data.pop("longitude"), data.pop("latitude"))
-
-    for key, value in data.items():
-        field = model._meta.get_field(key)
-
-        # We don't try to be fancy with None, it's None
-        if value is None:
-            # Same explanation as in data_serialize
-            if key == "location":
-                continue
-            else:
-                result[key] = value
-        elif isinstance(field, models.ForeignKey):
-            # Normalizing to {field} from {field}_id so all fields are
-            # represented in their Django flavour
-            if key.endswith("_id"):
-                try:
-                    key_no_id = key.rstrip("_id")
-                    field = model._meta.get_field(key_no_id)
-                    key = key_no_id
-                except Exception:
-                    pass
-
-            # Retrieving the related instance if it's not already an instance
-            if not isinstance(value, field.related_model):  # type: ignore
-                value = field.related_model.objects.get(pk=value)  # type: ignore
-
-            result[key] = value
-
         else:
             result[key] = value
 
