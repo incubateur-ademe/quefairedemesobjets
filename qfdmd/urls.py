224446099f3c8475fcdc0f94e267cebe7393f43d
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-04-22 12:30:39 +0200

    remove useless change

--- qfdmd/urls.py
+++ qfdmd/urls.py
@@ -5,7 +5,6 @@ from django.views.generic import RedirectView
 from qfdmd.views import (
     CMSPageDetailView,
     ContactFormView,
-    HomeView,
     SynonymeDetailView,
     get_assistant_script,
     get_sw,
@@ -35,5 +34,4 @@ urlpatterns = [
     path("dechet/<slug:slug>/", SynonymeDetailView.as_view(), name="synonyme-detail"),
     path("iframe.js", get_assistant_script, name="script"),
     path("sw.js", get_sw, name="service-worker"),
-    path("", HomeView.as_view(), name="home"),
 ]
