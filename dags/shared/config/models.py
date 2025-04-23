60b373f063dac06cdb0c18ac90379b2cdba0f877
maxcorbeau <contact@maxcorbeau.com>
2025-04-23 13:21:11 +0200

    aider le linter

--- dags/shared/config/models.py
+++ dags/shared/config/models.py
@@ -28,7 +28,7 @@ def config_to_airflow_params(model_instance: BaseModel) -> dict[str, Param]:
     model_cls = model_instance.__class__
     for field_name, field_info in model_cls.model_fields.items():
         field_value = getattr(model_instance, field_name)  # Get value from instance
-
+        assert field_info.annotation is not None
         params[field_name] = Param(
             field_value,
             type=PYDANTIC_TYPE_TO_AIRFLOW_TYPE[field_info.annotation],
