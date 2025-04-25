b2544079dafc56a275a9e0e33929778090efc695
Nicolas Oudard <nicolas@oudard.org>
2025-04-25 10:57:23 +0200

    utilisation de transaction pour gérer l'option dry-run

--- qfdmo/management/commands/update_external_ids.py
+++ qfdmo/management/commands/update_external_ids.py
@@ -2,10 +2,9 @@ import argparse
 import json
 
 from django.core.management.base import BaseCommand
+from django.db import transaction
 
 from qfdmo.models.acteur import Acteur, ActeurStatus, RevisionActeur
-
-CHUNK = 1000
 
 
 class Command(BaseCommand):
@@ -39,48 +38,54 @@ class Command(BaseCommand):
         with open(mapping_file, "r") as f:
             mapping = json.load(f)
 
-        for old_id, new_id in mapping.items():
-            # Check if the ids are not empty
-            if old_id and new_id:
-                self.stdout.write(
-                    self.style.SUCCESS(f"Updating `{old_id}` to `{new_id}`")
-                )
-            else:
-                self.stdout.write(
-                    self.style.WARNING(
-                        f"Skipping `{old_id}` to `{new_id}` : one of the ids is empty"
+        with transaction.atomic():
+            for old_id, new_id in mapping.items():
+                # Check if the ids are not empty
+                if old_id and new_id:
+                    self.stdout.write(
+                        self.style.SUCCESS(f"Mapping `{old_id}` to `{new_id}`")
                     )
-                )
-                continue
+                else:
+                    self.stdout.write(
+                        self.style.WARNING(
+                            f"Skipping `{old_id}` to `{new_id}` : one of the ids is"
+                            " empty"
+                        )
+                    )
+                    continue
 
-            # test acteur with this new external id already exists
-            acteur_from_db = Acteur.objects.filter(
-                identifiant_externe=new_id, source__code=source_code
-            ).first()
-            id_index = 0
-            while acteur_from_db:
-                id_index += 1
-                new_id_indexed = f"{new_id}_{id_index}"
-                acteur_from_db = Acteur.objects.filter(
-                    identifiant_externe=new_id_indexed, source__code=source_code
+                # Update acteur if exists
+                acteur = Acteur.objects.filter(
+                    identifiant_externe=old_id, source__code=source_code
                 ).first()
 
-            try:
-                new_id = new_id_indexed
-                statut = ActeurStatus.INACTIF
-            except NameError:
+                if not acteur:
+                    self.stdout.write(self.style.WARNING(f"Acteur {old_id} not found"))
+                    continue
+
+                # test acteur with this new external id already exists
+                acteur_from_db = Acteur.objects.filter(
+                    identifiant_externe=new_id, source__code=source_code
+                ).first()
+                id_index = 0
+                while acteur_from_db:
+                    self.stdout.write(
+                        self.style.WARNING(
+                            f"Acteur {acteur_from_db.identifiant_externe} already"
+                            " exists, trying to find an unused id"
+                        )
+                    )
+                    id_index += 1
+                    new_id_indexed = f"{new_id}_{id_index}"
+                    acteur_from_db = Acteur.objects.filter(
+                        identifiant_externe=new_id_indexed, source__code=source_code
+                    ).first()
+
                 statut = ActeurStatus.ACTIF
+                if id_index:
+                    new_id = f"{new_id}_{id_index}"
+                    statut = ActeurStatus.INACTIF
 
-            # Update acteur if exists
-            acteur = Acteur.objects.filter(
-                identifiant_externe=old_id, source__code=source_code
-            ).first()
-
-            if not acteur:
-                self.stdout.write(self.style.WARNING(f"Acteur {old_id} not found"))
-                continue
-
-            if not dry_run:
                 self.stdout.write(
                     self.style.SUCCESS(
                         f"Updating acteur {acteur.identifiant_unique} to {new_id}"
@@ -89,31 +94,23 @@ class Command(BaseCommand):
                 acteur.identifiant_externe = new_id
                 acteur.statut = statut
                 acteur.save()
-            else:
-                self.stdout.write(
-                    self.style.WARNING(
-                        f"Dry run: would update Acteur {old_id} to {new_id}"
-                    )
-                )
 
-            # Update revision acteur if exists
-            revision_acteur = RevisionActeur.objects.filter(
-                identifiant_externe=old_id, source__code=source_code
-            ).first()
+                # Update revision acteur if exists
+                revision_acteur = RevisionActeur.objects.filter(
+                    identifiant_externe=old_id, source__code=source_code
+                ).first()
 
-            if revision_acteur and not dry_run:
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        f"Updating revision acteur {revision_acteur.identifiant_unique}"
-                        f" to {new_id}"
+                if revision_acteur:
+                    self.stdout.write(
+                        self.style.SUCCESS(
+                            "Updating revision acteur "
+                            f"{revision_acteur.identifiant_unique} to {new_id}"
+                        )
                     )
-                )
-                revision_acteur.identifiant_externe = new_id
-                revision_acteur.statut = statut
-                revision_acteur.save()
-            if revision_acteur and dry_run:
-                self.stdout.write(
-                    self.style.WARNING(
-                        f"Dry run: would update RevisionActeur {old_id} to {new_id}"
-                    )
-                )
+                    revision_acteur.identifiant_externe = new_id
+                    revision_acteur.statut = statut
+                    revision_acteur.save()
+
+            if dry_run:
+                # Rollback the transaction
+                raise Exception("Rolling back transaction because of dry run")
