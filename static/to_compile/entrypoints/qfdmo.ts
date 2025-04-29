d5092f248b34571a9fd740533d2bf3f98d8f3c72
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-04-29 17:35:49 +0200

    wip

--- static/to_compile/entrypoints/qfdmo.ts
+++ static/to_compile/entrypoints/qfdmo.ts
@@ -1,7 +1,3 @@
-import "@gouvfr/dsfr/dist/dsfr.module.js"
-import { Application } from "@hotwired/stimulus"
-import * as Turbo from "@hotwired/turbo"
-
 import AddressAutocompleteController from "../js/address_autocomplete_controller"
 import AnalyticsController from "../js/analytics_controller"
 import CopyController from "../js/copy_controller"
@@ -14,8 +10,6 @@ import SsCatObjectAutocompleteController from "../js/ss_cat_object_autocomplete_
 import "../js/browser_check"
 import "../js/iframe"
 
-window.stimulus = Application.start()
-// stimulus.debug = true
 stimulus.register("map", MapController)
 stimulus.register("ss-cat-object-autocomplete", SsCatObjectAutocompleteController)
 stimulus.register("address-autocomplete", AddressAutocompleteController)
@@ -24,5 +18,3 @@ stimulus.register("analytics", AnalyticsController)
 stimulus.register("autocomplete", GenericAutocompleteController)
 stimulus.register("copy", CopyController)
 stimulus.register("scroll", ScrollController)
-
-Turbo.session.drive = false
