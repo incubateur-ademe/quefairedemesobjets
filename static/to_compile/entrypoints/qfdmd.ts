2c44ad82c3baa6c7e75205e5226e0dbd90882cbd
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-05-05 12:09:51 +0200

    wip

--- static/to_compile/entrypoints/qfdmd.ts
+++ static/to_compile/entrypoints/qfdmd.ts
@@ -1,6 +1,5 @@
-import * as Turbo from "@hotwired/turbo"
-import { Application } from "@hotwired/stimulus"
 import '@iframe-resizer/child'
+import "./qfdmo"
 
 // QFDMD
 import SearchController from "../js/controllers/assistant/search"
@@ -8,31 +7,13 @@ import BlinkController from "../js/controllers/assistant/blink"
 import AnalyticsController from "../js/controllers/assistant/analytics"
 import CopyController from "../js/copy_controller"
 
-// QFDMO
-import AddressAutocompleteController from "../js/address_autocomplete_controller"
-import GenericAutocompleteController from "../js/generic_autocomplete_controller"
-import MapController from "../js/map_controller"
-import ScrollController from "../js/scroll_controller"
-import SearchSolutionFormController from "../js/search_solution_form_controller"
-import SsCatObjectAutocompleteController from "../js/ss_cat_object_autocomplete_controller"
-
-import "../js/browser_check"
-import "../js/iframe"
-
-window.stimulus = Application.start()
+// Handled by qfdmo.ts
+// window.stimulus = Application.start()
 stimulus.debug = document.body.dataset.stimulusDebug
 stimulus.register("search", SearchController)
 stimulus.register("blink", BlinkController)
 stimulus.register("copy", CopyController)
 stimulus.register("analytics", AnalyticsController)
 
-stimulus.register("map", MapController)
-stimulus.register("ss-cat-object-autocomplete", SsCatObjectAutocompleteController)
-stimulus.register("address-autocomplete", AddressAutocompleteController)
-stimulus.register("search-solution-form", SearchSolutionFormController)
-// stimulus.register("analytics", AnalyticsController)
-stimulus.register("autocomplete", GenericAutocompleteController)
-stimulus.register("copy", CopyController)
-stimulus.register("scroll", ScrollController)
-
-Turbo.session.drive = false;
+// Handle by qfdmo
+// Turbo.session.drive = false;
--- static/to_compile/entrypoints/qfdmo.ts
+++ static/to_compile/entrypoints/qfdmo.ts
@@ -1,3 +1,7 @@
+import "@gouvfr/dsfr/dist/dsfr.module.js"
+import { Application } from "@hotwired/stimulus"
+import * as Turbo from "@hotwired/turbo"
+
 import AddressAutocompleteController from "../js/address_autocomplete_controller"
 import AnalyticsController from "../js/analytics_controller"
 import CopyController from "../js/copy_controller"
@@ -10,6 +14,8 @@ import SsCatObjectAutocompleteController from "../js/ss_cat_object_autocomplete_
 import "../js/browser_check"
 import "../js/iframe"
 
+window.stimulus = Application.start()
+// stimulus.debug = true
 stimulus.register("map", MapController)
 stimulus.register("ss-cat-object-autocomplete", SsCatObjectAutocompleteController)
 stimulus.register("address-autocomplete", AddressAutocompleteController)
@@ -18,3 +24,5 @@ stimulus.register("analytics", AnalyticsController)
 stimulus.register("autocomplete", GenericAutocompleteController)
 stimulus.register("copy", CopyController)
 stimulus.register("scroll", ScrollController)
+
+Turbo.session.drive = false
