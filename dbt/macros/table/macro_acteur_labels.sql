91e727a68025908db495c56ac89da09462f6a996
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2025-05-05 14:29:42 +0200

    fix DBT acteur qui fait appelle à des tables vueacteur plutôt qu'à int_acteur (#1597)
    fix DBT acteur ui fait appelle à des tables vueacteur plutôt qu'à int_acteur

--- dbt/macros/table/macro_acteur_labels.sql
+++ dbt/macros/table/macro_acteur_labels.sql
@@ -2,18 +2,18 @@
 
 WITH nochild_acteur_labels AS (
     SELECT
-        al.vueacteur_id AS acteur_id,
+        al.acteur_id AS acteur_id,
         al.labelqualite_id AS labelqualite_id
-    FROM qfdmo_vueacteur_labels al
-    INNER JOIN {{ ref(ephemeral_filtered_acteur) }} AS a ON al.vueacteur_id = a.identifiant_unique AND a.parent_id is null
-    GROUP BY al.vueacteur_id, al.labelqualite_id
+    FROM {{ ref( 'int_acteur_labels' )}} AS al
+    INNER JOIN {{ ref(ephemeral_filtered_acteur) }} AS a ON al.acteur_id = a.identifiant_unique AND a.parent_id is null
+    GROUP BY al.acteur_id, al.labelqualite_id
 ),
 parentacteur_labels AS (
     SELECT
         a.parent_id AS acteur_id,
         al.labelqualite_id AS labelqualite_id
-    FROM qfdmo_vueacteur_labels al
-    INNER JOIN {{ ref(ephemeral_filtered_acteur) }} AS a ON al.vueacteur_id = a.identifiant_unique AND a.parent_id is not null
+    FROM {{ ref( 'int_acteur_labels' )}} AS al
+    INNER JOIN {{ ref(ephemeral_filtered_acteur) }} AS a ON al.acteur_id = a.identifiant_unique AND a.parent_id is not null
     GROUP BY a.parent_id, al.labelqualite_id
 ),
 acteur_labels AS (
