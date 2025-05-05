c57ad0ab0e2c12e0f850d36227968f4673ad8602
Nicolas Oudard <nicolas@oudard.org>
2025-05-05 12:35:34 +0200

    fix DBT acteur ui fait appelle à des tables vueacteur plutôt qu'à int_acteur

--- dbt/macros/table/macro_acteur_acteur_services.sql
+++ dbt/macros/table/macro_acteur_acteur_services.sql
@@ -2,18 +2,18 @@
 
 WITH nochild_acteur_acteur_services AS (
     SELECT
-        aas.vueacteur_id AS acteur_id,
+        aas.acteur_id AS acteur_id,
         aas.acteurservice_id AS acteurservice_id
-    FROM qfdmo_vueacteur_acteur_services aas
-    INNER JOIN {{ ref(ephemeral_filtered_acteur) }} AS a ON aas.vueacteur_id = a.identifiant_unique AND a.parent_id is null
-    GROUP BY aas.vueacteur_id, aas.acteurservice_id
+    FROM {{ ref( 'int_acteur_acteur_services' )}} AS aas
+    INNER JOIN {{ ref(ephemeral_filtered_acteur) }} AS a ON aas.acteur_id = a.identifiant_unique AND a.parent_id is null
+    GROUP BY aas.acteur_id, aas.acteurservice_id
 ),
 parentacteur_acteur_services AS (
     SELECT
         a.parent_id AS acteur_id,
         aas.acteurservice_id AS acteurservice_id
-    FROM qfdmo_vueacteur_acteur_services aas
-    INNER JOIN {{ ref(ephemeral_filtered_acteur) }} AS a ON aas.vueacteur_id = a.identifiant_unique AND a.parent_id is not null
+    FROM {{ ref( 'int_acteur_acteur_services' )}} AS aas
+    INNER JOIN {{ ref(ephemeral_filtered_acteur) }} AS a ON aas.acteur_id = a.identifiant_unique AND a.parent_id is not null
     GROUP BY a.parent_id, aas.acteurservice_id
 ),
 acteur_acteur_services AS (
