10d181b54228461a57e9998b2ec640f06279329a
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-05-06 16:34:48 +0200

    inherit from carte config in produit

--- qfdmo/views/carte.py
+++ qfdmo/views/carte.py
@@ -44,7 +44,8 @@ class CarteSearchActeursView(SearchActeursView):
 class ProductCarteView(CarteSearchActeursView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context.update(is_carte=True, without_branding=True)
+        carte_config = CarteConfig(no_branding=True)
+        context.update(carte_config=carte_config)
         return context
 
 
