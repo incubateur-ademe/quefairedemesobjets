fc7ff000ef030a3602aaa9e33130b6e80cb976d3
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2025-05-27 15:52:50 +0200

    Résolution des erreur SSL quand une tache dure trop longtemps (#1640)

---
+++ dags/airflow_local_settings.py
@@ -0,0 +1,6 @@
+keepalive_kwargs = {
+    "keepalives": 1,  # enable keepalive
+    "keepalives_idle": 60,  # idle time before sending keepalive
+    "keepalives_interval": 60,  # interval between keepalive packets
+    "keepalives_count": 5,  # number of keepalive packets to send before closing
+}
