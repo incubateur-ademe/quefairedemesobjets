f9fb23fb3360e09d33465c667ce527b9e41803f4
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-06-04 18:04:22 +0200

    Add tests

--- integration_tests/carte/test_use_carte_config.py
+++ integration_tests/carte/test_use_carte_config.py
@@ -19,14 +19,21 @@ def get_carte_config_response_and_soup(client):
 
 @pytest.mark.django_db
 class TestCarteConfig:
-    def test_carte_config_context_name(self, get_carte_config_response_and_soup):
+    @pytest.fixture
+    def params(self):
+        return {"adresse": "Paris", "longitude": "2.347", "latitude": "48.859"}
+
+    def test_carte_config_context_name(
+        self, get_carte_config_response_and_soup, params
+    ):
         carte_config = CarteConfigFactory(hide_legend=True)
         response, _ = get_carte_config_response_and_soup(carte_config.slug)
         assert "carte_config" in response.context
 
-    def test_legend_is_visible_by_default(self, get_carte_config_response_and_soup):
+    def test_legend_is_visible_by_default(
+        self, get_carte_config_response_and_soup, params
+    ):
         carte_config = CarteConfigFactory()  # hide_legend is False by default
-        params = {"adresse": "Paris", "longitude": "2.347", "latitude": "48.859"}
         response, soup = get_carte_config_response_and_soup(
             carte_config.slug,
             params,
@@ -35,8 +42,7 @@ class TestCarteConfig:
         assert soup.find(attrs={"data-testid": "carte-legend"}) is not None
         assert soup.find(attrs={"data-testid": "carte-legend-mobile"}) is not None
 
-    def test_legend_can_be_hidden(self, get_carte_config_response_and_soup):
-        params = {"adresse": "Paris", "longitude": "2.347", "latitude": "48.859"}
+    def test_legend_can_be_hidden(self, get_carte_config_response_and_soup, params):
         carte_config = CarteConfigFactory(hide_legend=True)
         response, soup = get_carte_config_response_and_soup(
             carte_config.slug,
@@ -45,3 +51,16 @@ class TestCarteConfig:
         assert soup.find(attrs={"data-testid": "legend-mobile-button"}) is None
         assert soup.find(attrs={"data-testid": "carte-legend"}) is None
         assert soup.find(attrs={"data-testid": "carte-legend-mobile"}) is None
+
+    def test_preview_screen(self, get_carte_config_response_and_soup):
+        carte_config = CarteConfigFactory(
+            preview_title="Youpi",
+            preview_content="Coucou",
+        )
+        response, soup = get_carte_config_response_and_soup(
+            carte_config.slug,
+        )
+        assert soup.find(attrs={"data-testid": "preview-title"}).text.strip() == "Youpi"
+        assert (
+            soup.find(attrs={"data-testid": "preview-content"}).text.strip() == "Coucou"
+        )
