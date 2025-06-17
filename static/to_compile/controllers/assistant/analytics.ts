ff5f25c483d23fed3c01ea8cc22aa4f930096475
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-06-17 09:30:51 +0200

    Remove useless change

--- static/to_compile/controllers/assistant/analytics.ts
+++ static/to_compile/controllers/assistant/analytics.ts
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 import { InteractionType as PosthogUIInteractionType, PosthogEventType } from "../../js/types"
-import posthog from "posthog-js"
+import posthog, { PostHogConfig } from "posthog-js"
 import { areWeInAnIframe } from "../../js/iframe"
 
 type PersonProperties = {
@@ -20,9 +20,10 @@ export default class extends Controller<HTMLElement> {
   declare readonly posthogDebugValue
   declare readonly posthogKeyValue
   declare readonly userAdminValue
-  declare readonly userConversionScoreValue
   declare readonly userEmailValue
   declare readonly userUsernameValue
+  declare userConversionScoreValue
+
   personProperties: PersonProperties = {
     iframe: false
   }
@@ -32,12 +33,14 @@ export default class extends Controller<HTMLElement> {
     posthogDebug: Boolean,
     posthogKey: String,
     userAdmin: Boolean,
-    userConversionScore: Number,
+    userConversionScore: Object,
     userEmail: String,
     userUsername: String,
   }
 
-  posthogConfig = {
+  intersectionObserverThreshold = 0.1
+
+  posthogConfig: Partial<PostHogConfig> = {
     api_host: "https://eu.posthog.com",
     autocapture: false,
     capture_pageview: false,
@@ -45,30 +48,62 @@ export default class extends Controller<HTMLElement> {
     persistence: "memory",
   }
 
-  userActionScore = {
-    homePageView: 0,
+  // The user conversion score is computed from several actions : page views,
+  // clicks on specific UI areas, etc.
+  //
+  // Each action has it own score, for example a specific action
+  // might score 2 points because of its importance.
+  //
+  // Each action is listed in the object below with the format :
+  // { action name : points added when action is triggered }
+  userConversionScoreConfig = {
+    homePageView: 1,
     produitPageView: 1,
+    userInteractionWithMap: 1,
   }
 
   initialize(): void {
     posthog.init(this.posthogKeyValue, this.posthogConfig)
-    this.#checkAuthenticatedUser()
+    this.#identifyAuthenticatedUser()
     this.#checkIfWeAreInAnIframe()
-    this.#fillSessionStorageWithAction()
+    this.#syncSessionStorageWithLocalConversionScore()
     this.#setupIntersectionObserverEvents()
+    this.#setInitialActionValue()
+    this.#computeConversionScoreFromSessionStorage()
 
     if (this.posthogDebugValue) {
       posthog.debug()
     }
   }
 
-  #fillSessionStorageWithAction() {
-    if (this.actionValue) {
-      sessionStorage.setItem(this.actionValue, "1")
+  userConversionScoreValueChanged(value, previousValue) {
+    // TODO: compare values, use lodash ?
+    // if (value !== previousValue) {
+    this.#captureUserConversionScore()
+    this.#syncSessionStorageWithLocalConversionScore()
+    // }
+  }
+
+
+  // An initial action can be set in the template, as a Stimulus Value.
+  // This value is read when initializing the controller, and set the default score.
+  // For example, viewing the homepage or a Produit page scores 1 point.
+  #setInitialActionValue() {
+    if (this.actionValue && this.userConversionScoreConfig[this.actionValue]) {
+      this.userConversionScoreValue[this.actionValue] = this.userConversionScoreConfig[this.actionValue]
     }
   }
 
-  #checkAuthenticatedUser() {
+  #syncSessionStorageWithLocalConversionScore() {
+    for (const key of Object.keys(this.userConversionScoreConfig)) {
+      sessionStorage.setItem(key, this.userConversionScoreConfig[this.actionValue])
+    }
+  }
+
+  // This is used to discriminate authenticated users (LVAO Team)
+  // from anonymous users.
+  // In PostHog, authenticated users are excluded from insights.
+  #identifyAuthenticatedUser() {
     if (this.userUsernameValue) {
       posthog.identify(this.userUsernameValue, {
         email: this.userEmailValue,
@@ -77,40 +112,43 @@ export default class extends Controller<HTMLElement> {
     }
   }
 
+  // The iframe URL parameter is not always set :
+  // - After a navigaation (click on a link)
+  // - If the iframe integration does not use our script
+  // In all these cases, we still want to determine
+  // whether the user browse inside an iframe or not.
   #checkIfWeAreInAnIframe() {
-    // The iframe URL parameter is not always set :
-    // - After a navigaation (click on a link)
-    // - If the iframe integration does not use our script
-    // In all these cases, we still want to determine
-    // whether the user browse inside an iframe or not.
     const [weAreInAnIframe, referrer] = areWeInAnIframe()
     this.personProperties.iframe = weAreInAnIframe
     this.personProperties.iframeReferrer = referrer
   }
 
+  #computeConversionScoreFromSessionStorage() {
+    let conversionScore = {}
+    for (const key of Object.keys(this.userConversionScoreConfig)) {
+      if (sessionStorage.getItem(key)) {
+        conversionScore[key] = sessionStorage.getItem(key)
+      }
+    }
+
+    this.userConversionScoreValue = conversionScore
+  }
+
+  #updateDebugInspectorUI(conversionScore: number) {
+    const posthogBannerConversionScore = document.querySelector("#posthog-banner-conversion-score")
+    if (posthogBannerConversionScore) {
+      posthogBannerConversionScore.textContent = conversionScore.toString()
+    }
+  }
+
   async #captureUserConversionScore() {
-    // We allow a few seconds to ensure that the sesionstorage has been filled.
-    // As it is not possible to listen on sessionStorage updates...
-    setTimeout(() => {
-      let conversionScore = 0
-      for (const [key, value] of Object.entries(this.userActionScore)) {
-        if (sessionStorage.getItem(key)) {
-          conversionScore += value
-        }
-      }
-
-      posthog.capture("$set", {
-        $set: {
-          ...this.personProperties,
-          conversionScore,
-        },
-      })
-
-      const posthogBannerConversionScore = document.querySelector("#posthog-banner-conversion-score")
-      if (posthogBannerConversionScore) {
-        posthogBannerConversionScore.textContent = conversionScore.toString()
-      }
-    }, 1000)
+    posthog.capture("$set", {
+      $set: {
+        ...this.personProperties,
+        conversionScore: this.userConversionScoreValue,
+      },
+    })
+    this.#updateDebugInspectorUI(conversionScore)
   }
 
   #setupIntersectionObserverEvents() {
@@ -119,17 +157,31 @@ export default class extends Controller<HTMLElement> {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
             posthog.capture("$pageview")
-            this.#captureUserConversionScore()
+            // TODO: increase userConversionScore
             observer.unobserve(entry.target)
           }
         })
       },
       {
         root: null,
-        threshold: 0.1, // Trigger when at least 10% of the page is visible
+        threshold: this.intersectionObserverThreshold,  // Trigger when at least 10% of the page is visible
       },
     )
 
     observer.observe(document.body)
+  }
+
+  captureUIInteraction(UIInteractionType: PosthogUIInteractionType) {
+    posthog.capture("$set", {
+      ui_interaction_type: UIInteractionType,
+    })
+  }
+
+  captureInteractionWithSolutionDetails() {
+    this.captureUIInteraction("solution_details")
+  }
+
+  captureInteractionWithMap() {
+    this.captureUIInteraction("map")
   }
 }
