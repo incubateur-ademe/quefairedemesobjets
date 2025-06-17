00f47dff1ec3d4a394b63faed1d6cc942786f941
Nicolas Oudard <nicolas@oudard.org>
2025-06-17 14:39:42 +0200

    Option permettant de garder les données du parent lors d'un clustering avec parent existant

--- dags/cluster/tasks/business_logic/cluster_acteurs_parents_choose_data.py
+++ dags/cluster/tasks/business_logic/cluster_acteurs_parents_choose_data.py
@@ -3,17 +3,10 @@ from typing import Any
 
 import pandas as pd
 from cluster.config.constants import COL_PARENT_DATA_NEW, FIELDS_PARENT_DATA_EXCLUDED
-from django.forms.models import model_to_dict
+
 from utils.django import django_setup_full
 
 django_setup_full()
-
-from data.models.change import COL_CHANGE_MODEL_NAME  # noqa: E402
-from data.models.changes import (  # noqa: E402
-    ChangeActeurCreateAsParent,
-    ChangeActeurKeepAsParent,
-)
-from qfdmo.models.acteur import Acteur, DisplayedActeur, RevisionActeur  # noqa: E402
 
 logger = logging.getLogger(__name__)
 
@@ -71,77 +64,84 @@ def field_pick_value(
     return None
 
 
-def parent_choose_data(
-    parent_data_before: dict,
-    acteurs_revision: list[dict],
-    acteurs_base: list[dict],
-    fields_to_include: list[str],
-    exclude_source_ids: list[int],
-    prioritize_source_ids: list[int],
-    keep_empty: bool = False,
-) -> dict:
-    """Selects and assigns data for a parent. Since the data is
-    intended to create or enrich a revision, we ensure that the chosen data
-    satisfies the RevisionActor model.
-
-    Args:
-        acteurs (list[dict]): list of acteurs to consider = Priority 1
-        fields_to_include (list[str]): fields to include in the result = Fallback
-        exclusion_always_sources (list[str]): sources to exclude for all fields
-        priority_always_sources (list[str]): sources to prioritize for all fields
-        keep_empty (bool): if True, keep None values in the result if they come
-            from priority sources
-    """
-
-    # Priority = as per priority list OR rest by default
-    def source_priority(a):
-        return (
-            prioritize_source_ids.index(a["source_id"])
-            if a["source_id"] in prioritize_source_ids
-            else float("inf")
-        )
-
-    # Acteurs to consider: first revisions, then base, but not from excluded sources
-    acteurs = []
-    acteurs += [a for a in acteurs_revision if a["source_id"] not in exclude_source_ids]
-    acteurs.sort(key=source_priority)
-    acteurs += [a for a in acteurs_base if a["source_id"] not in exclude_source_ids]
-    acteurs.sort(key=source_priority)
-
-    # Fields: make sure we don't include unwanted fields
-    fields = fields_to_include_clean(fields_to_include)
-
-    result = {}
-    for field in fields:
-        value_old = parent_data_before.get(field)
-        values = [a.get(field) for a in acteurs]
-        value_new = field_pick_value(
-            field,
-            values,
-            keep_empty,
-        )
-        if value_new == value_old:
-            continue
-        if value_new is None and not keep_empty:
-            continue
-        result[field] = value_new
-
-    return result
-
-
 def cluster_acteurs_parents_choose_data(
     df_clusters: pd.DataFrame,
     fields_to_include: list[str],
     exclude_source_ids: list[int],
     prioritize_source_ids: list[int],
     keep_empty: bool = False,
+    keep_parent_data_by_default: bool = True,
 ) -> pd.DataFrame:
+
+    from django.db.models import QuerySet
+
+    from data.models.change import COL_CHANGE_MODEL_NAME
+    from data.models.changes import ChangeActeurCreateAsParent, ChangeActeurKeepAsParent
+    from qfdmo.models.acteur import Acteur, RevisionActeur
+
+    def parent_choose_data(
+        parent: RevisionActeur | None,
+        acteurs_revision: QuerySet[RevisionActeur],
+        acteurs_base: QuerySet[Acteur],
+        fields_to_include: list[str],
+        exclude_source_ids: list[int],
+        prioritize_source_ids: list[int],
+        keep_empty: bool = False,
+        keep_parent_data_by_default: bool = True,
+    ) -> dict:
+        """Selects and assigns data for a parent. Since the data is
+        intended to create or enrich a revision, we ensure that the chosen data
+        satisfies the RevisionActor model.
+
+        Args:
+            acteurs (list[dict]): list of acteurs to consider = Priority 1
+            fields_to_include (list[str]): fields to include in the result = Fallback
+            exclusion_always_sources (list[str]): sources to exclude for all fields
+            priority_always_sources (list[str]): sources to prioritize for all fields
+            keep_empty (bool): if True, keep None values in the result if they come
+                from priority sources
+        """
+
+        # Priority = as per priority list OR rest by default
+        def source_priority(a):
+            return (
+                prioritize_source_ids.index(a.source.id)
+                if a.source and a.source.id in prioritize_source_ids
+                else float("inf")
+            )
+
+        # Acteurs to consider: first revisions, then base, but not from excluded sources
+        acteurs = list(acteurs_revision) + list(acteurs_base)
+        acteurs.sort(key=source_priority)
+        if parent and keep_parent_data_by_default:
+            acteurs = [parent] + acteurs
+
+        # Fields: make sure we don't include unwanted fields
+        fields = fields_to_include_clean(fields_to_include)  # TODO : check which
+
+        result = {}
+        for field in fields:
+            value_old = getattr(parent, field) if parent else None
+            values = [getattr(a, field) for a in acteurs]
+            value_new = field_pick_value(
+                field,
+                values,
+                keep_empty,
+            )
+            if value_new == value_old:
+                continue
+            if value_new is None and not keep_empty:
+                continue
+            result[field] = value_new
+
+        return result
+
     """For all selected parents in clusters, select the data to use"""
     fields = fields_to_include_clean(fields_to_include)
     fields += ["source_id"]
 
     df_clusters[COL_PARENT_DATA_NEW] = None
-    for cluster_id, df_cluster in df_clusters.groupby("cluster_id"):
+    for _, df_cluster in df_clusters.groupby("cluster_id"):
         filter_parent = df_cluster[COL_CHANGE_MODEL_NAME].isin(
             [
                 ChangeActeurCreateAsParent.name(),
@@ -156,38 +156,40 @@ def cluster_acteurs_parents_choose_data(
         assert len(df_parents) == 1, "Should have 1 parent per cluster"
         parent_iloc = df_parents.index[0]
         parent_id = df_parents["identifiant_unique"].iloc[0]
-        parent_data_before = {}
 
         df_acteurs = df_cluster[~filter_parent]
         acteur_ids = df_acteurs["identifiant_unique"].unique()
 
         # We only need to get the parent's before data IF
         # it was an existing one (i.e. one to keep)
+        parent = None
         if (
             df_parents[COL_CHANGE_MODEL_NAME].values[0]
             == ChangeActeurKeepAsParent.name()
         ):
             try:
-                parent = DisplayedActeur.objects.get(pk=parent_id)
-            except DisplayedActeur.DoesNotExist:
+                parent = RevisionActeur.objects.get(pk=parent_id)
+            except RevisionActeur.DoesNotExist:
                 ptype = df_parents[COL_CHANGE_MODEL_NAME].values[0]
-                raise ValueError(f"Parent {ptype} {parent_id} pas dans displayed!!!")
-            parent_data_before = model_to_dict(parent)
+                raise ValueError(f"Parent {ptype} {parent_id} pas dans revision!!!")
 
         # We construct a list of acteurs, 1st from revision (higher prio
         # as might contain business-approved changes) and then from base
-        acteurs_revision = RevisionActeur.objects.filter(pk__in=acteur_ids).values(
-            *fields
+        acteurs_revision = RevisionActeur.objects.filter(pk__in=acteur_ids).exclude(
+            source__id__in=exclude_source_ids
         )
-        acteurs_base = Acteur.objects.filter(pk__in=acteur_ids).values(*fields)
+        acteurs_base = Acteur.objects.filter(pk__in=acteur_ids).exclude(
+            source__id__in=exclude_source_ids
+        )
         parent_data_new = parent_choose_data(
-            parent_data_before=parent_data_before,
-            acteurs_revision=list(acteurs_revision),
-            acteurs_base=list(acteurs_base),
+            parent=parent,
+            acteurs_revision=acteurs_revision,
+            acteurs_base=acteurs_base,
             fields_to_include=fields,
             exclude_source_ids=exclude_source_ids,
             prioritize_source_ids=prioritize_source_ids,
             keep_empty=keep_empty,
+            keep_parent_data_by_default=keep_parent_data_by_default,
         )
 
         df_clusters.at[parent_iloc, COL_PARENT_DATA_NEW] = parent_data_new
