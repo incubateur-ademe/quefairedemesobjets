79d189c2b9e933496297c266f9d9d5ce67b8265b
Nicolas Oudard <nicolas@oudard.org>
2025-06-25 10:41:04 +0200

    Remise à zero des bbox lorsqu'on mets à jour les données de localisation

--- static/to_compile/controllers/assistant/state.ts
+++ static/to_compile/controllers/assistant/state.ts
@@ -1,39 +1,56 @@
 import { Controller } from "@hotwired/stimulus"
+import isEmpty from "lodash/isEmpty"
 import AdresseAutocompleteController from "../carte/address_autocomplete_controller"
 import SearchFormController from "../carte/search_solution_form_controller"
-import isEmpty from "lodash/isEmpty"
 
 export default class extends Controller<HTMLElement> {
   static values = {
-    "location": Object,
+    location: Object,
   }
   static outlets = ["address-autocomplete", "search-solution-form"]
   declare addressAutocompleteOutlets: Array<AdresseAutocompleteController>
   declare searchSolutionFormOutlets: Array<SearchFormController>
-  declare locationValue: { adresse: string | null, latitude: string | null, longitude: string | null }
+  declare locationValue: {
+    adresse: string | null
+    latitude: string | null
+    longitude: string | null
+  }
 
   connect() {
-    document.addEventListener("turbo:frame-load", this.fetchLocationFromSessionStorageOnFirstLoad.bind(this))
+    document.addEventListener(
+      "turbo:frame-load",
+      this.fetchLocationFromSessionStorageOnFirstLoad.bind(this),
+    )
   }
 
   fetchLocationFromSessionStorageOnFirstLoad(event) {
     if (!isEmpty(this.locationValue)) {
-      document.removeEventListener("turbo:frame-load", this.fetchLocationFromSessionStorageOnFirstLoad.bind(this))
+      document.removeEventListener(
+        "turbo:frame-load",
+        this.fetchLocationFromSessionStorageOnFirstLoad.bind(this),
+      )
       return
     }
 
     const nextLocationValue = {
-      "adresse": sessionStorage.getItem("adresse"),
-      "latitude": sessionStorage.getItem("latitude"),
-      "longitude": sessionStorage.getItem("longitude")
+      adresse: sessionStorage.getItem("adresse"),
+      latitude: sessionStorage.getItem("latitude"),
+      longitude: sessionStorage.getItem("longitude"),
     }
 
-    if (Object.values(nextLocationValue).find(value => !!value)) {
+    if (Object.values(nextLocationValue).find((value) => !!value)) {
       this.locationValue = nextLocationValue
     }
   }
 
+  resetBboxInputs() {
+    for (const outlet of this.searchSolutionFormOutlets) {
+      outlet.resetBboxInput()
+    }
+  }
+
   setLocation(event) {
+    this.resetBboxInputs()
     const nextLocationValue = event.detail
     const updatedLocationIsNotEmpty = !isEmpty(nextLocationValue)
     if (updatedLocationIsNotEmpty) {
@@ -94,5 +111,3 @@ export default class extends Controller<HTMLElement> {
     }
   }
 }
-
-
