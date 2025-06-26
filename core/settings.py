a7f57f06ac32f7faf9cce05f2aa74761011200e5
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-06-26 16:32:13 +0200

    wip

--- core/settings.py
+++ core/settings.py
@@ -256,7 +256,10 @@ TEMPLATES = [
                 "sites_faciles.content_manager.context_processors.skiplinks",
                 "sites_faciles.content_manager.context_processors.mega_menus",
             ],
-            "builtins": ["core.templatetags.iframe_tags"],
+            "builtins": [
+                "core.templatetags.iframe_tags",
+                "core.templatetags.querystring_tag",
+            ],
         },
     },
 ]
