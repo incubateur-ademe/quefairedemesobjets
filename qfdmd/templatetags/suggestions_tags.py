a60f640e81c20a3bacf8dc6d0d5de13b83a41088
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-06-26 21:07:39 +0200

    Add comment

--- qfdmd/templatetags/suggestions_tags.py
+++ qfdmd/templatetags/suggestions_tags.py
@@ -9,7 +9,17 @@ logger = logging.getLogger(__name__)
 
 
 @register.filter
-def as_tuple(suggestions):
+def as_tuple(suggestions) -> list[tuple[str, str]]:
+    """
+    Converts a list of Suggestion objects into (product name, product URL) tuples.
+
+    Each suggestion must have a `produit` with `nom` and `slug` attributes.
+    The URL is generated using the "qfdmd:synonyme-detail" named route.
+
+    :param suggestions: Iterable of Suggestion objects.
+    :type suggestions: iterable
+    :return: List of (name, URL) tuples.
+    """
     return [
         (
             suggestion.produit.nom,
