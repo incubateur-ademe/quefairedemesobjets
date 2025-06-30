c926dbead54fc34a14ccbd821b8410c62d94123a
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-06-30 10:40:18 +0200

    Fix fixtures installation

--- qfdmo/management/commands/loaddata_with_dynamic_ids.py
+++ qfdmo/management/commands/loaddata_with_dynamic_ids.py
@@ -2,6 +2,7 @@ from django.core.management.commands.loaddata import Command as LoadDataCommand
 from django.core.serializers.base import DeserializedObject
 
 from qfdmo.models import DisplayedActeur
+from qfdmo.models.acteur import DisplayedPropositionService
 
 
 class Command(LoadDataCommand):
@@ -10,5 +11,8 @@ class Command(LoadDataCommand):
         if isinstance(obj.object, DisplayedActeur):
             obj.object.generate_source_if_missing()
             obj.object.generate_identifiant_unique_if_missing()
+
+        if isinstance(obj.object, DisplayedPropositionService):
+            obj.object.generate_id_if_missing()
 
         return super().save_obj(obj)
