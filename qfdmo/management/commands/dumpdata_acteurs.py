1f2a1e467ede1fbccac170738df929ed36f3e679
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-06-30 21:06:48 +0200

    Fix fixtures

--- qfdmo/management/commands/dumpdata_acteurs.py
+++ qfdmo/management/commands/dumpdata_acteurs.py
@@ -6,37 +6,32 @@ from qfdmo.models.acteur import DisplayedPropositionService
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        pks = ",".join(
-            [
-                "alrebobinage__altech_distribution_sce_154142_reparation_0626645539",
-                "locarmor_658_locations",
-                "communautelvao_LWTYYUPBDMWM",
-                "6554f1bb-82d2-567f-8453-eec5405e5b5d",  # pragma: allowlist secret
-                "65791ef2-bb37-4569-b011-8cece03dcdcf",  # pragma: allowlist secret
-                "antiquites_du_poulbenn_152575_reparation",
-                "refashion_TLC-REFASHION-PAV-3445001",  # pragma: allowlist secret
-                "communautelvao_VBOFDJDBOCTW",
-                # pragma: allowlist nextline secret
-                "refashion_TLC-REFASHION-REP-603665791852778329",
-                "ocad3e_SGS-02069",
-            ]
-        )
+        pks = [
+            "alrebobinage__altech_distribution_sce_154142_reparation_0626645539",
+            "locarmor_658_locations",
+            "communautelvao_LWTYYUPBDMWM",
+            "6554f1bb-82d2-567f-8453-eec5405e5b5d",  # pragma: allowlist secret
+            "65791ef2-bb37-4569-b011-8cece03dcdcf",  # pragma: allowlist secret
+            "antiquites_du_poulbenn_152575_reparation",
+            "refashion_TLC-REFASHION-PAV-3445001",  # pragma: allowlist secret
+            "communautelvao_VBOFDJDBOCTW",
+            # pragma: allowlist nextline secret
+            "refashion_TLC-REFASHION-REP-603665791852778329",
+            "ocad3e_SGS-02069",
+        ]
+
         call_command(
             "dumpdata",
             "qfdmo.displayedacteur",
             indent=4,
-            pks=pks,
+            pks=",".join(pks),
             natural_foreign=True,
             natural_primary=True,
             output="qfdmo/fixtures/acteurs.json",
         )
-        ps_pks = ",".join(
-            list(
-                DisplayedPropositionService.objects.filter(
-                    acteur__pk__in=pks
-                ).values_list("pk", flat=True)
-            )
-        )
+        ps_pks = DisplayedPropositionService.objects.filter(
+            acteur__pk__in=pks
+        ).values_list("pk", flat=True)
 
         call_command(
             "dumpdata",
@@ -44,6 +39,6 @@ class Command(BaseCommand):
             indent=4,
             natural_foreign=True,
             natural_primary=True,
-            pks=ps_pks,
+            pks=",".join(ps_pks),
             output="qfdmo/fixtures/propositions_services.json",
         )
