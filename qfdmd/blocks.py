a0559de150e21d524503c44432c992c9668df81b
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-07-31 08:57:01 +0200

    Améliorations Wagtail : Bonus, héritage... (#1813)
    * WIP
    
    * Update Poetry deps and fix Wagtail block overrideUpdate Poetry package
    dependencies
    
    * Remove comments from ReusableContent model

--- qfdmd/blocks.py
+++ qfdmd/blocks.py
@@ -1,3 +1,6 @@
+import logging
+
+from django.utils.functional import cached_property
 from django.utils.translation import gettext_lazy as _
 from sites_faciles.content_manager import blocks as sites_faciles_blocks
 from sites_faciles.content_manager.blocks import (
@@ -9,12 +12,19 @@ from sites_faciles.content_manager.blocks import (
 from wagtail import blocks
 from wagtail.snippets.blocks import SnippetChooserBlock
 
+logger = logging.getLogger(__name__)
+
 
 class ProductBlock(blocks.StructBlock):
     champ = blocks.ChoiceBlock(
-        choices=[("Bon état", "bon_etat"), ("Mauvais état", "mauvais_etat")]
+        choices=[("Bon état", "bon_etat"), ("Mauvais état", "mauvais_etat")],
     )
     produit = SnippetChooserBlock("qfdmd.produit", label="produit")
+
+
+def get_choices(*args, **kwargs) -> list:
+    logger.info(f"{args=} {kwargs=}")
+    return [("coucou", "coucou")]
 
 
 class ExtendedCommonStreamBlock(CommonStreamBlock):
@@ -37,8 +47,37 @@ class TabsBlock(sites_faciles_blocks.TabsBlock):
     tabs = TabBlock(label=_("Tab"), min_num=1, max_num=15)
 
 
+class Bonus(blocks.StaticBlock):
+    class Meta:
+        template = "blocks/bonus.html"
+        label = "Bonus réparation"
+
+
+class WagtailBlockChoiceBlock(blocks.ChooserBlock):
+    @cached_property
+    def target_model(self):
+        from qfdmd.views import WagtailBlock
+
+        return WagtailBlock
+
+    @cached_property
+    def widget(self):
+        from qfdmd.wagtail_hooks import WagtailBlockChooserWidget
+
+        return WagtailBlockChooserWidget(
+            linked_fields={"parent_page_id": "#id_parent_page_id"}
+        )
+
+
+class Override(blocks.StructBlock):
+    block = WagtailBlockChoiceBlock()
+    content = ExtendedCommonStreamBlock()
+
+
 STREAMFIELD_COMMON_BLOCKS = [
     *SITES_FACILES_BLOCKS,
+    ("override", Override()),
+    ("bonus", Bonus()),
     (
         "reusable",
         SnippetChooserBlock(
