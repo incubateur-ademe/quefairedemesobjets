d328f2fe2ffe7fc940cfd569c9fd1b625e7c1395
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2025-08-01 15:14:02 +0200

    Purge des logs de DB toutes les heures (#1837)

--- dags/shared/config/schedules.py
+++ dags/shared/config/schedules.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class SCHEDULES:
+    HOURLY: str = "3 * * * *"  # 3 minutes après chaque heure
     DAILY: str = "0 0 * * *"
     DAILY_AT_1AM: str = "0 1 * * *"
     WEEKLY: str = "0 0 * * 1"
