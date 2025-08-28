a14c3daca1c8095c319f66d6997df51ab56ecdc3
Nicolas Oudard <nicolas@oudard.org>
2025-08-28 16:55:41 +0200

    renommer les fonction de manipulation de bbox

--- qfdmo/views/adresses.py
+++ qfdmo/views/adresses.py
@@ -23,7 +23,11 @@ from core.jinja2_handler import distance_to_acteur
 from core.utils import get_direction
 from qfdmo.forms import FormulaireForm
 from qfdmo.geo_api import bbox_from_list_of_geojson, retrieve_epci_geojson
-from qfdmo.map_utils import center_from_map_bbox, compile_map_bbox, sanitize_map_bbox
+from qfdmo.map_utils import (
+    center_from_frontend_bbox,
+    compile_frontend_bbox,
+    sanitize_frontend_bbox,
+)
 from qfdmo.models import (
     Acteur,
     ActeurStatus,
@@ -245,12 +249,12 @@ class SearchActeursView(
         custom_bbox = cast(
             str, self.get_data_from_request_or_bounded_form("bounding_box")
         )
-        center = center_from_map_bbox(custom_bbox)
+        center = center_from_frontend_bbox(custom_bbox)
         latitude = center[1] or self.get_data_from_request_or_bounded_form("latitude")
         longitude = center[0] or self.get_data_from_request_or_bounded_form("longitude")
 
         if custom_bbox:
-            bbox = sanitize_map_bbox(custom_bbox)
+            bbox = sanitize_frontend_bbox(custom_bbox)
             acteurs_in_bbox = acteurs.in_bbox(bbox)
 
             if acteurs_in_bbox.count() > 0:
@@ -274,7 +278,7 @@ class SearchActeursView(
                 acteurs = acteurs.in_geojson(
                     [json.dumps(geojson) for geojson in geojson_list]
                 )
-            return compile_map_bbox(bbox), acteurs
+            return compile_frontend_bbox(bbox), acteurs
 
         return custom_bbox, acteurs.none()
 
