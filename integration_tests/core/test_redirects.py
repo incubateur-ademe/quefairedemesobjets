35f432434ebbf8c182e2786f191481f63c1b2101
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-08-28 16:42:52 +0200

    Remove useless tests

--- integration_tests/core/test_redirects.py
+++ integration_tests/core/test_redirects.py
@@ -43,67 +43,10 @@ def test_forms_redirects(client, test_url):
 @pytest.mark.parametrize(
     "host,url,expected_url,expected_status_code",
     [
-        # Redirects to CMS
-        ("lvao.ademe.fr", "", "https://quefairedemesdechets.ademe.fr/", 302),
-        ("lvao.ademe.fr", "/", "https://quefairedemesdechets.ademe.fr/", 302),
-        # Épargnons, Formulaire
-        (
-            "lvao.ademe.fr",
-            "/formulaire",
-            "https://quefairedemesdechets.ademe.fr/formulaire",
-            302,
-        ),
         # Carte
-        ("lvao.ademe.fr", "/carte", "https://quefairedemesdechets.ademe.fr/carte", 302),
-        # Scripts
-        # (
-        #     "lvao.ademe.fr",
-        #     "/static/iframe.js",
-        #     "https://quefairedemesdechets.ademe.fr/static/iframe.js",
-        #     302,
-        # ),
-        # (
-        #     "lvao.ademe.fr",
-        #     "/static/carte.js",
-        #     "https://quefairedemesdechets.ademe.fr/static/carte.js",
-        #     302,
-        # ),
         # Assistant
         ("quefairedemesdechets.ademe.fr", "/", None, 200),
         ("quefairedemesdechets.ademe.fr", "/carte", None, 200),
-        # (
-        #     "quefairedemesdechets.ademe.fr",
-        #     "/static/iframe.js",
-        #     None,
-        #     200,
-        # ),  # formulaire script
-        # (
-        #     "quefairedemesdechets.ademe.fr",
-        #     "/static/carte.js",
-        #     None,
-        #     200,
-        # ),  # carte script
-        # ("quefairedemesdechets.ademe.fr", "/iframe.js", None, 200),
-        # assistant script
-        ("lvao.ademe.fr", "?carte", "https://quefairedemesdechets.ademe.fr/carte", 302),
-        (
-            "lvao.ademe.fr",
-            "?formulaire",
-            "https://quefairedemesdechets.ademe.fr/formulaire",
-            302,
-        ),
-        (
-            "lvao.ademe.fr",
-            "?iframe&direction=jai",
-            "https://quefairedemesdechets.ademe.fr/?iframe&direction=jai",
-            302,
-        ),
-        (
-            "lvao.ademe.fr",
-            "?carte",
-            "https://quefairedemesdechets.ademe.fr/carte",
-            302,
-        ),
         (
             "quefairedemesdechets.ademe.fr",
             "?formulaire",
@@ -124,22 +67,10 @@ def test_forms_redirects(client, test_url):
         ),
         # Integrations from Notion list
         (
-            "lvao.ademe.fr",
-            "/carte?action_list=reparer%7Cdonner%7Cechanger%7Crapporter%7Cpreter%7Cemprunter%7Clouer%7Cmettreenlocation%7Cacheter%7Crevendre%7Ctrier&epci_codes=243100518&epci_codes=200071314&epci_codes=243100732&epci_codes=243100773&epci_codes=243100633&epci_codes=243100781&epci_codes=200034957&epci_codes=243100815&limit=50",
-            "https://quefairedemesdechets.ademe.fr/carte?action_list=reparer%7Cdonner%7Cechanger%7Crapporter%7Cpreter%7Cemprunter%7Clouer%7Cmettreenlocation%7Cacheter%7Crevendre%7Ctrier&epci_codes=243100518&epci_codes=200071314&epci_codes=243100732&epci_codes=243100773&epci_codes=243100633&epci_codes=243100781&epci_codes=200034957&epci_codes=243100815&limit=50",
-            302,
-        ),
-        (
             "quefairedemesdechets.ademe.fr",
             "/carte?action_list=reparer%7Cdonner%7Cechanger%7Crapporter%7Cpreter%7Cemprunter%7Clouer%7Cmettreenlocation%7Cacheter%7Crevendre%7Ctrier&epci_codes=243100518&epci_codes=200071314&epci_codes=243100732&epci_codes=243100773&epci_codes=243100633&epci_codes=243100781&epci_codes=200034957&epci_codes=243100815&limit=50",
             None,
             200,
-        ),
-        (
-            "lvao.ademe.fr",
-            "/carte?action_list=preter%7Cemprunter%7Clouer%7Cmettreenlocation%7Creparer%7Cdonner%7Cechanger%7Cacheter%7Crevendre%7Crapporter&bounding_box=%7B%22southWest%22%3A%7B%22lat%22%3A48.803292%2C%22lng%22%3A2.41289%7D%2C%22northEast%22%3A%7B%22lat%22%3A48.941676%2C%22lng%22%3A2.598727%7D%7D",
-            "https://quefairedemesdechets.ademe.fr/carte?action_list=preter%7Cemprunter%7Clouer%7Cmettreenlocation%7Creparer%7Cdonner%7Cechanger%7Cacheter%7Crevendre%7Crapporter&bounding_box=%7B%22southWest%22%3A%7B%22lat%22%3A48.803292%2C%22lng%22%3A2.41289%7D%2C%22northEast%22%3A%7B%22lat%22%3A48.941676%2C%22lng%22%3A2.598727%7D%7D",
-            302,
         ),
         (
             "quefairedemesdechets.ademe.fr",
