76533aa72832d3d38d3e95803ee1304f9210c06c
Nicolas Oudard <nicolas@oudard.org>
2025-09-10 00:18:50 +0200

    ajout de champs d'information sur la vue exhaustive des acteurs

--- dbt/models/marts/acteurs/exhaustive/marts_exhaustive_acteur.sql
+++ dbt/models/marts/acteurs/exhaustive/marts_exhaustive_acteur.sql
@@ -1,20 +1,24 @@
-select * from {{ ref('int_acteur') }}
+WITH enfants AS (
+    select
+        distinct(parent_id) as parent_id,
+        jsonb_agg(identifiant_unique) as enfants
+    from {{ ref('base_revisionacteur') }}
+    group by parent_id
+)
 
-/*
-A faire après avoir modifié le process de publication des tables displayed
-A lieu de renommer, on préfèrera faire une copie de carte -> displayed
-
-Ajout des champs calculés:
-
-est_parent                       | boolean                 |           |          |
- est_dans_displayedacteur         | boolean                 |           |          |
- est_dans_revisionacteur          | boolean                 |           |          |
- est_dans_acteur                  | boolean                 |           |          |
- location_lat                     | double precision        |           |          |
- location_long                    | double precision        |           |          |
- enfants_nombre                   | integer                 |           |          |
- enfants_liste                    | character varying[]     |           |          |
- source_code                      | character varying(255)  |           |          |
- acteur_type_code                 | character varying(255)
-
-*/
+select a.*,
+    -- TODO : add lat and long, issue intrpreting double precision by dbt
+    CAST(ST_X(a.location::geometry) AS DOUBLE PRECISION) AS latitude,
+    CAST(ST_Y(a.location::geometry) AS DOUBLE PRECISION) AS longitude,
+    e.enfants IS NOT NULL AS est_parent,
+    e.enfants AS enfants_liste,
+    jsonb_array_length(e.enfants) AS enfants_nombre,
+    ca.identifiant_unique IS NOT NULL AS est_dans_carte,
+    oa.identifiant_unique IS NOT NULL AS est_dans_opendata
+from {{ ref('int_acteur') }} as a
+LEFT JOIN enfants AS e
+  ON a.identifiant_unique = e.parent_id
+LEFT JOIN {{ ref('marts_carte_acteur') }} AS ca
+  ON a.identifiant_unique = ca.identifiant_unique
+LEFT JOIN {{ ref('marts_opendata_acteur') }} AS oa
+  ON a.identifiant_unique = oa.identifiant_unique
