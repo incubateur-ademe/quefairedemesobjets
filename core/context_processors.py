d2ba415be8e3bd391e4da4f368538b81b8f62f88
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-09-12 11:46:44 +0200

    Ajout de la déclaration d'accessibilité à la carte

--- core/context_processors.py
+++ core/context_processors.py
@@ -33,8 +33,11 @@ def global_context(request) -> dict:
             "POSTHOG_KEY": settings.ASSISTANT["POSTHOG_KEY"],
             "MATOMO_ID": settings.ASSISTANT["MATOMO_ID"],
         },
-        "lvao": {
+        "carte": {
             "POSTHOG_KEY": settings.LVAO["POSTHOG_KEY"],
+            "DECLARATION_ACCESSIBILITE_PAGE_ID": settings.LVAO[
+                "DECLARATION_ACCESSIBILITE_PAGE_ID"
+            ],
         },
     }
 
