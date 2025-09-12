f6568ee0edd9a31b805abf94eeb7e6c58f788fac
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-09-12 10:27:07 +0200

    Rename LVAO to Carte

--- core/context_processors.py
+++ core/context_processors.py
@@ -33,9 +33,9 @@ def global_context(request) -> dict:
             "POSTHOG_KEY": settings.ASSISTANT["POSTHOG_KEY"],
             "MATOMO_ID": settings.ASSISTANT["MATOMO_ID"],
         },
-        "lvao": {
-            "POSTHOG_KEY": settings.LVAO["POSTHOG_KEY"],
-            "MATOMO_ID": settings.LVAO["MATOMO_ID"],
+        "carte": {
+            "POSTHOG_KEY": settings.CARTE["POSTHOG_KEY"],
+            "MATOMO_ID": settings.CARTE["MATOMO_ID"],
         },
     }
 
