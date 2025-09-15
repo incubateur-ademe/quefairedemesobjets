f1352c46e3a0b089b32b64b39255c695012ae323
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-09-15 15:05:23 +0200

    Apply suggestion from @fabienheureux

--- static/to_compile/embed/helpers.ts
+++ static/to_compile/embed/helpers.ts
@@ -5,7 +5,6 @@ export async function generateBackLink(iframe: HTMLIFrameElement, key: BacklinkK
     "style",
     "font-size: 0.9rem; text-align: center; padding-top: 0.5rem;",
   )
-  // TODO: get backlink content
   const backlinkContent = await fetch(`${origin}/embed/backlink?key=${key}`)
   backlinkTag.innerHTML = await backlinkContent.text()
   iframe.insertAdjacentElement("afterend", backlinkTag)
