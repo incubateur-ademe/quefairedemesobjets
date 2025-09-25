f33baab71b168365d0d3b0c378a6896c1ebca7f2
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-09-25 14:45:41 +0200

    Remove random settings

--- jest.config.ts
+++ jest.config.ts
@@ -3,16 +3,10 @@ import type { Config } from "jest"
 const config: Config = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
-  testEnvironmentOptions: {
-    html: '<html lang="zh-cmn-Hant"></html>',
-    url: "https://jestjs.io/",
-    userAgent: "Agent/007",
-  },
   verbose: false,
   transform: {
     "^.+\\.ts?$": "ts-jest",
   },
-  transformIgnorePatterns: ["/node_modules/(?!crypto-random-string)(.*)"],
 }
 
 export default config
