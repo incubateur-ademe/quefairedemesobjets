890e1851cfd2286dd0cc861a266b6452ba2fd474
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-09-25 14:50:39 +0200

    Correction du backlink quand l'url est mal définie (#2060)
    * debug backlink
    
    * Add more test cases
    
    * Add tests
    
    * Disable cache control
    
    * Add font
    
    * fix test
    
    * fix test
    
    * Remove random settings

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,12 @@
 import type { Config } from "jest"
 
 const config: Config = {
-    preset: "ts-jest",
-    testEnvironment: "jsdom",
-    testEnvironmentOptions: {
-        html: '<html lang="zh-cmn-Hant"></html>',
-        url: "https://jestjs.io/",
-        userAgent: "Agent/007",
-    },
-    transform: {
-        "^.+\\.ts?$": "ts-jest",
-    },
-    transformIgnorePatterns: ["/node_modules/(?!crypto-random-string)(.*)"],
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  verbose: false,
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+  },
 }
 
 export default config
