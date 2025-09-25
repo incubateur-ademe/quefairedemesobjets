799f644457f9865908492103efb4a0b6c45a7afa
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-09-25 14:42:39 +0200

    fix test

--- static/to_compile/embed/helpers.ts
+++ static/to_compile/embed/helpers.ts
@@ -1,11 +1,18 @@
 export type BacklinkKey = "assistant" | "carte" | "formulaire"
 export async function generateBackLink(iframe: HTMLIFrameElement, key: BacklinkKey) {
+  if (!baseUrl) {
+    console.error("Origin is not defined or is empty")
+    return
+  }
+
   const backlinkTag = document.createElement("div")
   backlinkTag.setAttribute(
     "style",
-    "font-family: system-ui; font-size: 0.9rem; text-align: center; padding-top: 0.5rem;",
+    "font-size: 0.9rem; text-align: center; padding-top: 0.5rem;",
   )
+
   let backlinkContent = ""
+
   try {
     const req = await fetch(`${origin}/embed/backlink?key=${key}`)
     backlinkContent = await req.text()
