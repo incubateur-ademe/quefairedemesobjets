b70a0307527ab1f5587c6883a971d338468ff810
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2025-09-26 17:51:52 +0200

    Remove DB_MIGRATE

--- infrastructure/modules/container/airflow-scheduler.tf
+++ infrastructure/modules/container/airflow-scheduler.tf
@@ -23,7 +23,6 @@ resource "scaleway_container" "airflow_scheduler" {
   }
 
   environment_variables = {
-    _AIRFLOW_DB_MIGRATE                          = "true"
     AIRFLOW__API__AUTH_BACKENDS                  = "airflow.api.auth.backend.basic_auth,airflow.api.auth.backend.session"
     AIRFLOW__CORE__DAGS_ARE_PAUSED_AT_CREATION   = "true"
     AIRFLOW__CORE__DAGS_FOLDER                   = "/opt/airflow/dags"
