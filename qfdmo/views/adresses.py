dd38dbfc3a44affd5405b2f5c05198d8b70ab1d5
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-09-30 11:45:41 +0200

    Move things around

--- qfdmo/views/adresses.py
+++ qfdmo/views/adresses.py
@@ -533,7 +533,7 @@ class FormulaireSearchActeursView(SearchActeursView):
     modifiée."""
 
     is_iframe = True
-    template_name = "qfdmo/formulaire.html"
+    template_name = "pages/formulaire.html"
     form_class = FormulaireForm
 
     def get_context_data(self, **kwargs):
