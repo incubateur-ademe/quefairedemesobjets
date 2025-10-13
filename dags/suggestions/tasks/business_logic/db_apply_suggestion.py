0bcdf14bc4362d372a21ca8d2a203f79925b616c
Nicolas Oudard <nicolas@oudard.org>
2025-10-13 12:52:49 +0200

    Ajout de retry lors de l'apply atomic des suggestions

--- dags/suggestions/tasks/business_logic/db_apply_suggestion.py
+++ dags/suggestions/tasks/business_logic/db_apply_suggestion.py
@@ -1,12 +1,19 @@
 import logging
 
-from django.db import transaction
+from django.db import IntegrityError, transaction
 from sources.config import shared_constants as constants
 from suggestions.tasks.business_logic.db_check_suggestion_to_process import (
     get_suggestions_toprocess,
 )
+from tenacity import retry, retry_if_exception_type, stop_after_attempt
 
 logger = logging.getLogger(__name__)
+
+
+@retry(stop=stop_after_attempt(3), retry=retry_if_exception_type(IntegrityError))
+def suggestion_apply_atomic(suggestion):
+    with transaction.atomic():
+        suggestion.apply()
 
 
 def db_apply_suggestion():
@@ -18,8 +25,7 @@ def db_apply_suggestion():
             suggestion.save()
 
             # Apply suggestion here
-            with transaction.atomic():
-                suggestion.apply()
+            suggestion_apply_atomic(suggestion)
 
             suggestion.statut = constants.SUGGESTION_SUCCES
             suggestion.save()
