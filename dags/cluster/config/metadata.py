a4d33885a28102ec284d04477b9fd03375f63c2a
Nicolas Oudard <nicolas@oudard.org>
2025-11-10 18:05:13 +0100

    typos

--- dags/cluster/config/metadata.py
+++ dags/cluster/config/metadata.py
@@ -1,9 +1,11 @@
-METADATA_NUMBER_OF_UPDATES_BY_FIELD = "Nombre de mise à jour par champ"
+METADATA_NUMBER_OF_UPDATES_BY_FIELD = "Nombre de mises à jour par champ"
 METADATA_ANONYMIZED_ACTEURS_IGNORED = (
-    "Nombre d'acteur mis à jour ignoré car anonymisés pour RGPD"
+    "Nombre d'acteurs mis à jour ignorés car anonymisés pour RGPD"
 )
 METADATA_SERVICE_DOMICILE_FILTERED = (
-    "nb acteurs filtrés car service à domicile uniquement"
+    "Nombre d'acteurs filtrés car service à domicile uniquement"
 )
-METADATA_NO_SOUS_CATEGORIE_FILTERED = "nb acteurs filtrés car sans sous_categorie"
-METADATA_DUPLICATES_FILTERED = "nb acteurs filtrés car doublons sur identifiant_unique"
+METADATA_NO_SOUS_CATEGORIE_FILTERED = "Nombre d'acteurs filtrés car sans sous_categorie"
+METADATA_DUPLICATES_FILTERED = (
+    "Nombre d'acteurs filtrés car doublons sur identifiant_unique"
+)
