2d5c4b39c64518605b52cc3e8b567d7f206260ac
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2025-11-10 19:47:47 +0100

    Le DAG de correction des codes postaux ne fonctionne plus (#2186)
    Le DAG de correction des code postaux ne fonctionne plus

--- dags/enrich/tasks/business_logic/normalize_acteur_cp.py
+++ dags/enrich/tasks/business_logic/normalize_acteur_cp.py
@@ -4,6 +4,7 @@ import logging
 
 import pandas as pd
 from enrich.config.columns import COLS
+from sources.tasks.transform.exceptions import CodePostalWarning
 from sources.tasks.transform.transform_column import clean_code_postal
 
 logger = logging.getLogger(__name__)
@@ -11,7 +12,12 @@ logger = logging.getLogger(__name__)
 
 def normalize_acteur_cp(df: pd.DataFrame) -> pd.DataFrame:
     def get_clean_code_postal(row):
-        return clean_code_postal(row, None)
+        code_postal = None
+        try:
+            code_postal = clean_code_postal(row, None)
+        except CodePostalWarning as e:
+            logger.warning(e)
+        return code_postal
 
     df[COLS.SUGGEST_CODE_POSTAL] = df["code_postal"].apply(get_clean_code_postal)
     df[COLS.ACTEUR_ID] = df["identifiant_unique"]
