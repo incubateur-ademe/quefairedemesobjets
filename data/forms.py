dce040cd6fa72daf60bb709b4d04e7db38266d51
Nicolas Oudard <nicolas@oudard.org>
2025-11-17 11:14:22 +0100

    gestion des statuts des suggestion_groupe et des liens d'aide

--- data/forms.py
+++ data/forms.py
@@ -5,3 +5,9 @@ class SuggestionGroupeForm(forms.Form):
     suggestion_modele = forms.CharField(widget=forms.HiddenInput())
     champs = forms.CharField(widget=forms.HiddenInput())
     valeurs = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+
+class SuggestionGroupeStatusForm(forms.Form):
+    action = forms.ChoiceField(
+        choices=[("validate", "À Traiter"), ("reject", "Rejeter")]
+    )
--- data/urls.py
+++ data/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
-from data.views import SuggestionGroupeView
+from data.views import (
+    SuggestionGroupeStatusView,
+    SuggestionGroupeView,
+    get_suggestion_groupe_usefull_links,
+)
 
 app_name = "data"
 
@@ -9,5 +13,15 @@ urlpatterns = [
         "suggestion-groupe/<int:suggestion_groupe_id>/",
         SuggestionGroupeView.as_view(),
         name="suggestion_groupe",
+    ),
+    path(
+        "suggestion-groupe/<int:suggestion_groupe_id>/status/",
+        SuggestionGroupeStatusView.as_view(),
+        name="suggestion_groupe_status",
+    ),
+    path(
+        "suggestion-groupe/<int:suggestion_groupe_id>/<str:usefull_link>/",
+        get_suggestion_groupe_usefull_links,
+        name="suggestion_groupe_usefull_links",
     ),
 ]
