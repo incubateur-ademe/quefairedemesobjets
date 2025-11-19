30c0faa09631266f60de3b15a430a5e29f0669de
Nicolas Oudard <nicolas@oudard.org>
2025-11-19 17:28:04 +0100

    Utilisation de la lib requests pour plus de clareté

--- scripts/infrastructure/delete_old_images.py
+++ scripts/infrastructure/delete_old_images.py
@@ -3,10 +3,10 @@
 #
 # Usage:
 # TOKEN="XXXXX" TAGS_TO_KEEP=20 python delete_old_images.py
-import urllib.request
-import os
-import json
 import logging
+import os
+
+import requests
 
 REGION = "fr-par"
 TOKEN = os.environ["TOKEN"]
@@ -23,20 +23,20 @@ logger = logging.getLogger("delete_old_images")
 def main():
     # Fetch images
     url = f"https://api.scaleway.com/registry/v1/regions/{REGION}/images?page_size=100&order_by=created_at_asc&namespace_id={NAMESPACE_ID}"
-    req = urllib.request.Request(url, headers=headers)
-    with urllib.request.urlopen(req) as response:
-        res = response.read()
-        images = json.loads(res)["images"]
+    response = requests.get(url, headers=headers)
+    if response.status_code >= 400:
+        raise Exception(
+            f"Failed to fetch images: {response.status_code} - {response.text}"
+        )
+    images = response.json()["images"]
 
     # Get all tags by image and keep up to `TAGS_TO_KEEP` tags
     tags_to_delete = []
     for image in images:
         logger.info(f"Fetching tags for {image['name']}…")
         url = f"https://api.scaleway.com/registry/v1/regions/{REGION}/images/{image['id']}/tags?page_size=100&order_by=created_at_desc"
-        req = urllib.request.Request(url, headers=headers)
-        with urllib.request.urlopen(req) as response:
-            res = response.read()
-            tags = json.loads(res)["tags"]
+        response = requests.get(url, headers=headers)
+        tags = response.json()["tags"]
         i = 0
         for tag in tags:
             if i < TAGS_TO_KEEP:
@@ -48,9 +48,7 @@ def main():
     # Delete tags
     for tag in tags_to_delete:
         url = f"https://api.scaleway.com/registry/v1/regions/{REGION}/tags/{tag['id']}"
-        req = urllib.request.Request(url, headers=headers, method="DELETE")
-        with urllib.request.urlopen(req) as response:
-            res = response.read()
+        response = requests.delete(url, headers=headers)
 
 
 if __name__ == "__main__":
