02cb13c31a5fd9363c37242bd66043d2bf89a060
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-11-20 11:37:49 +0100

    Factorise turbo tagS

--- core/templatetags/turbo_tags.py
+++ core/templatetags/turbo_tags.py
@@ -4,11 +4,6 @@ from core.constants import MAP_CONTAINER_ID
 
 
 @register.simple_tag(takes_context=True)
-def acteur_frame_id(context):
-    return f"{context.get(MAP_CONTAINER_ID)}:acteur-detail"
-
-
-@register.simple_tag(takes_context=True)
 def main_frame_id(context):
     return context.get(MAP_CONTAINER_ID, "carte")
 
@@ -16,3 +11,8 @@ def main_frame_id(context):
 @register.simple_tag(takes_context=True)
 def namespaced(context, id):
     return f"{context.get(MAP_CONTAINER_ID)}:{id}"
+
+
+@register.simple_tag(takes_context=True)
+def acteur_frame_id(context):
+    return namespaced(context, "acteur-detail")
