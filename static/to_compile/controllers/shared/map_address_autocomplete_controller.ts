e3e806abd90b856215f8564c2b239d8ed0101901
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-11-29 20:45:46 +0100

    WIP implement address

---
+++ static/to_compile/controllers/shared/map_address_autocomplete_controller.ts
@@ -0,0 +1,52 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class MapAddressAutocompleteController extends Controller {
+  static targets = ["latitudeInput", "longitudeInput", "input"]
+
+  declare readonly latitudeInputTarget: HTMLInputElement
+  declare readonly longitudeInputTarget: HTMLInputElement
+  declare readonly inputTarget: HTMLInputElement
+
+  connect() {
+    console.log("MapAddressAutocompleteController connected", this.element)
+    // Listen for the custom event from next-autocomplete controller
+    this.element.addEventListener(
+      "next-autocomplete:commit",
+      this.handleSelection.bind(this),
+    )
+  }
+
+  disconnect() {
+    this.element.removeEventListener(
+      "next-autocomplete:commit",
+      this.handleSelection.bind(this),
+    )
+  }
+
+  handleSelection(event: CustomEvent) {
+    const { option, value } = event.detail
+
+    console.log("COUCOU", { option, value })
+    if (!option) return
+
+    const latitude = option.dataset.latitude
+    const longitude = option.dataset.longitude
+
+    if (latitude && longitude) {
+      this.latitudeInputTarget.value = latitude
+      this.longitudeInputTarget.value = longitude
+
+      // Set the input value to the selected address
+      if (this.hasInputTarget && value) {
+        this.inputTarget.value = value
+      }
+
+      // Dispatch to global state (matches address_autocomplete_controller behavior)
+      this.dispatchLocationToGlobalState(value, latitude, longitude)
+    }
+  }
+
+  dispatchLocationToGlobalState(adresse: string, latitude: string, longitude: string) {
+    this.dispatch("change", { detail: { adresse, latitude, longitude } })
+  }
+}
