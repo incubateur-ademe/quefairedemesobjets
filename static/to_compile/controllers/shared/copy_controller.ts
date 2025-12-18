4c28f5a9957d14d739003ca7e93ec42e3c991b07
Fabien Le Frapper <contact@fabienlefrapper.me>
2025-12-18 15:16:49 +0100

    woopsy

--- static/to_compile/controllers/shared/copy_controller.ts
+++ static/to_compile/controllers/shared/copy_controller.ts
@@ -13,11 +13,6 @@ export default class extends Controller<HTMLElement> {
 
   async toClipboard() {
     await navigator.clipboard.writeText(this.toCopyTarget.textContent!)
-    console.log("copy", { t: this }, this.toCopyTarget.textContent, {
-      hasButtonTarget: this.hasButtonTarget,
-      hasCopiedValue: this.hasCopiedValue,
-      copiedValue: this.copiedValue,
-    })
     if (this.hasButtonTarget && this.hasCopiedValue) {
       this.buttonTarget.textContent = this.copiedValue
     }
