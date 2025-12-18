05c29131d9487af247972cb360190f73c274ae7a
Nicolas Oudard <nicolas@oudard.org>
2025-12-18 23:36:32 +0100

    creation d'extension

--- infrastructure/modules/database_sample/main.tf
+++ infrastructure/modules/database_sample/main.tf
@@ -1,3 +1,7 @@
+data "scaleway_rdb_instance" "webapp" {
+  instance_id = var.webapp_instance_id
+}
+
 resource "scaleway_rdb_database" "webapp_sample" {
   instance_id = var.webapp_instance_id
   name        = var.webapp_db_sample_name
@@ -14,4 +18,24 @@ resource "scaleway_rdb_privilege" "webapp_sample_privilege" {
   user_name     = var.webapp_db_sample_username
   database_name = scaleway_rdb_database.webapp_sample.name
   permission    = "all"
+}
+
+resource "null_resource" "create_extensions" {
+  depends_on = [
+    scaleway_rdb_database.webapp_sample,
+    scaleway_rdb_user.webapp_sample,
+    scaleway_rdb_privilege.webapp_sample_privilege
+  ]
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      psql "postgresql://${var.webapp_db_sample_username}:${var.webapp_db_sample_password}@${data.scaleway_rdb_instance.webapp.load_balancer.0.ip}:${data.scaleway_rdb_instance.webapp.load_balancer.0.port}/${scaleway_rdb_database.webapp_sample.name}?sslmode=require" -f ${var.create_extensions_script_path}
+    EOT
+  }
+
+  triggers = {
+    database_id = scaleway_rdb_database.webapp_sample.id
+    user_id     = scaleway_rdb_user.webapp_sample.id
+    privilege_id = scaleway_rdb_privilege.webapp_sample_privilege.id
+  }
 }
