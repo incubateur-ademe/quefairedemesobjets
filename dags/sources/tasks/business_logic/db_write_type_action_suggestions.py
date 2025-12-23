03a71a647c59fd25ed3330f8efa7f03df9f13ffb
Nicolas Oudard <nicolas@oudard.org>
2025-12-24 00:25:00 +0100

    Gestion d'un switch entre Suggestion et SuggestionGroupe pour les DAG source

--- dags/sources/tasks/business_logic/db_write_type_action_suggestions.py
+++ dags/sources/tasks/business_logic/db_write_type_action_suggestions.py
@@ -1,7 +1,9 @@
 import json
 import logging
+from datetime import datetime
 
 import pandas as pd
+from shared.tasks.database_logic.db_manager import PostgresConnectionManager
 from sources.config import shared_constants as constants
 from utils.django import django_setup_full
 
@@ -121,6 +123,7 @@ def db_write_type_action_suggestions(
     metadata_to_delete: dict,
     df_log_error: pd.DataFrame,
     df_log_warning: pd.DataFrame,
+    use_legacy_suggestions: bool,
 ):
 
     (
@@ -144,7 +147,12 @@ def db_write_type_action_suggestions(
 
     run_name = run_id.replace("__", " - ")
 
-    insert_suggestion(
+    if use_legacy_suggestions:
+        function_to_use = insert_suggestion_legacy
+    else:
+        function_to_use = insert_suggestion
+
+    function_to_use(
         df=df_acteur_to_create,
         metadata=metadata_to_create,
         dag_name=f"{dag_name} - AJOUT",
@@ -153,14 +161,14 @@ def db_write_type_action_suggestions(
         df_log_error=df_log_error_to_create,
         df_log_warning=df_log_warning_to_create,
     )
-    insert_suggestion(
+    function_to_use(
         df=df_acteur_to_delete,
         metadata=metadata_to_delete,
         dag_name=f"{dag_name} - SUP",
         run_name=run_name,
         type_action=constants.SUGGESTION_SOURCE_SUPRESSION,
     )
-    insert_suggestion(
+    function_to_use(
         df=df_acteur_to_update,
         metadata=metadata_to_update,
         dag_name=f"{dag_name} - MODIF",
@@ -304,3 +312,95 @@ def insert_suggestion(
                     champs=keys,
                     valeurs=values,
                 ).save()
+
+
+def insert_suggestion_legacy(
+    df: pd.DataFrame,
+    metadata: dict,
+    dag_name: str,
+    run_name: str,
+    type_action: str,
+    df_log_error: pd.DataFrame = pd.DataFrame(),
+    df_log_warning: pd.DataFrame = pd.DataFrame(),
+):
+    from data.models.suggestion import SuggestionLog
+
+    if df.empty:
+        return
+    engine = PostgresConnectionManager().engine
+    current_date = datetime.now()
+
+    with engine.connect() as conn:
+        # Insert a new suggestion
+        result = conn.execute(
+            """
+            INSERT INTO data_suggestioncohorte
+            (
+                identifiant_action,
+                identifiant_execution,
+                type_action,
+                statut,
+                metadata,
+                cree_le,
+                modifie_le
+            )
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+            RETURNING ID;
+        """,
+            (
+                dag_name,
+                run_name,
+                type_action,
+                constants.SUGGESTION_AVALIDER,
+                json.dumps(metadata),
+                current_date,
+                current_date,
+            ),
+        )
+        suggestion_cohorte_id = result.fetchone()[0]
+
+    # Insert suggestion_logs
+    if not df_log_error.empty:
+        df_log_error["suggestion_cohorte_id"] = suggestion_cohorte_id
+        df_log_error["niveau_de_log"] = SuggestionLog.SuggestionLogLevel.ERROR
+        df_log_error.to_sql(
+            "data_suggestionlog",
+            engine,
+            if_exists="append",
+            index=False,
+            method="multi",
+            chunksize=1000,
+        )
+    if not df_log_warning.empty:
+        df_log_warning["suggestion_cohorte_id"] = suggestion_cohorte_id
+        df_log_warning["niveau_de_log"] = SuggestionLog.SuggestionLogLevel.WARNING
+        df_log_warning.to_sql(
+            "data_suggestionlog",
+            engine,
+            if_exists="append",
+            index=False,
+            method="multi",
+            chunksize=1000,
+        )
+    df_log_warning.to_sql(
+        "data_suggestionlog",
+        engine,
+        if_exists="append",
+        index=False,
+        method="multi",
+        chunksize=1000,
+    )
+
+    # Insert dag_run_change
+    df["suggestion_cohorte_id"] = suggestion_cohorte_id
+    df["statut"] = constants.SUGGESTION_AVALIDER
+    # TODO: here the Suggestion model could be used instead of using pandas to insert
+    # the data into the database
+    df[["contexte", "suggestion", "suggestion_cohorte_id", "statut"]].to_sql(
+        "data_suggestion",
+        engine,
+        if_exists="append",
+        index=False,
+        method="multi",
+        chunksize=1000,
+    )
