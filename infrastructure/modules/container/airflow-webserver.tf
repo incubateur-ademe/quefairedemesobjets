e0a3c99d568af4d355b58417c4c96838034e119b
Nicolas Oudard <nicolas@oudard.org>
2026-01-06 12:24:04 +0100

    Add variable to not display thewarning about internet exposure

--- infrastructure/modules/container/airflow-webserver.tf
+++ infrastructure/modules/container/airflow-webserver.tf
@@ -22,26 +22,27 @@ resource "scaleway_container" "airflow_webserver" {
   }
 
   environment_variables = {
-    _AIRFLOW_DB_MIGRATE                        = "true"
-    _AIRFLOW_WWW_USER_CREATE                   = "true"
-    _PIP_ADDITIONAL_REQUIREMENTS               = ""
-    AIRFLOW__API__AUTH_BACKENDS                = "airflow.api.auth.backend.basic_auth,airflow.api.auth.backend.session"
-    AIRFLOW__CORE__DAGS_ARE_PAUSED_AT_CREATION = "true"
-    AIRFLOW__CORE__DAGS_FOLDER                 = "/opt/airflow/dags"
-    AIRFLOW__CORE__ENABLE_XCOM_PICKLING        = "true"
-    AIRFLOW__CORE__EXECUTOR                    = "LocalExecutor"
-    AIRFLOW__CORE__FERNET_KEY                  = ""
-    AIRFLOW__CORE__LOAD_EXAMPLES               = "false"
-    AIRFLOW__LOGGING__ENCRYPT_S3_LOGS          = "false"
-    AIRFLOW__LOGGING__REMOTE_BASE_LOG_FOLDER   = "s3://${var.prefix}-${var.environment}-airflow"
-    AIRFLOW__LOGGING__REMOTE_LOG_CONN_ID       = "scalewaylogs"
-    AIRFLOW__LOGGING__REMOTE_LOGGING           = "true"
-    AIRFLOW__SCHEDULER__ENABLE_HEALTH_CHECK    = "true"
-    AIRFLOW__WEBSERVER__EXPOSE_CONFIG          = "true"
-    AIRFLOW__WEBSERVER__INSTANCE_NAME          = var.AIRFLOW__WEBSERVER__INSTANCE_NAME
-    AIRFLOW__WEBSERVER__WORKERS                = "1"
-    AIRFLOW_WEBSERVER_WARN_DEPLOYMENT_EXPOSURE = "false"
-    ENVIRONMENT                                = var.environment
+    _AIRFLOW_DB_MIGRATE                          = "true"
+    _AIRFLOW_WWW_USER_CREATE                     = "true"
+    _PIP_ADDITIONAL_REQUIREMENTS                 = ""
+    AIRFLOW__API__AUTH_BACKENDS                  = "airflow.api.auth.backend.basic_auth,airflow.api.auth.backend.session"
+    AIRFLOW__CORE__DAGS_ARE_PAUSED_AT_CREATION   = "true"
+    AIRFLOW__CORE__DAGS_FOLDER                   = "/opt/airflow/dags"
+    AIRFLOW__CORE__ENABLE_XCOM_PICKLING          = "true"
+    AIRFLOW__CORE__EXECUTOR                      = "LocalExecutor"
+    AIRFLOW__CORE__FERNET_KEY                    = ""
+    AIRFLOW__CORE__LOAD_EXAMPLES                 = "false"
+    AIRFLOW__LOGGING__ENCRYPT_S3_LOGS            = "false"
+    AIRFLOW__LOGGING__REMOTE_BASE_LOG_FOLDER     = "s3://${var.prefix}-${var.environment}-airflow"
+    AIRFLOW__LOGGING__REMOTE_LOG_CONN_ID         = "scalewaylogs"
+    AIRFLOW__LOGGING__REMOTE_LOGGING             = "true"
+    AIRFLOW__SCHEDULER__ENABLE_HEALTH_CHECK      = "true"
+    AIRFLOW__WEBSERVER__EXPOSE_CONFIG            = "true"
+    AIRFLOW__WEBSERVER__INSTANCE_NAME            = var.AIRFLOW__WEBSERVER__INSTANCE_NAME
+    AIRFLOW__WEBSERVER__WORKERS                  = "1"
+    AIRFLOW__WEBSERVER__WARN_DEPLOYMENT_EXPOSURE = "false"
+    AIRFLOW__WEBSERVER__X_FRAME_ENABLED          = "false"
+    ENVIRONMENT                                  = var.environment
   }
   secret_environment_variables = {
     _AIRFLOW_WWW_USER_USERNAME          = var._AIRFLOW_WWW_USER_USERNAME
