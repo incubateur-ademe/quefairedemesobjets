1c33f707f4b52e6c31644e1c5fd35cc48ef9c70d
Fabien Le Frapper <contact@fabienlefrapper.me>
2026-01-21 14:01:40 +0100

    Corrige un problème d'alignement de l'infotri (#2411)
    * Use same classname wrapper for the infotri
    
    * Apply last class
    
    * Remove leftover

--- previews/template_preview.py
+++ previews/template_preview.py
@@ -320,11 +320,6 @@ class ComponentsPreview(LookbookPreview):
     def logo_homepage(self, **kwargs):
         return render_to_string("ui/components/logo/homepage.html")
 
-    @component_docs("ui/components/produit/legacy_heading.md")
-    def produit_legacy_heading(self, **kwargs):
-        context = {"title": "Coucou !"}
-        return render_to_string("ui/components/produit/legacy_heading.html", context)
-
     @register_form_class(ProduitHeadingForm)
     @component_docs("ui/components/produit/heading.md")
     def produit_heading(self, synonyme=None, pronom="mon", **kwargs):
