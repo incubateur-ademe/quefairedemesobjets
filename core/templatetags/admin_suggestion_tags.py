eae0684dc81d7316d7d39c071bec49d339766b79
Nicolas Oudard <nicolas@oudard.org>
2026-01-28 16:39:44 +0100

    Affichage des valeurs même quand elles ne sont pas éditables

--- core/templatetags/admin_suggestion_tags.py
+++ core/templatetags/admin_suggestion_tags.py
@@ -5,4 +5,8 @@ from data.models.suggestions.source import SuggestionSourceModel
 
 @register.filter
 def is_not_editable(key):
+    if isinstance(key, list) or isinstance(key, tuple):
+        return any(
+            field in SuggestionSourceModel.get_not_editable_fields() for field in key
+        )
     return key in SuggestionSourceModel.get_not_editable_fields()
