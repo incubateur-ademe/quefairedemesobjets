6f96d6a6f3f00aafd719f9b2930886c279927516
Fabien Le Frapper <contact@fabienlefrapper.me>
2026-02-02 20:08:01 +0100

    Fix sample webapp script

--- dbt/models/marts/acteurs/sample/marts_sample_displayedacteur.sql
+++ dbt/models/marts/acteurs/sample/marts_sample_displayedacteur.sql
@@ -1,6 +1,9 @@
 SELECT displayedacteur.* FROM {{ source('qfdmo', 'qfdmo_displayedacteur') }} AS displayedacteur
-INNER JOIN {{ source('qfdmo','qfdmo_epci') }} AS epci ON displayedacteur.epci_id = epci.id
-WHERE epci.code IN (
-    '200043123', /* CC Auray Quiberon Terre Atlantique */
-    '200065647' /* CA Pays de Montbéliard Agglomération */
+LEFT JOIN {{ source('qfdmo','qfdmo_epci') }} AS epci ON displayedacteur.epci_id = epci.id
+LEFT JOIN {{ source('qfdmo','qfdmo_acteurtype') }} AS acteur_type ON displayedacteur.acteur_type_id = acteur_type.id
+WHERE
+    epci.code IN (
+        '200043123', /* CC Auray Quiberon Terre Atlantique */
+        '200065647' /* CA Pays de Montbéliard Agglomération */
     )
+    OR acteur_type.code = 'acteur_digital' /* Include all digital acteurs */
