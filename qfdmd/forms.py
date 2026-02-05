cd3352277dfd2121cf7e56e706e66589cd4b50fd
Fabien Le Frapper <contact@fabienlefrapper.me>
2026-02-05 21:11:19 +0100

    add back templates

--- qfdmd/forms.py
+++ qfdmd/forms.py
@@ -1,18 +1,10 @@
 import logging
 
 from django import forms
-from django.contrib.postgres.lookups import Unaccent
-from django.contrib.postgres.search import (
-    TrigramSimilarity,
-    TrigramStrictWordSimilarity,
-)
-from django.db.models import Case, F, Value, When
 from dsfr.forms import DsfrBaseForm
-from modelsearch.query import Fuzzy
 
 from search.models import SearchTerm
 
-from .models import ProduitPage, Synonyme
 
 logger = logging.getLogger(__name__)
 
@@ -35,42 +27,8 @@ class SearchForm(DsfrBaseForm):
             self.results = []
             return self.results
 
-        self.results = list(SearchTerm.objects.autocomplete(Fuzzy(search_query))[:10])
+        self.results = SearchTerm.objects.search(search_query)[:10]
         return self.results
-
-    def _search_pages(self, search_query: str):
-        return list(
-            ProduitPage.objects.live().autocomplete(search_query)
-        ) + self._search_legacy_synonymes(search_query)
-
-    def _search_legacy_synonymes(self, search_query: str):
-        return list(
-            Synonyme.objects.annotate(
-                word_similarity=TrigramStrictWordSimilarity(search_query, "nom"),
-                similarity=TrigramSimilarity("nom", search_query),
-                unaccented_nom=Unaccent("nom"),
-            )
-            .annotate(boosted_score=self._get_boosted_score_case(search_query))
-            .filter(word_similarity__gte=0.1)
-            .order_by("-word_similarity", "-boosted_score", "-similarity")
-            .values("nom", "boosted_score", "word_similarity", "similarity", "slug")[
-                :10
-            ]
-        )
-
-    def _get_boosted_score_case(self, search_query: str) -> Case:
-        """Create a Case expression for boosting scores based on position matches."""
-        return Case(
-            When(
-                unaccented_nom__istartswith=search_query,
-                then=F("similarity") + Value(0.2),
-            ),
-            When(
-                unaccented_nom__iendswith=search_query,
-                then=F("similarity") + Value(0.05),
-            ),
-            default=F("similarity"),
-        )
 
 
 class ContactForm(DsfrBaseForm):
