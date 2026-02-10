3301996fc7b53d81ea1bf203486f1a16fca306e8
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2026-02-10 18:55:09 +0100

    Calcul du taux d'acteur empilés vs la date (#2500)

---
+++ dbt/models/exposure/stats/exposure_stats_rate_acteur_stacked_rounded_history.sql
@@ -0,0 +1,18 @@
+WITH stacked AS (
+    SELECT COUNT(*) AS nb_acteurs_stacked
+    FROM {{ ref('marts_acteur_stacked_rounded') }}
+),
+visible AS (
+    SELECT COUNT(*) AS nb_acteurs_visible_with_location
+    FROM {{ ref('int_acteur_visible_location') }}
+)
+SELECT
+    CURRENT_DATE AS date_snapshot,
+    s.nb_acteurs_stacked,
+    v.nb_acteurs_visible_with_location,
+    CASE
+        WHEN v.nb_acteurs_visible_with_location = 0 THEN 0
+        ELSE ROUND((s.nb_acteurs_stacked::NUMERIC / v.nb_acteurs_visible_with_location) * 100, 2)
+    END AS rate_acteur_stacked_rounded
+FROM stacked s
+CROSS JOIN visible v
