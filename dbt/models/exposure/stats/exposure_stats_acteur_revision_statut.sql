778958e8c95bc2ddbd0d41f8a089fdd6066b0b1f
Nicolas Oudard <nicolas@oudard.org>
2026-02-12 15:26:04 +0100

    Comparaison SIREN et SIRET de Acteur et révision

--- dbt/models/exposure/stats/exposure_stats_acteur_revision_statut.sql
+++ dbt/models/exposure/stats/exposure_stats_acteur_revision_statut.sql
@@ -1,4 +1,9 @@
+
 SELECT
-  *
-FROM {{ ref('marts_acteur_revision_statut') }}
+  identifiant_unique,
+  nom,
+  source_code,
+  acteur_statut,
+  revision_statut
+FROM {{ ref('marts_acteur_revision') }}
 WHERE revision_statut = 'ACTIF' AND acteur_statut != 'ACTIF'
