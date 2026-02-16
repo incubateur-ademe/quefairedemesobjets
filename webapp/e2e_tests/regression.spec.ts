8449fd8b30200e97cbbdf1f2353fbc7d73035107
Fabien Le Frapper <contact@fabienlefrapper.me>
2026-02-16 21:28:46 +0100

    Collect

--- webapp/e2e_tests/regression.spec.ts
+++ webapp/e2e_tests/regression.spec.ts
@@ -5,15 +5,15 @@ import crypto from "node:crypto"
 const urlsToTest = ["https://google.fr"]
 
 test("Les pages et composants n'ont pas changé", async ({ page }) => {
-  // await navigateTo(page, `/lookbook`)
-  // const links = await page.$$("a[data-controller=lookbook-sidebar-link]")
-  // for (const link of links) {
-  //   const href = await link.getAttribute("href")
+  await navigateTo(page, `/lookbook`)
+  const links = await page.$$("a[data-controller=lookbook-sidebar-link]")
+  for (const link of links) {
+    const href = await link.getAttribute("href")
 
-  //   // if (href) {
-  //   //   urlsToTest.push(href)
-  //   // }
-  // }
+    if (href) {
+      urlsToTest.push(href.replace("inspect", "preview"))
+    }
+  }
 
   for (const pageToTest of urlsToTest) {
     await navigateTo(page, pageToTest)
