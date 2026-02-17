bbe65bdc7076b253241b77297834ceba96d6a83d
Fabien Le Frapper <contact@fabienlefrapper.me>
2026-02-17 12:27:29 +0100

    update timeout

--- webapp/playwright.regression.ts
+++ webapp/playwright.regression.ts
@@ -8,10 +8,12 @@ dotenv.config({ path: path.resolve(__dirname, ".env") })
 const config: PlaywrightTestConfig = {
   ...baseConfig,
   testIgnore: "",
+  timeout: 120000_000,
   testMatch: "*regression*",
   expect: {
     toHaveScreenshot: {
       pathTemplate: `./${process.env.SCREENSHOTS_BASE_PATH || "__screenshots__"}/{testFilePath}/{arg}{ext}`,
+      maxDiffPixels: 100,
     },
   },
 }
