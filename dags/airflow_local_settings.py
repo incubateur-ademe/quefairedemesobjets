3f526f2c15320f66c231bfed2d9ac50f7c8509be
Nicolas Oudard <nicolas@oudard.org>
2026-02-18 11:59:54 +0100

    Pas besoin de modifier le sys.path

--- dags/airflow_local_settings.py
+++ dags/airflow_local_settings.py
@@ -1,11 +1,3 @@
-import sys
-from pathlib import Path
-
-# Add Django path to sys.path
-_django_root = Path(__file__).resolve().parent.parent
-if _django_root not in sys.path:
-    sys.path.insert(0, str(_django_root))
-
 keepalive_kwargs = {
     "keepalives": 1,  # enable keepalive
     "keepalives_idle": 60,  # idle time before sending keepalive
