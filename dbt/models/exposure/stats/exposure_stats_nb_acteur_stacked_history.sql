1d17a8e3bd3fa9228592c06f99debf3594556653
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2026-02-19 09:07:08 +0100

    Refactorisation des tables exposure_stats : facilite la maintenance (#2523)
    * refactorisation des tables exposure_stats pour réduire le nombre de table à maintenir
    
    * les taux de siret et siren actif sont par rapport aux acteurs avec siren ou siret

--- dbt/models/exposure/stats/exposure_stats_nb_acteur_stacked_history.sql
+++ dbt/models/exposure/stats/exposure_stats_nb_acteur_stacked_history.sql
@@ -1,3 +1,6 @@
+-- DEPRECATED: will use exposure_stats_acteur_stacked_history instead of
+-- exposure_stats_rate_acteur_stacked_history and exposure_stats_nb_acteur_stacked_history
+
 
 SELECT
     CURRENT_DATE AS date_snapshot,
