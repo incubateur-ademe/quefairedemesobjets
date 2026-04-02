97882b66eec755e01b2adb729a96447101aa52f2
Fabien Le Frapper <contact@fabienlefrapper.me>
2026-04-02 13:20:32 +0200

    Handle accents in fuzzy search

--- webapp/unit_tests/conftest.py
+++ webapp/unit_tests/conftest.py
@@ -5,6 +5,8 @@ from django.core.management import call_command
 @pytest.fixture(scope="session")
 def django_db_setup(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
+        call_command("enable_unaccent")
+        call_command("enable_trigram")
         call_command(
             "loaddata",
             "acteur_types",
