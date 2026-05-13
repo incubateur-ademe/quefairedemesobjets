81b9685f66408826cc27415f5d8ff9cbe6477d65
Nicolas Oudard <nicolas@oudard.org>
2026-05-13 16:44:28 +0200

    Airflow Source : default False pour les booleens de la classe Acteur

--- data-platform/dags/sources/tasks/business_logic/keep_acteur_changed.py
+++ data-platform/dags/sources/tasks/business_logic/keep_acteur_changed.py
@@ -7,6 +7,7 @@ from cluster.config.metadata import (
     METADATA_ANONYMIZED_ACTEURS_IGNORED,
     METADATA_NUMBER_OF_UPDATES_BY_FIELD,
 )
+from data.models.changes.acteur_rgpd_anonymize import VALUE_ANONYMIZED
 from sources.config.models import SourceConfig
 from sources.tasks.transform.transform_df import compute_identifiant_unique
 from utils.django import django_setup_full, get_model_fields
