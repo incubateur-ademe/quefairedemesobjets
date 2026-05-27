cd330de55e7b1aea0b3da98b84d48ff086376208
Fabien Le Frapper <contact@fabienlefrapper.me>
2026-05-27 12:05:19 +0200

    Missing merge migration

---
+++ webapp/qfdmd/migrations/0084_merge_20260527_0000.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("qfdmd", "0083_alter_produitindexpage_body_alter_produitpage_body"),
+        ("qfdmd", "0083_produitpage_ab_test_carte_default_view"),
+    ]
+
+    operations = []
