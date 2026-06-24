c8cdf4ca31bb951d57f80fa5d36123c3edffd7b0
Fabien Le Frapper <contact@fabienlefrapper.me>
2026-06-24 14:55:36 +0200

    Fix unit tests

--- webapp/unit_tests/core/test_healthz.py
+++ webapp/unit_tests/core/test_healthz.py
@@ -4,6 +4,6 @@ import pytest
 @pytest.mark.django_db
 class TestHealthz:
     def test_healthz_returns_ok(self, client):
-        response = client.get("/healthz")
+        response = client.get("/healthz/")
         assert response.status_code == 200
         assert response.text == "ok"
