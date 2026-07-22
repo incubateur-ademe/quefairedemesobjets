826ac189b91752d96f6d77db3e0f335358dc7a4b
Fabien Le Frapper <contact@fabienlefrapper.me>
2026-07-22 21:56:02 +0200

    a11y(carte): corrige la cohérence des liens (aria-label et title)
    - Les boutons « Ajouter un lieu » (mode carte et mode liste) avaient un
      aria-label codé en dur (« Proposer une adresse ») différent du texte
      visible (CARTE.ajouter_un_lieu / ajouter_un_lieu_mode_liste).
      L'aria-label reprend désormais le texte visible.
    - Les title des liens de partage (Facebook, X, LinkedIn, email)
      suivaient l'ordre « partager {nom} sur {plateforme} » au lieu de
      « Partager sur {plateforme} {nom} ».
    
    RGAA 6.1.

--- webapp/integration_tests/core/test_sharer.py
+++ webapp/integration_tests/core/test_sharer.py
@@ -23,19 +23,19 @@ class TestSharer:
         assert sharer == {
             "url": "http://testserver/dechet/nom-du-synonyme/",
             "facebook": {
-                "title": "partager Nom du synonyme sur Facebook - nouvelle fenêtre",
+                "title": "Partager sur Facebook Nom du synonyme - nouvelle fenêtre",
                 "url": "https://www.facebook.com/sharer.php?u=http%3A%2F%2Ftestserver%2Fdechet%2Fnom-du-synonyme%2F",
             },
             "twitter": {
-                "title": "partager Nom du synonyme sur X - nouvelle fenêtre",
+                "title": "Partager sur X Nom du synonyme - nouvelle fenêtre",
                 "url": "https://twitter.com/intent/tweet?url=http%3A%2F%2Ftestserver%2Fdechet%2Fnom-du-synonyme%2F&text=Découvrez le site de l'ADEME “Que faire de mes objets & déchets”&via=Longue+vie+aux+objets&hashtags=longuevieauxobjets,ademe",
             },
             "linkedin": {
-                "title": "partager Nom du synonyme sur LinkedIn - nouvelle fenêtre",
+                "title": "Partager sur LinkedIn Nom du synonyme - nouvelle fenêtre",
                 "url": "https://www.linkedin.com/shareArticle?url=http%3A%2F%2Ftestserver%2Fdechet%2Fnom-du-synonyme%2F&title=Découvrez le site de l'ADEME “Que faire de mes objets & déchets”",
             },
             "email": {
-                "title": "partager Nom du synonyme par email - nouvelle fenêtre",
+                "title": "Partager par email Nom du synonyme - nouvelle fenêtre",
                 "url": "mailto:?subject=D%C3%A9couvrez%20le%20site%20de%20l%27ADEME%20%E2%80%9CQue%20faire%20de%20mes%20objets%20%26%20d%C3%A9chets%E2%80%9D&body=Bonjour%2C%0A%20Vous%20souhaitez%20encourager%20au%20tri%20et%20la%20consommation%20responsable%2C%20le%20site%20de%20l%E2%80%99ADEME%20Que%20faire%20de%20mes%20objets%20%26%20d%C3%A9chets%20accompagne%20les%20citoyens%20gr%C3%A2ce%20%C3%A0%20des%20bonnes%20pratiques%20et%20adresses%20pr%C3%A8s%20de%20chez%20eux%2C%20pour%20%C3%A9viter%20l%27achat%20neuf%20et%20r%C3%A9duire%20les%20d%C3%A9chets.%0AD%C3%A9couvrez%20le%20ici%20%3A%20http%3A//testserver/dechet/nom-du-synonyme/",
             },
         }
@@ -50,19 +50,19 @@ class TestSharer:
         assert sharer == {
             "url": "http://testserver/adresse_details/un-coucou-unique",
             "facebook": {
-                "title": "partager Coucou sur Facebook - nouvelle fenêtre",
+                "title": "Partager sur Facebook Coucou - nouvelle fenêtre",
                 "url": "https://www.facebook.com/sharer.php?u=http%3A%2F%2Ftestserver%2Fadresse_details%2Fun-coucou-unique",
             },
             "twitter": {
-                "title": "partager Coucou sur X - nouvelle fenêtre",
+                "title": "Partager sur X Coucou - nouvelle fenêtre",
                 "url": "https://twitter.com/intent/tweet?url=http%3A%2F%2Ftestserver%2Fadresse_details%2Fun-coucou-unique&text=Découvrez le site de l'ADEME “Que faire de mes objets & déchets”&via=Longue+vie+aux+objets&hashtags=longuevieauxobjets,ademe",
             },
             "linkedin": {
-                "title": "partager Coucou sur LinkedIn - nouvelle fenêtre",
+                "title": "Partager sur LinkedIn Coucou - nouvelle fenêtre",
                 "url": "https://www.linkedin.com/shareArticle?url=http%3A%2F%2Ftestserver%2Fadresse_details%2Fun-coucou-unique&title=Découvrez le site de l'ADEME “Que faire de mes objets & déchets”",
             },
             "email": {
-                "title": "partager Coucou par email - nouvelle fenêtre",
+                "title": "Partager par email Coucou - nouvelle fenêtre",
                 "url": "mailto:?subject=D%C3%A9couvrez%20le%20site%20de%20l%27ADEME%20%E2%80%9CQue%20faire%20de%20mes%20objets%20%26%20d%C3%A9chets%E2%80%9D&body=Bonjour%2C%0A%20Vous%20souhaitez%20encourager%20au%20tri%20et%20la%20consommation%20responsable%2C%20le%20site%20de%20l%E2%80%99ADEME%20Que%20faire%20de%20mes%20objets%20%26%20d%C3%A9chets%20accompagne%20les%20citoyens%20gr%C3%A2ce%20%C3%A0%20des%20bonnes%20pratiques%20et%20adresses%20pr%C3%A8s%20de%20chez%20eux%2C%20pour%20%C3%A9viter%20l%27achat%20neuf%20et%20r%C3%A9duire%20les%20d%C3%A9chets.%0AD%C3%A9couvrez%20le%20ici%20%3A%20http%3A//testserver/adresse_details/un-coucou-unique",
             },
         }
